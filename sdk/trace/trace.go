04d91d0b4a73cf5695c9f596573e30e51895beab
rahulpa <rahulpa@google.com>
2019-07-02 10:42:34 -0700

    fix TraceID.

--- sdk/trace/trace.go
+++ sdk/trace/trace.go
@@ -110,11 +110,11 @@ func (t *tracer) Start(ctx context.Context, name string, opts ...apitrace.SpanOp
 
 	if parentScope.HasTraceID() {
 		parent := parentScope.SpanContext
-		child.TraceIDHigh = parent.TraceIDHigh
-		child.TraceIDLow = parent.TraceIDLow
+		child.TraceID.High = parent.TraceID.High
+		child.TraceID.Low = parent.TraceID.Low
 	} else {
-		child.TraceIDHigh = rand.Uint64()
-		child.TraceIDLow = rand.Uint64()
+		child.TraceID.High = rand.Uint64()
+		child.TraceID.Low = rand.Uint64()
 	}
 
 	childScope := core.ScopeID{
