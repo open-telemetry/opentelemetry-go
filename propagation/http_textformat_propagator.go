067cb3f3847cc27b912f9fc21911c607a2a4f6f3
rahulpa <rahulpa@google.com>
2019-08-09 12:44:34 -0700

    add http propagator interface and w3c propagator implementation.

---
+++ propagation/http_textformat_propagator.go
@@ -0,0 +1,169 @@
+// Copyright 2018, OpenCensus Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package tracecontext contains HTTP propagator for TraceContext standard.
+// See https://github.com/w3c/distributed-tracing for more information.
+package propagation // import "go.opentelemetry.io/propagation"
+
+import (
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"net/textproto"
+	"strconv"
+	"strings"
+
+	"go.opentelemetry.io/api/core"
+	"go.opentelemetry.io/api/tag"
+	"go.opentelemetry.io/api/trace"
+)
+
+const (
+	supportedVersion  = 0
+	maxVersion        = 254
+	traceparentHeader = "traceparent"
+)
+
+type textFormatPropagator struct{}
+
+var _ HTTPPropagator = textFormatPropagator{}
+
+func (t textFormatPropagator) Extractor(req *http.Request) trace.Extractor {
+	return textFormatExtractor{req: req}
+}
+
+func (t textFormatPropagator) Injector(req *http.Request) trace.Injector {
+	return textFormatInjector{req: req}
+}
+
+// TextFormatPropagator creates a new propagator. The propagator is then used
+// to create Injector and Extrator associated with a specific request. Injectors
+// and Extractors respectively provides method to inject and extract SpanContext
+// into/from the http request.
+func TextFormatPropagator() textFormatPropagator {
+	return textFormatPropagator{}
+}
+
+type textFormatExtractor struct {
+	req *http.Request
+}
+
+var _ trace.Extractor = textFormatExtractor{}
+
+// Extract implements Extract method of trace.Extractor interface. It extracts
+// W3C TraceContext Header and decodes SpanContext from the Header.
+func (f textFormatExtractor) Extract() (sc core.SpanContext, tm tag.Map) {
+	h, ok := getRequestHeader(f.req, traceparentHeader, false)
+	if !ok {
+		return core.INVALID_SPAN_CONTEXT, nil
+	}
+	sections := strings.Split(h, "-")
+	if len(sections) < 4 {
+		return core.INVALID_SPAN_CONTEXT, nil
+	}
+
+	if len(sections[0]) != 2 {
+		return core.INVALID_SPAN_CONTEXT, nil
+	}
+	ver, err := hex.DecodeString(sections[0])
+	if err != nil {
+		return core.INVALID_SPAN_CONTEXT, nil
+	}
+	version := int(ver[0])
+	if version > maxVersion {
+		return core.INVALID_SPAN_CONTEXT, nil
+	}
+
+	if version == 0 && len(sections) != 4 {
+		return core.INVALID_SPAN_CONTEXT, nil
+	}
+
+	if len(sections[1]) != 32 {
+		return core.INVALID_SPAN_CONTEXT, nil
+	}
+
+	result, err := strconv.ParseUint(sections[1][0:16], 16, 64)
+	if err != nil {
+		return core.INVALID_SPAN_CONTEXT, nil
+	}
+	sc.TraceID.High = result
+
+	result, err = strconv.ParseUint(sections[1][16:32], 16, 64)
+	if err != nil {
+		return core.INVALID_SPAN_CONTEXT, nil
+	}
+	sc.TraceID.Low = result
+
+	if len(sections[2]) != 16 {
+		return core.INVALID_SPAN_CONTEXT, nil
+	}
+	result, err = strconv.ParseUint(sections[2][0:], 16, 64)
+	if err != nil {
+		return core.INVALID_SPAN_CONTEXT, nil
+	}
+	sc.SpanID = result
+
+	opts, err := hex.DecodeString(sections[3])
+	if err != nil || len(opts) < 1 {
+		return core.INVALID_SPAN_CONTEXT, nil
+	}
+	sc.TraceOptions = opts[0]
+
+	if !sc.IsValid() {
+		return core.INVALID_SPAN_CONTEXT, nil
+	}
+
+	// TODO: [rghetia] add tag.Map (distributed context) extraction
+	return sc, nil
+}
+
+type textFormatInjector struct {
+	req *http.Request
+}
+
+var _ trace.Injector = textFormatInjector{}
+
+// Inject implements Inject method of trace.Injector interface. It encodes
+// SpanContext into W3C TraceContext Header and injects the header into
+// the associated request.
+func (f textFormatInjector) Inject(sc core.SpanContext, tm tag.Map) {
+	if sc.IsValid() {
+		h := fmt.Sprintf("%.2x-%.16x%.16x-%.16x-%.2x",
+			supportedVersion,
+			sc.TraceID.High,
+			sc.TraceID.Low,
+			sc.SpanID,
+			sc.TraceOptions)
+		f.req.Header.Set(traceparentHeader, h)
+	}
+	// TODO: [rghetia] add tag.Map (distributed context) injection
+}
+
+// getRequestHeader returns a combined header field according to RFC7230 section 3.2.2.
+// If commaSeparated is true, multiple header fields with the same field name using be
+// combined using ",".
+// If no header was found using the given name, "ok" would be false.
+// If more than one headers was found using the given name, while commaSeparated is false,
+// "ok" would be false.
+func getRequestHeader(req *http.Request, name string, commaSeparated bool) (hdr string, ok bool) {
+	v := req.Header[textproto.CanonicalMIMEHeaderKey(name)]
+	switch len(v) {
+	case 0:
+		return "", false
+	case 1:
+		return v[0], true
+	default:
+		return strings.Join(v, ","), commaSeparated
+	}
+}
---
+++ propagation/http_textformat_propagator_test.go
@@ -0,0 +1,129 @@
+// Copyright 2018, OpenCensus Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package propagation_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"go.opentelemetry.io/api/core"
+	"go.opentelemetry.io/propagation"
+)
+
+var (
+	traceID = core.TraceID{High: 0x4bf92f3577b34da6, Low: 0xa3ce929d0e0e4736}
+	spanID  = uint64(0x00f067aa0ba902b7)
+)
+
+func TestExtractTraceContextFromHTTPReq(t *testing.T) {
+	propagator := propagation.TextFormatPropagator()
+	tests := []struct {
+		name   string
+		header string
+		wantSc core.SpanContext
+	}{
+		{
+			name:   "future version",
+			header: "02-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-01",
+			wantSc: core.SpanContext{
+				TraceID:      traceID,
+				SpanID:       spanID,
+				TraceOptions: core.TraceOptionSampled,
+			},
+		},
+		{
+			name:   "zero trace ID and span ID",
+			header: "00-00000000000000000000000000000000-0000000000000000-01",
+			wantSc: core.INVALID_SPAN_CONTEXT,
+		},
+		{
+			name:   "valid header",
+			header: "00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-01",
+			wantSc: core.SpanContext{
+				TraceID:      traceID,
+				SpanID:       spanID,
+				TraceOptions: core.TraceOptionSampled,
+			},
+		},
+		{
+			name:   "missing options",
+			header: "00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7",
+			wantSc: core.INVALID_SPAN_CONTEXT,
+		},
+		{
+			name:   "empty options",
+			header: "00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-",
+			wantSc: core.INVALID_SPAN_CONTEXT,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req, _ := http.NewRequest("GET", "http://example.com", nil)
+			req.Header.Set("traceparent", tt.header)
+			f := propagator.Extractor(req)
+
+			gotSc, _ := f.Extract()
+			if diff := cmp.Diff(gotSc, tt.wantSc); diff != "" {
+				t.Errorf("Extract Tracecontext: %s: -got +want %s", tt.name, diff)
+			}
+		})
+	}
+}
+
+func TestInjectTraceContextToHTTPReq(t *testing.T) {
+	propagator := propagation.TextFormatPropagator()
+	tests := []struct {
+		name       string
+		sc         core.SpanContext
+		wantHeader string
+	}{
+		{
+			name: "valid spancontext, sampled",
+			sc: core.SpanContext{
+				TraceID:      traceID,
+				SpanID:       spanID,
+				TraceOptions: core.TraceOptionSampled,
+			},
+			wantHeader: "00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-01",
+		},
+		{
+			name: "valid spancontext, not sampled",
+			sc: core.SpanContext{
+				TraceID: traceID,
+				SpanID:  spanID,
+			},
+			wantHeader: "00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-00",
+		},
+		{
+			name:       "invalid spancontext",
+			sc:         core.INVALID_SPAN_CONTEXT,
+			wantHeader: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req, _ := http.NewRequest("GET", "http://example.com", nil)
+			f := propagator.Injector(req)
+			f.Inject(tt.sc, nil)
+
+			gotHeader := req.Header.Get("traceparent")
+			if diff := cmp.Diff(gotHeader, tt.wantHeader); diff != "" {
+				t.Errorf("Extract Tracecontext: %s: -got +want %s", tt.name, diff)
+			}
+		})
+	}
+}
