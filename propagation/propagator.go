d9c61573fe0baa47776a3249c9d1d78c8dab5009
rahulpa <rahulpa@google.com>
2019-08-09 14:00:10 -0700

    fix copyright.

--- propagation/propagator.go
+++ propagation/propagator.go
@@ -1,4 +1,4 @@
-// Copyright 2018, OpenCensus Authors
+// Copyright 2019, OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
