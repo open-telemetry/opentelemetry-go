cd9c0e20870b1ea41e6b07f6156e680bfaf9e2b1
Krzesimir Nowak <krzesimir@kinvolk.io>
2019-08-16 13:06:20 +0200

    Eliminate the use of the Event interface
    There is no need for the SDK to provide the implementation of the
    Event interface - it is used nowhere.

--- sdk/trace/basetypes.go
+++ sdk/trace/basetypes.go
@@ -18,7 +18,6 @@ import (
 	"time"
 
 	"go.opentelemetry.io/api/core"
-	apievent "go.opentelemetry.io/api/event"
 )
 
 // event is used to describe an event with a message string and set of
@@ -27,14 +26,4 @@ type event struct {
 	msg        string
 	attributes []core.KeyValue
 	time       time.Time
-}
-
-var _ apievent.Event = &event{}
-
-func (me *event) Message() string {
-	return me.msg
-}
-
-func (me *event) Attributes() []core.KeyValue {
-	return me.attributes
 }
