bacf64004db40decfe43b5b8666904c229f78d24
Krzesimir Nowak <krzesimir@kinvolk.io>
2019-10-22 13:25:31 +0200

    drop the global meter API

--- api/metric/global.go
+++ api/metric/global.go
@@ -15,10 +15,7 @@
 package metric
 
 import (
-	"context"
 	"sync/atomic"
-
-	"go.opentelemetry.io/api/core"
 )
 
 var global atomic.Value
@@ -35,49 +32,4 @@ func GlobalMeter() Meter {
 // SetGlobalMeter sets provided meter as a global meter.
 func SetGlobalMeter(t Meter) {
 	global.Store(t)
-}
-
-// Labels gets a LabelSet from the global Meter.
-func Labels(ctx context.Context, kv ...core.KeyValue) LabelSet {
-	return GlobalMeter().Labels(ctx, kv...)
-}
-
-// NewInt64Counter creates a new integral counter with the global
-// Meter.
-func NewInt64Counter(name string, cos ...CounterOptionApplier) Int64Counter {
-	return GlobalMeter().NewInt64Counter(name, cos...)
-}
-
-// NewFloat64Counter creates a new floating point counter with the
-// global Meter.
-func NewFloat64Counter(name string, cos ...CounterOptionApplier) Float64Counter {
-	return GlobalMeter().NewFloat64Counter(name, cos...)
-}
-
-// NewInt64Gauge creates a new integral gauge with the global Meter.
-func NewInt64Gauge(name string, gos ...GaugeOptionApplier) Int64Gauge {
-	return GlobalMeter().NewInt64Gauge(name, gos...)
-}
-
-// NewFloat64Gauge creates a new floating point gauge with the global
-// Meter.
-func NewFloat64Gauge(name string, gos ...GaugeOptionApplier) Float64Gauge {
-	return GlobalMeter().NewFloat64Gauge(name, gos...)
-}
-
-// NewInt64Measure creates a new integral measure with the global
-// Meter.
-func NewInt64Measure(name string, mos ...MeasureOptionApplier) Int64Measure {
-	return GlobalMeter().NewInt64Measure(name, mos...)
-}
-
-// NewFloat64Measure creates a new floating point measure with the
-// global Meter.
-func NewFloat64Measure(name string, mos ...MeasureOptionApplier) Float64Measure {
-	return GlobalMeter().NewFloat64Measure(name, mos...)
-}
-
-// RecordBatch reports to the global Meter.
-func RecordBatch(ctx context.Context, labels LabelSet, batch ...Measurement) {
-	GlobalMeter().RecordBatch(ctx, labels, batch...)
 }
