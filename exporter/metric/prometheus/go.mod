a475d27a73dc81439b5f3f28d8cba3f141ba1385
Shreyas Srivatsan <shreyas@chronosphere.io>
2019-10-29 23:45:04 -0700

    more skeleton

--- exporter/metric/prometheus/go.mod
+++ exporter/metric/prometheus/go.mod
@@ -3,3 +3,5 @@ module go.opentelemetry.io/exporter/metric/prometheus
 go 1.13
 
 replace go.opentelemetry.io => ../../..
+
+require go.opentelemetry.io v0.0.0-00010101000000-000000000000
--- exporter/metric/prometheus/prometheus.go
+++ exporter/metric/prometheus/prometheus.go
@@ -1,3 +1,32 @@
 package prometheus
 
-// type prometheusExporter struct{}
+import (
+	"context"
+
+	"go.opentelemetry.io/sdk/export"
+)
+
+// Exporter is an implementation of metric.Exporter that sends metrics to
+// Prometheus.
+type Exporter struct {
+}
+
+var _ export.MetricBatcher = (*Exporter)(nil)
+
+// NewExporter returns a new prometheus exporter.
+func NewExporter() *Exporter {
+	return &Exporter{}
+}
+
+// AggregatorFor returns the metric aggregator used for the particular exporter.
+func (e *Exporter) AggregatorFor(record export.MetricRecord) export.MetricAggregator {
+	return nil
+}
+
+// Export exports the provide metric record to prometheus.
+func (e *Exporter) Export(
+	ctx context.Context,
+	record export.MetricRecord,
+	aggregator export.MetricAggregator) {
+
+}
