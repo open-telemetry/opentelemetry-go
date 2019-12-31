7c4aa325a31008940f03b6dd770e3f0ba7e3a3d0
Tyler Yahn <tyahn@newrelic.com>
2019-12-31 11:35:04 -0800

    Global trace forwarding implementation
    Initial concept.

--- api/global/internal/state.go
+++ api/global/internal/state.go
@@ -23,6 +23,7 @@ var (
 	globalMeter  = defaultMeterValue()
 
 	delegateMeterOnce sync.Once
+	delegateTraceOnce sync.Once
 )
 
 // TraceProvider is the internal implementation for global.TraceProvider.
@@ -32,6 +33,18 @@ func TraceProvider() trace.Provider {
 
 // SetTraceProvider is the internal implementation for global.SetTraceProvider.
 func SetTraceProvider(tp trace.Provider) {
+	delegateTraceOnce.Do(func() {
+		current := TraceProvider()
+		if current == tp {
+			// Setting the provider to the prior default is nonsense, panic.
+			// Panic is acceptable because we are likely still early in the
+			// process lifetime.
+			panic("invalid Provider, the global instance cannot be reinstalled")
+		} else if def, ok := current.(*traceProvider); ok {
+			def.setDelegate(tp)
+		}
+
+	})
 	globalTracer.Store(traceProviderHolder{tp: tp})
 }
 
@@ -59,7 +72,7 @@ func SetMeterProvider(mp metric.Provider) {
 
 func defaultTracerValue() *atomic.Value {
 	v := &atomic.Value{}
-	v.Store(traceProviderHolder{tp: trace.NoopProvider{}})
+	v.Store(traceProviderHolder{tp: &traceProvider{}})
 	return v
 }
 
@@ -74,4 +87,5 @@ func ResetForTest() {
 	globalTracer = defaultTracerValue()
 	globalMeter = defaultMeterValue()
 	delegateMeterOnce = sync.Once{}
+	delegateTraceOnce = sync.Once{}
 }
---
+++ api/global/internal/trace.go
@@ -0,0 +1,80 @@
+package internal
+
+import (
+	"context"
+	"sync"
+
+	"go.opentelemetry.io/otel/api/trace"
+)
+
+type traceProvider struct {
+	lock    sync.Mutex
+	tracers []*tracer
+
+	delegate trace.Provider
+}
+
+var _ trace.Provider = &traceProvider{}
+
+func (p *traceProvider) setDelegate(provider trace.Provider) {
+	if p.delegate != nil {
+		return
+	}
+
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	p.delegate = provider
+	for _, t := range p.tracers {
+		t.setDelegate(provider)
+	}
+
+	p.tracers = nil
+}
+
+// Tracer creates a trace.Tracer with the given name. When a delegate is set,
+// all previously returned trace.Tracers will be swapped to equivalent
+// trace.Tracers created from the delegate.
+func (p *traceProvider) Tracer(name string) trace.Tracer {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	if p.delegate != nil {
+		return p.delegate.Tracer(name)
+	}
+
+	t := &tracer{name: name}
+	p.tracers = append(p.tracers, t)
+	return t
+}
+
+type tracer struct {
+	trace.NoopTracer
+
+	once sync.Once
+	name string
+
+	delegate trace.Tracer
+}
+
+var _ trace.Tracer = &tracer{}
+
+func (t *tracer) setDelegate(provider trace.Provider) {
+	t.once.Do(func() { t.delegate = provider.Tracer(t.name) })
+}
+
+// WithSpan wraps around execution of func with delegated Tracer.
+func (t *tracer) WithSpan(ctx context.Context, name string, body func(context.Context) error) error {
+	if t.delegate != nil {
+		return t.delegate.WithSpan(ctx, name, body)
+	}
+	return t.NoopTracer.WithSpan(ctx, name, body)
+}
+
+// Start starts a span from the delegated tracer.
+func (t *tracer) Start(ctx context.Context, name string, opts ...trace.StartOption) (context.Context, trace.Span) {
+	if t.delegate != nil {
+		return t.delegate.Start(ctx, name, opts...)
+	}
+	return t.NoopTracer.Start(ctx, name, opts...)
+}
