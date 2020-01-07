8b0f7ef5f49fb9009bc671ec821a25de09899823
Martin Garton <mjgarton@users.noreply.github.com>
2020-01-07 15:30:41 +0000

    Fix aggregator import path (#421)
    When this was moved, the import path was not updated to match.  This
    fixes it to avoid a message like "code in directory foo expects import
    bar"

--- sdk/export/metric/aggregator/aggregator.go
+++ sdk/export/metric/aggregator/aggregator.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package aggregator // import "go.opentelemetry.io/otel/sdk/metric/aggregator"
+package aggregator // import "go.opentelemetry.io/otel/sdk/export/metric/aggregator"
 
 import (
 	"fmt"
