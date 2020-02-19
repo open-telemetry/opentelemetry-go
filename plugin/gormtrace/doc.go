08a3b11c3d5afecb4fff806f85e0d23b2070d223
rizkybiz <justin.defrank@gmail.com>
2020-02-19 16:34:14 -0500

    Adding gormtrace plugin

---
+++ plugin/gormtrace/doc.go
@@ -0,0 +1,6 @@
+//Package gormtrace allows for the wrapping of GORM calls to
+//databases with OpenTelemetry tracing spans. You only need to
+//create your GORM db client and pass that into gormtrace.WithContext
+//along with context.Context(). If there is a parent span referenced
+//within the context the GORM call will be a child span.
+package gormtrace
