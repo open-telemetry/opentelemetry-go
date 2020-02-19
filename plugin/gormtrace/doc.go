bdede3f5b82b1c1114a050ac4649a4d21b9b94b6
rizkybiz <justin.defrank@gmail.com>
2020-02-19 16:14:52 -0500

    Revert "Adding plugin gormtrace" wrong github
    This reverts commit 009d2e71cf5a00728fd7e567f49139c54060acbc.

--- plugin/gormtrace/doc.go
+++
@@ -1,6 +0,0 @@
-//Package gormtrace allows for the wrapping of GORM calls to
-//databases with OpenTelemetry tracing spans. You only need to
-//create your GORM db client and pass that into gormtrace.WithContext
-//along with context.Context(). If there is a parent span referenced
-//within the context the GORM call will be a child span.
-package gormtrace
