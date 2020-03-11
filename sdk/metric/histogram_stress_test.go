288821cd22b2e5d587d034ad98b80dc6bd55ded4
Gustavo Silva Paiva <guustavo.paiva@gmail.com>
2020-03-11 15:12:23 -0300

    Make histogram aggregator checkpoint consistent (#438)
    * change the histogram aggregator to have a consistent but blocking Checkpoint()
    
    * docs
    
    * wrapping docs
    
    * remove currentIdx from the 8bit alignment check
    
    * stress test
    
    * add export and move lockfreewrite algorithm to an external struct.
    
    * move state locker to another package.
    
    * add todos
    
    * minimal tests
    
    * renaming and docs
    
    * change to context.Background()
    
    * add link to algorithm and grammars
    
    Co-authored-by: Joshua MacDonald <jmacd@users.noreply.github.com>

---
+++ sdk/metric/histogram_stress_test.go
@@ -0,0 +1,60 @@
+// Copyright 2019, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This test is too large for the race detector.  This SDK uses no locks
+// that the race detector would help with, anyway.
+// +build !race
+
+package metric_test
+
+import (
+	"context"
+	"math/rand"
+	"testing"
+	"time"
+
+	"go.opentelemetry.io/otel/api/core"
+	"go.opentelemetry.io/otel/sdk/export/metric"
+	"go.opentelemetry.io/otel/sdk/metric/aggregator/histogram"
+)
+
+func TestStressInt64Histogram(t *testing.T) {
+	desc := metric.NewDescriptor("some_metric", metric.MeasureKind, nil, "", "", core.Int64NumberKind, false)
+	h := histogram.New(desc, []core.Number{core.NewInt64Number(25), core.NewInt64Number(50), core.NewInt64Number(75)})
+
+	go func() {
+		rnd := rand.New(rand.NewSource(time.Now().Unix()))
+		for {
+			_ = h.Update(context.Background(), core.NewInt64Number(rnd.Int63()), desc)
+		}
+	}()
+
+	startTime := time.Now()
+	for time.Since(startTime) < time.Second {
+		h.Checkpoint(context.Background(), desc)
+
+		b, _ := h.Histogram()
+		c, _ := h.Count()
+
+		var realCount int64
+		for _, c := range b.Counts {
+			v := c.AsInt64()
+			realCount += v
+		}
+
+		if realCount != c {
+			t.Fail()
+		}
+	}
+}
