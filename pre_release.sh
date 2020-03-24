757a29365d7b49d47a485c178f8f75492bcb1684
Krzesimir Nowak <krzesimir@kinvolk.io>
2020-03-24 18:05:40 +0100

    Make pre_release.sh script work with GNU
    The `-i` sed flag is for doing in-place updates of the file. On some
    systems it requires a parameter, on other (GNU) the parameter is
    optional, but should be "glued" to the flag (`-i.bak` for example).
    That's is problematic, so just emulate the in-place updates with
    copies and removals.

--- pre_release.sh
+++ pre_release.sh
@@ -62,8 +62,9 @@ git checkout -b pre_release_${TAG} master
 PACKAGE_DIRS=$(find . -mindepth 2 -type f -name 'go.mod' -exec dirname {} \; | egrep -v 'tools' | sed 's/^\.\///' | sort)
 
 for dir in $PACKAGE_DIRS; do
-	sed -i .bak "s/opentelemetry.io\/otel\([^ ]*\) v[0-9]*\.[0-9]*\.[0-9]/opentelemetry.io\/otel\1 ${TAG}/" ${dir}/go.mod
-	rm -f ${dir}/go.mod.bak
+	cp "${dir}/go.mod" "${dir}/go.mod.bak"
+	sed "s/opentelemetry.io\/otel\([^ ]*\) v[0-9]*\.[0-9]*\.[0-9]/opentelemetry.io\/otel\1 ${TAG}/" "${dir}/go.mod.bak" >"${dir}/go.mod"
+	rm -f "${dir}/go.mod.bak"
 done
 
 # Run lint to update go.sum
