973fddd2a27537ca31052ee048db2ae3ffdcf0c2
rahulpa <rahulpa@google.com>
2020-03-27 17:26:39 -0700

    fix review comments.

--- tools/go.mod
+++ tools/go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/client9/misspell v0.3.4
 	github.com/golangci/golangci-lint v1.23.6
+	github.com/itchyny/gojq v0.9.0 // indirect
 	golang.org/x/tools v0.0.0-20200220155224-947cbf191135
 )
--- verify_examples.sh
+++ verify_examples.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
-# Copyright 2020, OpenTelemetry Authors
+#!/bin/bash
+
+# Copyright The OpenTelemetry Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,9 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
+set -euo pipefail
 
 cd $(dirname $0)
+TOOLS_DIR=$(pwd)/.tools
 
 if [ -z "${GOPATH}" ] ; then
 	printf "GOPATH is not defined.\n"
@@ -27,23 +29,45 @@ if [ ! -d "${GOPATH}" ] ; then
 	exit -1
 fi
 
+# Pre-requisites
+if ! git diff --quiet; then \
+	git status
+	printf "\n\nError: working tree is not clean\n"
+	exit -1
+fi
+
+if [ "$(git tag --contains $(git log -1 --pretty=format:"%H"))" = "" ] ; then
+	printf "$(git log -1)"
+	printf "\n\nError: HEAD is not pointing to a tagged version"
+fi
+exit
+
+
+make ${TOOLS_DIR}/gojq
+
 DIR_TMP="${GOPATH}/src/oteltmp/"
 rm -rf $DIR_TMP
 mkdir -p $DIR_TMP
 
 printf "Copy examples to ${DIR_TMP}\n"
-rsync -r ./example ${DIR_TMP}
+cp -a ./example ${DIR_TMP}
 
 # Update go.mod files
 printf "Update go.mod: rename module and remove replace\n"
 
 PACKAGE_DIRS=$(find . -mindepth 2 -type f -name 'go.mod' -exec dirname {} \; | egrep 'example' | sed 's/^\.\///' | sort)
+
 for dir in $PACKAGE_DIRS; do
 	printf "  Update go.mod for $dir\n"
-	#rsync -r ${dir} ${DIR_TMP}/example
 	(cd "${DIR_TMP}/${dir}" && \
-         sed -i .bak "s/module go.opentelemetry.io\/otel/module oteltmp/" go.mod && \
-         sed -i .bak "s/^.*=\>.*$//" go.mod && \
+	 # replaces is ("mod1" "mod2" …)
+	 replaces=($(go mod edit -json | ${TOOLS_DIR}/gojq '.Replace[].Old.Path')) && \
+	 # strip double quotes
+	 replaces=("${replaces[@]%\"}") && \
+	 replaces=("${replaces[@]#\"}") && \
+	 # make an array (-dropreplace=mod1 -dropreplace=mod2 …) 
+	 dropreplaces=("${replaces[@]/#/-dropreplace=}") && \
+	 go mod edit -module "oteltmp/${dir}" "${dropreplaces[@]}" && \
 	 go mod tidy)
 done
 printf "Update done:\n\n"
@@ -52,7 +76,7 @@ printf "Update done:\n\n"
 # directories that contain go.mod files.
 # 
 printf "Build examples:\n"
-EXAMPLES=`./get_main_pkgs.sh ./example`
+EXAMPLES=$(./get_main_pkgs.sh ./example)
 for ex in $EXAMPLES; do
 	printf "  Build $ex in ${DIR_TMP}/${ex}\n"
 	(cd "${DIR_TMP}/${ex}" && \
