4b272526a4b94a6016a044ae07502963b06365a2
Steven E. Harris <seh@panix.com>
2020-04-09 09:25:03 -0400

    othttp: add RemoteAddrKey for HTTP client address
    Include the requesting HTTP client's network address in each top-level
    span emitted by othttp.NewHandler.

--- plugin/othttp/handler.go
+++ plugin/othttp/handler.go
@@ -34,6 +34,7 @@ const (
 	URLKey        = core.Key("http.url")         // the http url (http.Request.URL.String())
 	UserAgentKey  = core.Key("http.user_agent")  // the http user agent (http.Request.UserAgent())
 	RouteKey      = core.Key("http.route")       // the http route (ex: /users/:id)
+	RemoteAddrKey = core.Key("http.remote_addr") // the network address of the client that sent the HTTP request (http.Request.RemoteAddr)
 	StatusCodeKey = core.Key("http.status_code") // if set, the http status
 	ReadBytesKey  = core.Key("http.read_bytes")  // if anything was read from the request body, the total number of bytes read
 	ReadErrorKey  = core.Key("http.read_error")  // If an error occurred while reading a request, the string of the error (io.EOF is not recorded)
@@ -216,6 +217,7 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		PathKey.String(r.URL.Path),
 		URLKey.String(r.URL.String()),
 		UserAgentKey.String(r.UserAgent()),
+		RemoteAddrKey.String(r.RemoteAddr),
 	)
 
 	h.handler.ServeHTTP(rww, r.WithContext(ctx))
