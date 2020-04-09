56790effca02c4efa495a0d17e24d7562fcf4810
Anthony J Mirabella <a9@aneurysm9.com>
2020-04-08 21:08:21 -0400

    Create basic othttp.Transport for simple client instrumentation

--- plugin/othttp/handler_test.go
+++ plugin/othttp/handler_test.go
@@ -23,7 +23,7 @@ import (
 	mocktrace "go.opentelemetry.io/otel/internal/trace"
 )
 
-func TestBasics(t *testing.T) {
+func TestHandlerBasics(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	var id uint64
@@ -57,106 +57,5 @@ func TestBasics(t *testing.T) {
 	}
 	if got, expected := string(d), "hello world"; got != expected {
 		t.Fatalf("got %q, expected %q", got, expected)
-	}
-}
-
-func TestBasicFilter(t *testing.T) {
-	rr := httptest.NewRecorder()
-
-	var id uint64
-	tracer := mocktrace.MockTracer{StartSpanID: &id}
-
-	h := NewHandler(
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if _, err := io.WriteString(w, "hello world"); err != nil {
-				t.Fatal(err)
-			}
-		}), "test_handler",
-		WithTracer(&tracer),
-		WithFilter(func(r *http.Request) bool {
-			return false
-		}),
-	)
-
-	r, err := http.NewRequest(http.MethodGet, "http://localhost/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	h.ServeHTTP(rr, r)
-	if got, expected := rr.Result().StatusCode, http.StatusOK; got != expected {
-		t.Fatalf("got %d, expected %d", got, expected)
-	}
-	if got := rr.Header().Get("Traceparent"); got != "" {
-		t.Fatal("expected empty trace header")
-	}
-	if got, expected := id, uint64(0); got != expected {
-		t.Fatalf("got %d, expected %d", got, expected)
-	}
-	d, err := ioutil.ReadAll(rr.Result().Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if got, expected := string(d), "hello world"; got != expected {
-		t.Fatalf("got %q, expected %q", got, expected)
-	}
-}
-
-func TestSpanNameFormatter(t *testing.T) {
-	var testCases = []struct {
-		name      string
-		formatter func(s string, r *http.Request) string
-		operation string
-		expected  string
-	}{
-		{
-			name:      "default formatter",
-			formatter: defaultFormatter,
-			operation: "test_operation",
-			expected:  "test_operation",
-		},
-		{
-			name: "custom formatter",
-			formatter: func(s string, r *http.Request) string {
-				return r.URL.Path
-			},
-			operation: "",
-			expected:  "/hello",
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			rr := httptest.NewRecorder()
-			var id uint64
-			var spanName string
-			tracer := mocktrace.MockTracer{
-				StartSpanID: &id,
-				OnSpanStarted: func(span *mocktrace.MockSpan) {
-					spanName = span.Name
-				},
-			}
-			handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				if _, err := io.WriteString(w, "hello world"); err != nil {
-					t.Fatal(err)
-				}
-			})
-			h := NewHandler(
-				handler,
-				tc.operation,
-				WithTracer(&tracer),
-				WithSpanNameFormatter(tc.formatter),
-			)
-			r, err := http.NewRequest(http.MethodGet, "http://localhost/hello", nil)
-			if err != nil {
-				t.Fatal(err)
-			}
-			h.ServeHTTP(rr, r)
-			if got, expected := rr.Result().StatusCode, http.StatusOK; got != expected {
-				t.Fatalf("got %d, expected %d", got, expected)
-			}
-			if got, expected := spanName, tc.expected; got != expected {
-				t.Fatalf("got %q, expected %q", got, expected)
-			}
-		})
 	}
 }
