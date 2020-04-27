e440a2cf608d38fbf92b2e3d2724a1de75ede9b8
Evan Torrie <evantorrie@users.noreply.github.com>
2020-04-27 14:49:43 -0700

    Switch MinMaxSumCount to a mutex lock instead of StateLocker
    With multiple values being modified for each Update(), a single mutex
    lock and non-atomic operations ends up being faster than making each
    value update into an atomic operation.

---
+++ sdk/metric/aggregator/minmaxsumcount/mmsc_statelocker.go
@@ -0,0 +1,193 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package minmaxsumcount // import "go.opentelemetry.io/otel/sdk/metric/aggregator/minmaxsumcount"
+
+import (
+	"context"
+
+	"go.opentelemetry.io/otel/api/core"
+	"go.opentelemetry.io/otel/api/metric"
+	export "go.opentelemetry.io/otel/sdk/export/metric"
+	"go.opentelemetry.io/otel/sdk/export/metric/aggregator"
+	"go.opentelemetry.io/otel/sdk/internal"
+)
+
+type (
+	// Aggregator aggregates measure events, keeping only the max,
+	// sum, and count.
+	AggregatorSL struct {
+		// states has to be aligned for 64-bit atomic operations.
+		states [2]stateSL
+		lock   internal.StateLocker
+		kind   core.NumberKind
+	}
+
+	stateSL struct {
+		// all fields have to be aligned for 64-bit atomic operations.
+		count core.Number
+		sum   core.Number
+		min   core.Number
+		max   core.Number
+	}
+)
+
+var _ export.Aggregator = &AggregatorSL{}
+var _ aggregator.MinMaxSumCount = &AggregatorSL{}
+
+// NewSL returns a new measure aggregator for computing min, max, sum, and
+// count.  It does not compute quantile information other than Max.
+//
+// This aggregator uses the StateLocker pattern to guarantee
+// the count, sum, min and max are consistent within a checkpoint
+func NewSL(desc *metric.Descriptor) *AggregatorSL {
+	kind := desc.NumberKind()
+	return &AggregatorSL{
+		kind: kind,
+		states: [2]stateSL{
+			{
+				count: core.NewUint64Number(0),
+				sum:   kind.Zero(),
+				min:   kind.Maximum(),
+				max:   kind.Minimum(),
+			},
+			{
+				count: core.NewUint64Number(0),
+				sum:   kind.Zero(),
+				min:   kind.Maximum(),
+				max:   kind.Minimum(),
+			},
+		},
+	}
+}
+
+// Sum returns the sum of values in the checkpoint.
+func (c *AggregatorSL) Sum() (core.Number, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.checkpoint().sum, nil
+}
+
+// Count returns the number of values in the checkpoint.
+func (c *AggregatorSL) Count() (int64, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.checkpoint().count.CoerceToInt64(core.Uint64NumberKind), nil
+}
+
+// Min returns the minimum value in the checkpoint.
+// The error value aggregator.ErrNoData will be returned
+// if there were no measurements recorded during the checkpoint.
+func (c *AggregatorSL) Min() (core.Number, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	if c.checkpoint().count.IsZero(core.Uint64NumberKind) {
+		return c.kind.Zero(), aggregator.ErrNoData
+	}
+	return c.checkpoint().min, nil
+}
+
+// Max returns the maximum value in the checkpoint.
+// The error value aggregator.ErrNoData will be returned
+// if there were no measurements recorded during the checkpoint.
+func (c *AggregatorSL) Max() (core.Number, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	if c.checkpoint().count.IsZero(core.Uint64NumberKind) {
+		return c.kind.Zero(), aggregator.ErrNoData
+	}
+	return c.checkpoint().max, nil
+}
+
+// Checkpoint saves the current state and resets the current state to
+// the empty set.
+func (c *AggregatorSL) Checkpoint(ctx context.Context, desc *metric.Descriptor) {
+	c.lock.SwapActiveState(c.resetCheckpoint)
+}
+
+// checkpoint returns the "cold" state, i.e. state collected prior to the
+// most recent Checkpoint() call
+func (c *AggregatorSL) checkpoint() *stateSL {
+	return &c.states[c.lock.ColdIdx()]
+}
+
+func (c *AggregatorSL) resetCheckpoint() {
+	checkpoint := c.checkpoint()
+
+	checkpoint.count.SetUint64(0)
+	checkpoint.sum.SetNumber(c.kind.Zero())
+	checkpoint.min.SetNumber(c.kind.Maximum())
+	checkpoint.max.SetNumber(c.kind.Minimum())
+}
+
+// Update adds the recorded measurement to the current data set.
+func (c *AggregatorSL) Update(_ context.Context, number core.Number, desc *metric.Descriptor) error {
+	kind := desc.NumberKind()
+
+	cIdx := c.lock.Start()
+	defer c.lock.End(cIdx)
+
+	current := &c.states[cIdx]
+	current.count.AddUint64Atomic(1)
+	current.sum.AddNumberAtomic(kind, number)
+
+	for {
+		cmin := current.min.AsNumberAtomic()
+
+		if number.CompareNumber(kind, cmin) >= 0 {
+			break
+		}
+		if current.min.CompareAndSwapNumber(cmin, number) {
+			break
+		}
+	}
+	for {
+		cmax := current.max.AsNumberAtomic()
+
+		if number.CompareNumber(kind, cmax) <= 0 {
+			break
+		}
+		if current.max.CompareAndSwapNumber(cmax, number) {
+			break
+		}
+	}
+	return nil
+}
+
+// Merge combines two data sets into one.
+func (c *AggregatorSL) Merge(oa export.Aggregator, desc *metric.Descriptor) error {
+	o, _ := oa.(*AggregatorSL)
+	if o == nil {
+		return aggregator.NewInconsistentMergeError(c, oa)
+	}
+
+	// Lock() synchronizes Merge() and Checkpoint() to ensure all operations of
+	// Merge() are performed on the same state.
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	current := c.checkpoint()
+	ocheckpoint := o.checkpoint()
+
+	current.count.AddNumber(core.Uint64NumberKind, ocheckpoint.count)
+	current.sum.AddNumber(desc.NumberKind(), ocheckpoint.sum)
+
+	if current.min.CompareNumber(desc.NumberKind(), ocheckpoint.min) > 0 {
+		current.min.SetNumber(ocheckpoint.min)
+	}
+	if current.max.CompareNumber(desc.NumberKind(), ocheckpoint.max) < 0 {
+		current.max.SetNumber(ocheckpoint.max)
+	}
+	return nil
+}
--- sdk/metric/aggregator/minmaxsumcount/mmsc_test.go
+++ sdk/metric/aggregator/minmaxsumcount/mmsc_test.go
@@ -16,6 +16,7 @@ package minmaxsumcount
 
 import (
 	"context"
+	"fmt"
 	"math"
 	"math/rand"
 	"os"
@@ -27,6 +28,7 @@ import (
 	"go.opentelemetry.io/otel/api/core"
 	"go.opentelemetry.io/otel/api/metric"
 	ottest "go.opentelemetry.io/otel/internal/testing"
+	export "go.opentelemetry.io/otel/sdk/export/metric"
 	"go.opentelemetry.io/otel/sdk/export/metric/aggregator"
 	"go.opentelemetry.io/otel/sdk/metric/aggregator/test"
 )
@@ -66,8 +68,8 @@ var (
 func TestMain(m *testing.M) {
 	fields := []ottest.FieldOffset{
 		{
-			Name:   "Aggregator.states",
-			Offset: unsafe.Offsetof(Aggregator{}.states),
+			Name:   "Aggregator.current",
+			Offset: unsafe.Offsetof(Aggregator{}.current),
 		},
 		{
 			Name:   "state.count",
@@ -237,4 +239,41 @@ func TestMaxSumCountNotSet(t *testing.T) {
 		require.Equal(t, aggregator.ErrNoData, err)
 		require.Equal(t, core.Number(0), max)
 	})
+}
+
+func BenchmarkMinMaxSumCountAggregators(b *testing.B) {
+	ctx := context.Background()
+	descriptor := test.NewAggregatorTest(metric.MeasureKind, core.Float64NumberKind)
+
+	for name, agg := range map[string]export.Aggregator{
+		"mutex":       New(descriptor),
+		"statelocker": NewSL(descriptor)} {
+		b.Run(name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				if err := agg.Update(ctx, core.NewFloat64Number(float64(i)), descriptor); err != nil {
+					fmt.Print(err)
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkMinMaxSumCountAggregatorsRunParallel(b *testing.B) {
+	ctx := context.Background()
+	descriptor := test.NewAggregatorTest(metric.MeasureKind, core.Float64NumberKind)
+
+	for name, agg := range map[string]export.Aggregator{
+		"mutex":       New(descriptor),
+		"statelocker": NewSL(descriptor)} {
+		b.Run(name, func(b *testing.B) {
+			b.ResetTimer()
+			b.RunParallel(func(pb *testing.PB) {
+				for pb.Next() {
+					if err := agg.Update(ctx, core.NewFloat64Number(float64(1.23)), descriptor); err != nil {
+						fmt.Print(err)
+					}
+				}
+			})
+		})
+	}
 }
