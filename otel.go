82538a830b4822ff288abc14a9a27f2f0a892c43
tensorchen <tensorchen@gmail.com>
2020-05-07 00:41:11 +0800

    rm api/global alias functions

--- otel.go
+++ otel.go
@@ -15,54 +15,13 @@
 package otel
 
 import (
-	"go.opentelemetry.io/otel/api/global"
 	"go.opentelemetry.io/otel/api/metric"
 	"go.opentelemetry.io/otel/api/propagation"
 	"go.opentelemetry.io/otel/api/trace"
 )
 
 type Tracer = trace.Tracer
-type TraceProvider = trace.Provider
 
 type Meter = metric.Meter
-type MeterProvider = metric.Provider
 
 type Propagators = propagation.Propagators
-
-func GlobalTracer(name string) Tracer {
-	return global.Tracer(name)
-}
-
-// GlobalTraceProvider returns the registered global trace provider.
-func GlobalTraceProvider() TraceProvider {
-	return global.TraceProvider()
-}
-
-// SetGlobalTraceProvider registers `tp` as the global trace provider.
-func SetGlobalTraceProvider(tp TraceProvider) {
-	global.SetTraceProvider(tp)
-}
-
-func GlobalMeter(name string) Meter {
-	return global.Meter(name)
-}
-
-// GlobalMeterProvider returns the registered global meter provider.
-func GlobalMeterProvider() MeterProvider {
-	return global.MeterProvider()
-}
-
-// SetGlobalMeterProvider registers `mp` as the global meter provider.
-func SetGlobalMeterProvider(mp MeterProvider) {
-	global.SetMeterProvider(mp)
-}
-
-// GlobalPropagators returns the registered global propagators instance.
-func GlobalPropagators() Propagators {
-	return global.Propagators()
-}
-
-// SetGlobalPropagators registers `p` as the global propagators instance.
-func SetGlobalPropagators(p Propagators) {
-	global.SetPropagators(p)
-}
