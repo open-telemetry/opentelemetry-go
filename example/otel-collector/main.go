c6c841fc7c11dcda5929bdeafa867af0b20c1892
Stefan Prisca <stefan.prisca@gmail.com>
2020-05-08 14:25:20 +0200

    add simple demo for connecting to collector

---
+++ example/otel-collector/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.opentelemetry.io/otel/api/global"
+	"go.opentelemetry.io/otel/exporters/otlp"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	exp, err := otlp.NewExporter(otlp.WithInsecure(),
+		otlp.WithGRPCDialOption(grpc.WithBlock()))
+	if err != nil {
+		log.Fatalf("Failed to create the collector exporter: %v", err)
+	}
+	if err = exp.GetLastConnectError(); err != nil {
+		log.Fatalf("Failed to create the collector exporter: %v", err)
+	}
+
+	defer func() {
+
+		_ = exp.Stop()
+	}()
+
+	tp, _ := sdktrace.NewProvider(
+		sdktrace.WithConfig(sdktrace.Config{DefaultSampler: sdktrace.AlwaysSample()}),
+		sdktrace.WithBatcher(exp, // add following two options to ensure flush
+			sdktrace.WithScheduleDelayMillis(5),
+			sdktrace.WithMaxExportBatchSize(1),
+		))
+	if err != nil {
+		log.Fatalf("error creating trace provider: %v\n", err)
+	}
+
+	global.SetTraceProvider(tp)
+	tracer := global.Tracer("test-tracer")
+
+	// Then use the OpenTelemetry tracing library, like we normally would.
+	ctx, span := tracer.Start(context.Background(), "CollectorExporter-Example")
+	defer span.End()
+
+	for i := 0; i < 10; i++ {
+		_, iSpan := tracer.Start(ctx, fmt.Sprintf("Sample-%d", i))
+		<-time.After(6 * time.Second)
+		iSpan.End()
+	}
+
+	// exp.ExportSpan(ctx, span)
+}
