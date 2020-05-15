f7f3fc39181f6326752f0b889774010d02e08a85
Tyler Yahn <codingalias@gmail.com>
2020-05-15 11:38:02 -0700

    Remove redundant "Key" from const names

--- sdk/resource/resourcekey/const.go
+++ sdk/resource/resourcekey/const.go
@@ -20,18 +20,18 @@ package resourcekey // import "go.opentelemetry.io/otel/sdk/resource/resourcekey
 // Constants for Service resources.
 const (
 	// A uniquely identifying name for a Service.
-	ServiceKeyName       = "service.name"
-	ServiceKeyNamespace  = "service.namespace"
-	ServiceKeyInstanceID = "service.instance.id"
-	ServiceKeyVersion    = "service.version"
+	ServiceName       = "service.name"
+	ServiceNamespace  = "service.namespace"
+	ServiceInstanceID = "service.instance.id"
+	ServiceVersion    = "service.version"
 )
 
 // Constants for Library resources.
 const (
 	// A uniquely identifying name for a Library.
-	LibraryKeyName     = "library.name"
-	LibraryKeyLanguage = "library.language"
-	LibraryKeyVersion  = "library.version"
+	LibraryName     = "library.name"
+	LibraryLanguage = "library.language"
+	LibraryVersion  = "library.version"
 )
 
 // Constants for Kubernetes resources.
@@ -40,26 +40,26 @@ const (
 	// does not have cluster names as an internal concept so this may be
 	// set to any meaningful value within the environment. For example,
 	// GKE clusters have a name which can be used for this label.
-	K8SKeyClusterName    = "k8s.cluster.name"
-	K8SKeyNamespaceName  = "k8s.namespace.name"
-	K8SKeyPodName        = "k8s.pod.name"
-	K8SKeyDeploymentName = "k8s.deployment.name"
+	K8SClusterName    = "k8s.cluster.name"
+	K8SNamespaceName  = "k8s.namespace.name"
+	K8SPodName        = "k8s.pod.name"
+	K8SDeploymentName = "k8s.deployment.name"
 )
 
 // Constants for Container resources.
 const (
 	// A uniquely identifying name for the Container.
-	ContainerKeyName      = "container.name"
-	ContainerKeyImageName = "container.image.name"
-	ContainerKeyImageTag  = "container.image.tag"
+	ContainerName      = "container.name"
+	ContainerImageName = "container.image.name"
+	ContainerImageTag  = "container.image.tag"
 )
 
 // Constants for Cloud resources.
 const (
-	CloudKeyProvider  = "cloud.provider"
-	CloudKeyAccountID = "cloud.account.id"
-	CloudKeyRegion    = "cloud.region"
-	CloudKeyZone      = "cloud.zone"
+	CloudProvider  = "cloud.provider"
+	CloudAccountID = "cloud.account.id"
+	CloudRegion    = "cloud.region"
+	CloudZone      = "cloud.zone"
 
 	// Cloud Providers
 	CloudProviderAWS   = "aws"
@@ -70,13 +70,13 @@ const (
 // Constants for Host resources.
 const (
 	// A uniquely identifying name for the host.
-	HostKeyName = "host.name"
+	HostName = "host.name"
 
 	// A hostname as returned by the 'hostname' command on host machine.
-	HostKeyHostName     = "host.hostname"
-	HostKeyID           = "host.id"
-	HostKeyType         = "host.type"
-	HostKeyImageName    = "host.image.name"
-	HostKeyImageID      = "host.image.id"
-	HostKeyImageVersion = "host.image.version"
+	HostHostName     = "host.hostname"
+	HostID           = "host.id"
+	HostType         = "host.type"
+	HostImageName    = "host.image.name"
+	HostImageID      = "host.image.id"
+	HostImageVersion = "host.image.version"
 )
