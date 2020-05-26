a4cf6e1d69ace51097420bc20d07d843a3fe0bfb
wtong98 <williamtong@google.com>
2020-05-26 15:24:16 -0500

    Add OTLP metrics

--- example/otlp/main.go
+++ example/otlp/main.go
@@ -8,43 +8,71 @@ import (
 	"log"
 	"time"
 
-	// "github.com/open-telemetry/opentelemetry-collector/translator/conventions"
-
 	"go.opentelemetry.io/otel/api/global"
-	// "go.opentelemetry.io/otel/api/kv"
-	// "go.opentelemetry.io/otel/api/metric"
+	"go.opentelemetry.io/otel/api/kv"
+	"go.opentelemetry.io/otel/api/metric"
 	"go.opentelemetry.io/otel/exporters/otlp"
+	"go.opentelemetry.io/otel/sdk/metric/controller/push"
+	"go.opentelemetry.io/otel/sdk/metric/selector/simple"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	// tracestdout "go.opentelemetry.io/otel/exporters/trace/stdout"
 )
 
-func initExporter() {
+// TODO: basic documentation
+func initProvider() {
 	exp, err := otlp.NewExporter(
 		otlp.WithInsecure(),
 		otlp.WithAddress("localhost:55680"),
 	)
-	// exp, err := tracestdout.NewExporter(tracestdout.Options{PrettyPrint: true})
 	handleErr(err, "Failed to create exporter: $v")
 
-	// defer handleErr(exp.Stop(), "Failed to stop exporter: %v")
-
-	provider, err := sdktrace.NewProvider(
+	traceProvider, err := sdktrace.NewProvider(
 		sdktrace.WithConfig(sdktrace.Config{DefaultSampler: sdktrace.AlwaysSample()}),
         sdktrace.WithSyncer(exp),
 	)
 	handleErr(err, "Failed to create trace provider: %v")
 
-	global.SetTraceProvider(provider)
+	pusher := push.New(
+		simple.NewWithExactDistribution(),
+		exp,
+		push.WithStateful(true),
+		push.WithPeriod(time.Duration(5) * time.Second),
+	)
+
+	global.SetTraceProvider(traceProvider)
+	global.SetMeterProvider(pusher.Provider())
+	pusher.Start()
 }
 
+
 func main() {
-	initExporter()
+	initProvider()
+
 	tracer := global.Tracer("mage-sense")
+	meter := global.Meter("mage-read")
+
+	commonLabels := []kv.KeyValue{
+		kv.String("work-room", "East Scriptorium"),
+		kv.String("occupancy", "69,105"),
+		kv.String("priority", "Ultra"),
+	}
+
+	oneMetricCB := func(_ context.Context, result metric.Float64ObserverResult) {
+		result.Observe(1, commonLabels...)
+	}
+	_ = metric.Must(meter).NewFloat64ValueObserver("scrying.glass.one", oneMetricCB,
+		metric.WithDescription("A ValueObserver set to 1.0"),
+	)
+
+	valuerecorder := metric.Must(meter).
+		NewFloat64ValueRecorder("scrying.glass.two").
+		Bind(commonLabels...)
+	defer valuerecorder.Unbind()
 
 	ctx, span := tracer.Start(context.Background(), "Archmage-Overlord")
 	for i := 0; i < 10; i++ {
 		_, innerSpan := tracer.Start(ctx, fmt.Sprintf("Minion-%d", i))
 		log.Println("Minions hard at work, scribing...")
+		valuerecorder.Record(ctx, float64(i) * 1.5)
 		<-time.After(time.Second)
 		innerSpan.End()
 	}
