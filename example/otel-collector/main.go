1b212ebb0b7ab2c38e0aad70e8d2ac64df8f8984
William Tong <williamtong@google.com>
2020-06-15 18:42:58 +0000

    Tweak prometheus configs

--- example/otel-collector/main.go
+++ example/otel-collector/main.go
@@ -98,22 +98,24 @@ func main() {
 		result.Observe(1, commonLabels...)
 	}
 	_ = metric.Must(meter).NewFloat64ValueObserver(
-		"an.important.metric",
+		"an_important_metric",
 		metricCallback,
 		metric.WithDescription("A ValueObserver set to ... 1.0 it seems"),
 	)
 
 	// Recorder metric example
 	valuerecorder := metric.Must(meter).
-		NewFloat64ValueRecorder("another.important.metric").
+		NewFloat64ValueRecorder("another_important_metric").
 		Bind(commonLabels...)
 	defer valuerecorder.Unbind()
 
 	// work begins
 	ctx, span := tracer.Start(context.Background(), "CollectorExporter-Example")
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 1000; i++ {
 		_, iSpan := tracer.Start(ctx, fmt.Sprintf("Sample-%d", i))
 		log.Printf("Doing really hard work (%d / 10)\n", i + 1)
+		valuerecorder.Record(ctx, 2.5)
+
 		<-time.After(time.Second)
 		iSpan.End()
 	}
