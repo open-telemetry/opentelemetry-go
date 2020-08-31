1512f66c83b67014eca5c11fe03c79ed16920414
Andrei Ozerov <andrei.ozerov92@gmail.com>
2020-08-31 20:37:12 +0300

    Revert bridge/opentracing/internal/mock.go imports
    Do not change imports lines in bridge/opentracing/internal/mock.go.

--- bridge/opentracing/internal/mock.go
+++ bridge/opentracing/internal/mock.go
@@ -23,10 +23,11 @@ import (
 
 	otelcorrelation "go.opentelemetry.io/otel/api/correlation"
 	oteltrace "go.opentelemetry.io/otel/api/trace"
-	"go.opentelemetry.io/otel/bridge/opentracing/migration"
 	"go.opentelemetry.io/otel/codes"
 	otelparent "go.opentelemetry.io/otel/internal/trace/parent"
 	"go.opentelemetry.io/otel/label"
+
+	"go.opentelemetry.io/otel/bridge/opentracing/migration"
 )
 
 var (
