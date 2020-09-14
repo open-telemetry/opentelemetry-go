3de7a0708922da1871351c91712afbc27628b82d
JBD <jbd@google.com>
2020-09-14 16:35:21 -0700

    Merge otel into one file and add package doc (#1176)
    No need to split a tiny package into multiple files. Also adding package doc for the otel package.

--- doc.go
+++ doc.go
@@ -12,4 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package otel contains OpenTelemetry Go packages.
 package otel // import "go.opentelemetry.io/otel"
