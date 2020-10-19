6bc93b62fc334fb109e2b19f06ed12891b32a844
Alex Boten <aboten@lightstep.com>
2020-10-19 11:45:52 -0700

    cleanup comment

--- propagators/baggage.go
+++ propagators/baggage.go
@@ -24,8 +24,6 @@ import (
 	"go.opentelemetry.io/otel/label"
 )
 
-// Temporary header name until W3C finalizes format.
-// https://github.com/open-telemetry/opentelemetry-specification/blob/18b2752ebe6c7f0cdd8c7b2bcbdceb0ae3f5ad95/specification/correlationcontext/api.md#header-name
 const baggageHeader = "baggage"
 
 // Baggage is a propagator that supports the W3C Baggage format.
