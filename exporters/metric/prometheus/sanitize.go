e22a48de5a1353fb5450783ca9bb5edf651e9849
José Carlos Chávez <jcchavezs@gmail.com>
2020-11-04 18:10:58 +0100

    chore: adds vanity import for files that don't have it. (#1297)

--- exporters/metric/prometheus/sanitize.go
+++ exporters/metric/prometheus/sanitize.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package prometheus
+package prometheus // import "go.opentelemetry.io/otel/exporters/metric/prometheus"
 
 import (
 	"strings"
--- exporters/stdout/config.go
+++ exporters/stdout/config.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package stdout
+package stdout // import "go.opentelemetry.io/otel/exporters/stdout"
 
 import (
 	"io"
