ec5e2dca583d097e56f972e485679f4ff24d13fd
David Ashpole <dashpole@google.com>
2020-11-16 09:49:22 -0800

    Update bridge/opencensus/doc.go
    Co-authored-by: Tyler Yahn <MrAlias@users.noreply.github.com>

--- bridge/opencensus/doc.go
+++ bridge/opencensus/doc.go
@@ -12,4 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package opencensus provides a migration bridge forwarding the OpenCensus API to the OpenTelemetry SDK.
 package opencensus // import "go.opentelemetry.io/otel/bridge/opencensus"
