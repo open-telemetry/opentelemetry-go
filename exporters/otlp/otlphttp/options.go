40333fa10a2ed802f230a99e91da8d8500999fb6
Krzesimir Nowak <krzesimir@kinvolk.io>
2020-11-26 14:50:26 +0100

    Add an HTTP protocol driver for OTLP exporter
    Currently it supports only binary protobuf payloads.

---
+++ exporters/otlp/otlphttp/options.go
@@ -0,0 +1,142 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package otlphttp
+
+import (
+	"crypto/tls"
+	"time"
+)
+
+// Compression describes the compression used for payloads sent to the
+// collector.
+type Compression int
+
+const (
+	// NoCompression tells the driver to send payloads without
+	// compression.
+	NoCompression Compression = iota
+	// GzipCompression tells the driver to send payloads after
+	// compressing them with gzip.
+	GzipCompression
+)
+
+const (
+	// DefaultMaxAttempts describes how many times the driver
+	// should retry the sending of the payload in case of a
+	// retryable error.
+	DefaultMaxAttempts int = 5
+	// DefaultTracesPath is a default URL path for endpoint that
+	// receives spans.
+	DefaultTracesPath string = "/v1/traces"
+	// DefaultMetricsPath is a default URL path for endpoint that
+	// receives metrics.
+	DefaultMetricsPath string = "/v1/metrics"
+	// DefaultBackoff is a default base backoff time used in the
+	// exponential backoff strategy.
+	DefaultBackoff time.Duration = 300 * time.Millisecond
+)
+
+type config struct {
+	endpoint       string
+	compression    Compression
+	tracesURLPath  string
+	metricsURLPath string
+	maxAttempts    int
+	backoff        time.Duration
+	tlsCfg         *tls.Config
+	insecure       bool
+	headers        map[string]string
+}
+
+// Option applies an option to the HTTP driver.
+type Option func(cfg *config)
+
+// WithEndpoint allows one to set the address of the collector
+// endpoint that the driver will use to send metrics and spans. If
+// unset, it will instead try to use
+// DefaultCollectorHost:DefaultCollectorPort. Note that the endpoint
+// must not contain any URL path.
+func WithEndpoint(endpoint string) Option {
+	return func(cfg *config) {
+		cfg.endpoint = endpoint
+	}
+}
+
+// WithCompression tells the driver to compress the sent data.
+func WithCompression(compression Compression) Option {
+	return func(cfg *config) {
+		cfg.compression = compression
+	}
+}
+
+// WithTracesURLPath allows one to override the default URL path used
+// for sending traces. If unset, DefaultTracesPath will be used.
+func WithTracesURLPath(urlPath string) Option {
+	return func(cfg *config) {
+		cfg.tracesURLPath = urlPath
+	}
+}
+
+// WithMetricsURLPath allows one to override the default URL path used
+// for sending metrics. If unset, DefaultMetricsPath will be used.
+func WithMetricsURLPath(urlPath string) Option {
+	return func(cfg *config) {
+		cfg.metricsURLPath = urlPath
+	}
+}
+
+// WithMetricsURLPath allows one to override how many times the driver
+// will try to send the payload in case of retryable errors. If unset,
+// DefaultMaxAttempts will be used.
+func WithMaxAttempts(maxAttempts int) Option {
+	return func(cfg *config) {
+		cfg.maxAttempts = maxAttempts
+	}
+}
+
+// WithBackoff tells the driver to use the duration as a base of the
+// exponential backoff strategy. If unset, DefaultBackoff will be
+// used.
+func WithBackoff(duration time.Duration) Option {
+	return func(cfg *config) {
+		cfg.backoff = duration
+	}
+}
+
+// WithTLSClientConfig can be used to set up a custom TLS
+// configuration for the client used to send payloads to the
+// collector. Use it if you want to use a custom certificate.
+func WithTLSClientConfig(tlsCfg *tls.Config) Option {
+	return func(cfg *config) {
+		cfg.tlsCfg = tlsCfg
+	}
+}
+
+// WithInsecure tells the driver to connect to the collector using the
+// HTTP scheme, instead of HTTPS.
+func WithInsecure() Option {
+	return func(cfg *config) {
+		cfg.insecure = true
+	}
+}
+
+// WithHeaders allows one to tell the driver to send additional HTTP
+// headers with the payloads. Specifying headers like Content-Length,
+// Content-Encoding and Content-Type may result in a broken driver.
+func WithHeaders(headers map[string]string) Option {
+	return func(cfg *config) {
+		cfg.headers = headers
+	}
+}
