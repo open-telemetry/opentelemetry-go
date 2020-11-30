410096f60c141550969e1a999189d3affcac0663
Josh MacDonald <jmacd@lightstep.com>
2020-11-29 22:52:33 -0800

    Shorten the example

--- example/prom-collector/main.go
+++ example/prom-collector/main.go
@@ -16,10 +16,9 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
-	"sync"
 	"time"
 
 	"google.golang.org/grpc"
@@ -49,16 +48,11 @@ func initMeter() {
 
 	otlpExporter, err := otlp.NewExporter(ctx,
 		otlp.WithInsecure(),
-		otlp.WithAddress("127.0.0.1:7001"),
+		otlp.WithAddress("127.0.0.1:17001"),
 		otlp.WithGRPCDialOption(grpc.WithBlock()), // useful for testing
 	)
 	if err != nil {
 		log.Panic("could not initialize OTLP:", err)
-	}
-
-	options := []controller.Option{
-		controller.WithResource(res),
-		controller.WithExporter(otlpExporter),
 	}
 
 	cont := controller.New(
@@ -69,7 +63,8 @@ func initMeter() {
 			export.CumulativeExportKindSelector(),
 			processor.WithMemory(true),
 		),
-		options...,
+		controller.WithResource(res),
+		controller.WithExporter(otlpExporter),
 	)
 
 	cont.Start()
@@ -80,79 +75,45 @@ func initMeter() {
 	}
 	http.HandleFunc("/", promExporter.ServeHTTP)
 	go func() {
-		_ = http.ListenAndServe(":2222", nil)
+		log.Fatal(http.ListenAndServe(":17000", nil))
 	}()
 
 	otel.SetMeterProvider(cont.MeterProvider())
 
-	fmt.Println("Prometheus server running on :2222")
-	fmt.Println("Exporting OTLP to :7001")
+	log.Println("Prometheus server running on :17000")
+	log.Println("Exporting OTLP to :17001")
 }
 
 func main() {
 	initMeter()
 
-	meter := otel.Meter("ex.com/basic")
-	observerLock := new(sync.RWMutex)
-	observerValueToReport := new(float64)
-	observerLabelsToReport := new([]label.KeyValue)
-	cb := func(_ context.Context, result metric.Float64ObserverResult) {
-		(*observerLock).RLock()
-		value := *observerValueToReport
-		labels := *observerLabelsToReport
-		(*observerLock).RUnlock()
-		result.Observe(value, labels...)
+	labels := []label.KeyValue{
+		label.String("label1", "value1"),
 	}
-	_ = metric.Must(meter).NewFloat64ValueObserver("ex.com.one", cb,
-		metric.WithDescription("A ValueObserver set to 1.0"),
+
+	meter := otel.Meter("ex.com/prom-collector")
+	_ = metric.Must(meter).NewFloat64ValueObserver(
+		"randval",
+		func(_ context.Context, result metric.Float64ObserverResult) {
+			result.Observe(
+				rand.Float64(),
+				labels...,
+			)
+		},
+		metric.WithDescription("A random value"),
 	)
 
-	valuerecorder := metric.Must(meter).NewFloat64ValueRecorder("ex.com.two")
-	counter := metric.Must(meter).NewFloat64Counter("ex.com.three")
-
-	commonLabels := []label.KeyValue{label.Int("I", 10), label.String("A", "1"), label.String("B", "2"), label.String("C", "3")}
-	notSoCommonLabels := []label.KeyValue{label.Int("I", 13)}
+	temperature := metric.Must(meter).NewFloat64ValueRecorder("temperature")
+	interrupts := metric.Must(meter).NewInt64Counter("interrupts")
 
 	ctx := context.Background()
 
-	(*observerLock).Lock()
-	*observerValueToReport = 1.0
-	*observerLabelsToReport = commonLabels
-	(*observerLock).Unlock()
-	meter.RecordBatch(
-		ctx,
-		commonLabels,
-		valuerecorder.Measurement(2.0),
-		counter.Measurement(12.0),
-	)
+	log.Println("Example is running, please visit :17000")
 
-	time.Sleep(5 * time.Second)
+	for {
+		temperature.Record(ctx, 100+10*rand.NormFloat64(), labels...)
+		interrupts.Add(ctx, int64(rand.Intn(100)), labels...)
 
-	(*observerLock).Lock()
-	*observerValueToReport = 1.0
-	*observerLabelsToReport = notSoCommonLabels
-	(*observerLock).Unlock()
-	meter.RecordBatch(
-		ctx,
-		notSoCommonLabels,
-		valuerecorder.Measurement(2.0),
-		counter.Measurement(22.0),
-	)
-
-	time.Sleep(5 * time.Second)
-
-	(*observerLock).Lock()
-	*observerValueToReport = 13.0
-	*observerLabelsToReport = commonLabels
-	(*observerLock).Unlock()
-	meter.RecordBatch(
-		ctx,
-		commonLabels,
-		valuerecorder.Measurement(12.0),
-		counter.Measurement(13.0),
-	)
-
-	fmt.Println("Example finished updating, please visit :2222")
-
-	select {}
+		time.Sleep(time.Second * time.Duration(rand.Intn(10)))
+	}
 }
