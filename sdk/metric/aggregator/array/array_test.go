bcaff5fa23374fddce84069867aa3d14c264f696
Joshua MacDonald <jmacd@lightstep.com>
2021-01-01 23:13:06 -0800

    Use uint64 Count consistently

--- sdk/metric/aggregator/array/array_test.go
+++ sdk/metric/aggregator/array/array_test.go
@@ -42,7 +42,7 @@ func checkZero(t *testing.T, agg *Aggregator, desc *metric.Descriptor) {
 
 	count, err := agg.Count()
 	require.NoError(t, err)
-	require.Equal(t, int64(0), count)
+	require.Equal(t, uint64(0), count)
 
 	max, err := agg.Max()
 	require.True(t, errors.Is(err, aggregation.ErrNoData))
@@ -237,7 +237,7 @@ func TestArrayErrors(t *testing.T) {
 		require.NoError(t, agg.SynchronizedMove(ckpt, descriptor))
 
 		count, err := ckpt.Count()
-		require.Equal(t, int64(1), count, "NaN value was not counted")
+		require.Equal(t, uint64(1), count, "NaN value was not counted")
 		require.Nil(t, err)
 
 		num, err := ckpt.Quantile(0)
