21b17eef12483bcc6f6c6aa0aba17b1f927d4e4c
Punya Biswal <punya@google.com>
2021-01-21 11:23:20 -0500

    Remove inaccurate and unnecessary import comment
    Based on its module, it should be go.opencensus.io/otel/example/opencensus.
    However, it's simpler to get rid of it since main packages shouldn't be imported at all.

--- example/opencensus/main.go
+++ example/opencensus/main.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main // import "go.opentelemetry.io/otel/bridge/opencensus/examples/simple"
+package main
 
 import (
 	"context"
