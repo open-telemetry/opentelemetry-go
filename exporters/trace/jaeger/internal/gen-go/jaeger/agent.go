8a2384f94306e826de2bb3233a56c5840254a10c
Punya Biswal <punya@google.com>
2021-02-18 19:28:40 +0000

    Vendor Thrift dependency

--- exporters/trace/jaeger/internal/gen-go/jaeger/agent.go
+++ exporters/trace/jaeger/internal/gen-go/jaeger/agent.go
@@ -8,7 +8,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/apache/thrift/lib/go/thrift"
+	"go.opentelemetry.io/otel/exporters/trace/jaeger/internal/vendor/github.com/apache/thrift/lib/go/thrift"
 )
 
 // (needed to ensure safety because of naive import list construction.)
