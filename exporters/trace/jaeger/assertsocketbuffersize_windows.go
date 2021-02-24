9e0fcb75d00f6a84559fd7a9589104f9ec6f313d
Tyler Yahn <codingalias@gmail.com>
2021-02-23 18:40:40 -0800

    Blind succeed to account for unimplemented functionality on Windows

--- exporters/trace/jaeger/assertsocketbuffersize_windows.go
+++ exporters/trace/jaeger/assertsocketbuffersize_windows.go
@@ -18,22 +18,17 @@ package jaeger
 
 import (
 	"net"
-	"syscall"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func assertSockBufferSize(t *testing.T, expectedBytes int, conn *net.UDPConn) bool {
-	fd, err := conn.File()
-	if !assert.NoError(t, err) {
-		return false
-	}
-
-	bufferBytes, err := syscall.GetsockoptInt(syscall.Handle(fd.Fd()), syscall.SOL_SOCKET, syscall.SO_SNDBUF)
-	if !assert.NoError(t, err) {
-		return false
-	}
-
-	return assert.Equal(t, expectedBytes, bufferBytes)
+	// The Windows implementation of the net.UDPConn does not implement the
+	// functionality to return a file handle, instead a "not supported" error
+	// is returned:
+	//
+	// https://github.com/golang/go/blob/6cc8aa7ece96aca282db19f08aa5c98ed13695d9/src/net/fd_windows.go#L175-L178
+	//
+	// This means we are not able to pass the connection to a syscall and
+	// determine the buffer size.
+	return true
 }
