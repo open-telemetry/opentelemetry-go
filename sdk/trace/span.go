c4e58eaaf07a7fa4b53a501a8abe3294adc512de
Veera Pirla <17177346+veera83372@users.noreply.github.com>
2021-03-08 22:32:07 +0530

    Update sdk/trace/span.go
    Co-authored-by: Tyler Yahn <MrAlias@users.noreply.github.com>

--- sdk/trace/span.go
+++ sdk/trace/span.go
@@ -491,6 +491,8 @@ func (s *span) copyToCappedAttributes(attributes ...attribute.KeyValue) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	for _, a := range attributes {
+		// Ensure attributes conform to the specification:
+		// https://github.com/open-telemetry/opentelemetry-specification/blob/v1.0.1/specification/common/common.md#attributes
 		if a.Value.Type() != attribute.INVALID && a.Key != "" {
 			s.attributes.add(a)
 		}
