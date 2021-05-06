462720829593a015f4265eac141ca9c7d5c9044f
Nelson Germán Ghezzi <nelsonghezzi@users.noreply.github.com>
2021-05-06 19:49:59 -0300

    Linux implementation: avoid unquoting strings less than two chars

--- sdk/resource/os_release_unix.go
+++ sdk/resource/os_release_unix.go
@@ -99,6 +99,10 @@ func parse(line string) (string, string, bool) {
 // and, if so, returns a version of the string without them. Otherwise it returns
 // the provided string unchanged.
 func unquote(s string) string {
+	if len(s) < 2 {
+		return s
+	}
+
 	if (s[0] == '"' || s[0] == '\'') && s[0] == s[len(s)-1] {
 		return s[1 : len(s)-1]
 	}
