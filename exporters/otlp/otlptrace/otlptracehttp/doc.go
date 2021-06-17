4e492d3f13f58e1e9e77741bade6dd3201f0227e
Anthony J Mirabella <a9@aneurysm9.com>
2021-06-17 15:48:32 -0400

    Update godoc references to "pre-GA phase" for RC1 packages
    Signed-off-by: Anthony J Mirabella <a9@aneurysm9.com>

--- exporters/otlp/otlptrace/otlptracehttp/doc.go
+++ exporters/otlp/otlptrace/otlptracehttp/doc.go
@@ -16,9 +16,7 @@
 Package otlptracehttp a client that sends traces to the collector
 using HTTP with binary protobuf payloads.
 
-This package is currently in a pre-GA phase. Backwards incompatible
-changes may be introduced in subsequent minor version releases as we
-work to track the evolving OpenTelemetry specification and user
-feedback.
+This package is currently in a Release Candidate phase. Backwards incompatible changes
+may be introduced prior to v1.0.0, but we believe the current API is ready to stabilize.
 */
 package otlptracehttp // import "go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
