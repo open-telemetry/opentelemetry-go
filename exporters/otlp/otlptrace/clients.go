694c9a413da927dc999803c2a6f7f2a4a4465837
Tyler Yahn <MrAlias@users.noreply.github.com>
2021-06-18 14:56:11 +0000

    Interface stability documentation (#2012)
    * Interface stability documentation
    
    * Update versioning policy
    
    * Update CONTRIBUTING
    
    * Document how to extend immutable interfaces
    
    * Markdown format VERSIONING changes

--- exporters/otlp/otlptrace/clients.go
+++ exporters/otlp/otlptrace/clients.go
@@ -24,10 +24,16 @@ import (
 // transformation of data into wire format, and the transmission of that
 // data to the collector.
 type Client interface {
+	// DO NOT CHANGE: any modification will not be backwards compatible and
+	// must never be done outside of a new major release.
+
 	// Start should establish connection(s) to endpoint(s). It is
 	// called just once by the exporter, so the implementation
 	// does not need to worry about idempotence and locking.
 	Start(ctx context.Context) error
+	// DO NOT CHANGE: any modification will not be backwards compatible and
+	// must never be done outside of a new major release.
+
 	// Stop should close the connections. The function is called
 	// only once by the exporter, so the implementation does not
 	// need to worry about idempotence, but it may be called
@@ -36,8 +42,13 @@ type Client interface {
 	// synchronization point - after the function returns, the
 	// process of closing connections is assumed to be finished.
 	Stop(ctx context.Context) error
+	// DO NOT CHANGE: any modification will not be backwards compatible and
+	// must never be done outside of a new major release.
+
 	// UploadTraces should transform the passed traces to the wire
 	// format and send it to the collector. May be called
 	// concurrently.
 	UploadTraces(ctx context.Context, protoSpans []*tracepb.ResourceSpans) error
+	// DO NOT CHANGE: any modification will not be backwards compatible and
+	// must never be done outside of a new major release.
 }
