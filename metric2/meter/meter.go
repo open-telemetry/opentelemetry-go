822392ae0f128e0c7afff41574d3a1586ec5ee99
Joshua MacDonald <jmacd@lightstep.com>
2021-07-09 14:02:22 -0700

    comments

--- metric2/meter/meter.go
+++ metric2/meter/meter.go
@@ -4,22 +4,31 @@ import (
 	"context"
 
 	"go.opentelemetry.io/otel/attribute"
-	metric "go.opentelemetry.io/otel/metric2"
+	"go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/metric2/async"
 	"go.opentelemetry.io/otel/metric2/sync"
 )
 
+// MeterProvider supports creating named Meter instances, for
+// instrumenting an application containing multiple libraries of code.
 type MeterProvider interface {
 	Meter(instrumentationName string /*, opts ...MeterOption*/) Meter
 }
 
+// Meter is an instance of an OpenTelemetry metrics interface for an
+// individual named library of code.  This is the top-level entry
+// point for creating instruments.
 type Meter struct {
 }
 
+// Asynchronous provides access to an async.Meter for constructing
+// asynchronous metric instruments.
 func (m Meter) Asynchronous() async.Meter {
 	return async.Meter{}
 }
 
+// Synchronous provides access to a sync.Meter for constructing
+// synchronous metric instruments.
 func (m Meter) Synchronous() sync.Meter {
 	return sync.Meter{}
 }
