abe224371883209d4db1bc8e099750a5b7d8ebb5
Bhautik Pipaliya <56270044+bhautikpip@users.noreply.github.com>
2021-07-22 11:39:27 -0700

    API: create new linked span from current context (#2115)
    * API: create new linked span from current context
    
    * ran make precommit
    
    * Update trace/trace.go
    
    * Updated CHANGELOG
    
    Co-authored-by: Tyler Yahn <MrAlias@users.noreply.github.com>

--- trace/trace.go
+++ trace/trace.go
@@ -408,6 +408,14 @@ type Link struct {
 	DroppedAttributeCount int
 }
 
+// LinkFromContext returns a link encapsulating the SpanContext in the provided ctx.
+func LinkFromContext(ctx context.Context, attrs ...attribute.KeyValue) Link {
+	return Link{
+		SpanContext: SpanContextFromContext(ctx),
+		Attributes:  attrs,
+	}
+}
+
 // SpanKind is the role a Span plays in a Trace.
 type SpanKind int
 
