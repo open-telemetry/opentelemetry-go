cd14e0d55c73c64c768d459c2341f3a1b676bfb7
Tyler Yahn <codingalias@gmail.com>
2021-07-28 08:39:47 -0700

    Add unit tests for span

--- bridge/opencensus/internal/span_test.go
+++ bridge/opencensus/internal/span_test.go
@@ -17,6 +17,12 @@ package internal
 import (
 	"testing"
 
+	octrace "go.opencensus.io/trace"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/bridge/opencensus/internal/oc2otel"
+	"go.opentelemetry.io/otel/bridge/opencensus/internal/otel2oc"
+	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/trace"
 )
 
@@ -26,11 +32,21 @@ type span struct {
 	recording bool
 	ended     bool
 	sc        trace.SpanContext
+	name      string
+	sCode     codes.Code
+	sMsg      string
+	attrs     []attribute.KeyValue
+	eName     string
+	eOpts     []trace.EventOption
 }
 
-func (s *span) IsRecording() bool              { return s.recording }
-func (s *span) End(...trace.SpanEndOption)     { s.ended = true }
-func (s *span) SpanContext() trace.SpanContext { return s.sc }
+func (s *span) IsRecording() bool                         { return s.recording }
+func (s *span) End(...trace.SpanEndOption)                { s.ended = true }
+func (s *span) SpanContext() trace.SpanContext            { return s.sc }
+func (s *span) SetName(n string)                          { s.name = n }
+func (s *span) SetStatus(c codes.Code, d string)          { s.sCode, s.sMsg = c, d }
+func (s *span) SetAttributes(a ...attribute.KeyValue)     { s.attrs = a }
+func (s *span) AddEvent(n string, o ...trace.EventOption) { s.eName, s.eOpts = n, o }
 
 func TestSpanIsRecordingEvents(t *testing.T) {
 	s := &span{recording: true}
@@ -62,5 +78,190 @@ func TestSpanSpanContext(t *testing.T) {
 		TraceID: [16]byte{1},
 		SpanID:  [8]byte{1},
 	})
-	converted := util
+	// Do not test the conversion, only that the method is called.
+	converted := otel2oc.SpanContext(sc)
+
+	s := &span{sc: sc}
+	ocS := NewSpan(s)
+	if ocS.SpanContext() != converted {
+		t.Error("span.SpanContext did not use OpenTelemetry SpanContext")
+	}
+}
+
+func TestSpanSetName(t *testing.T) {
+	// OpenCensus does not set a name if not recording.
+	s := &span{recording: true}
+	ocS := NewSpan(s)
+	name := "test name"
+	ocS.SetName(name)
+	if s.name != name {
+		t.Error("span.SetName did not set OpenTelemetry span name")
+	}
+}
+
+func TestSpanSetStatus(t *testing.T) {
+	// OpenCensus does not set a status if not recording.
+	s := &span{recording: true}
+	ocS := NewSpan(s)
+
+	c, d := codes.Error, "error"
+	status := octrace.Status{Code: int32(c), Message: d}
+	ocS.SetStatus(status)
+
+	if s.sCode != c {
+		t.Error("span.SetStatus failed to set OpenTelemetry status code")
+	}
+	if s.sMsg != d {
+		t.Error("span.SetStatus failed to set OpenTelemetry status description")
+	}
+}
+
+func TestSpanAddAttributes(t *testing.T) {
+	attrs := []octrace.Attribute{
+		octrace.BoolAttribute("a", true),
+	}
+	// Do not test the conversion, only that the method is called.
+	converted := oc2otel.Attributes(attrs)
+
+	// OpenCensus does not set attributes if not recording.
+	s := &span{recording: true}
+	ocS := NewSpan(s)
+	ocS.AddAttributes(attrs...)
+
+	if len(s.attrs) != len(converted) || s.attrs[0] != converted[0] {
+		t.Error("span.AddAttributes failed to set OpenTelemetry attributes")
+	}
+}
+
+func TestSpanAnnotate(t *testing.T) {
+	name := "annotation"
+	attrs := []octrace.Attribute{
+		octrace.BoolAttribute("a", true),
+	}
+	// Do not test the conversion, only that the method is called.
+	want := oc2otel.Attributes(attrs)
+
+	// OpenCensus does not set attributes if not recording.
+	s := &span{recording: true}
+	ocS := NewSpan(s)
+	ocS.Annotate(attrs, name)
+
+	if s.eName != name {
+		t.Error("span.Annotate did not set event name")
+	}
+
+	got := trace.NewEventConfig(s.eOpts...).Attributes()
+	if len(want) != len(got) || want[0] != got[0] {
+		t.Error("span.Annotate did not set event options")
+	}
+}
+
+func TestSpanAnnotatef(t *testing.T) {
+	format := "annotation %s"
+	attrs := []octrace.Attribute{
+		octrace.BoolAttribute("a", true),
+	}
+	// Do not test the conversion, only that the method is called.
+	want := oc2otel.Attributes(attrs)
+
+	// OpenCensus does not set attributes if not recording.
+	s := &span{recording: true}
+	ocS := NewSpan(s)
+	ocS.Annotatef(attrs, format, "a")
+
+	if s.eName != "annotation a" {
+		t.Error("span.Annotatef did not set event name")
+	}
+
+	got := trace.NewEventConfig(s.eOpts...).Attributes()
+	if len(want) != len(got) || want[0] != got[0] {
+		t.Error("span.Annotatef did not set event options")
+	}
+}
+
+func TestSpanAddMessageSendEvent(t *testing.T) {
+	var u, c int64 = 1, 2
+
+	// OpenCensus does not set attributes if not recording.
+	s := &span{recording: true}
+	ocS := NewSpan(s)
+	ocS.AddMessageSendEvent(0, u, c)
+
+	if s.eName != MessageSendEvent {
+		t.Error("span.AddMessageSendEvent did not set event name")
+	}
+
+	got := trace.NewEventConfig(s.eOpts...).Attributes()
+	if len(got) != 2 {
+		t.Fatalf("span.AddMessageSendEvent set %d attributes, want 2", len(got))
+	}
+
+	want := attribute.KeyValue{Key: UncompressedKey, Value: attribute.Int64Value(u)}
+	if got[0] != want {
+		t.Errorf("span.AddMessageSendEvent wrong uncompressed attribute: %v", got[0])
+	}
+
+	want = attribute.KeyValue{Key: CompressedKey, Value: attribute.Int64Value(c)}
+	if got[1] != want {
+		t.Errorf("span.AddMessageSendEvent wrong compressed attribute: %v", got[1])
+	}
+}
+
+func TestSpanAddMessageReceiveEvent(t *testing.T) {
+	var u, c int64 = 3, 4
+
+	// OpenCensus does not set attributes if not recording.
+	s := &span{recording: true}
+	ocS := NewSpan(s)
+	ocS.AddMessageReceiveEvent(0, u, c)
+
+	if s.eName != MessageReceiveEvent {
+		t.Error("span.AddMessageReceiveEvent did not set event name")
+	}
+
+	got := trace.NewEventConfig(s.eOpts...).Attributes()
+	if len(got) != 2 {
+		t.Fatalf("span.AddMessageReceiveEvent set %d attributes, want 2", len(got))
+	}
+
+	want := attribute.KeyValue{Key: UncompressedKey, Value: attribute.Int64Value(u)}
+	if got[0] != want {
+		t.Errorf("span.AddMessageReceiveEvent wrong uncompressed attribute: %v", got[0])
+	}
+
+	want = attribute.KeyValue{Key: CompressedKey, Value: attribute.Int64Value(c)}
+	if got[1] != want {
+		t.Errorf("span.AddMessageReceiveEvent wrong compressed attribute: %v", got[1])
+	}
+}
+
+type handler struct{ err error }
+
+func (h *handler) Handle(e error) { h.err = e }
+
+func TestSpanAddLinkFails(t *testing.T) {
+	h := new(handler)
+	otel.SetErrorHandler(h)
+
+	// OpenCensus does not set attributes if not recording.
+	s := &span{recording: true}
+	ocS := NewSpan(s)
+	ocS.AddLink(octrace.Link{})
+
+	if h.err == nil {
+		t.Error("span.AddLink failed to raise an error")
+	}
+}
+
+func TestSpanString(t *testing.T) {
+	sc := trace.NewSpanContext(trace.SpanContextConfig{
+		TraceID: [16]byte{1},
+		SpanID:  [8]byte{1},
+	})
+
+	s := &span{sc: sc}
+	ocS := NewSpan(s)
+	if expected := "span 0100000000000000"; ocS.String() != expected {
+		t.Errorf("span.String = %q, not %q", ocS.String(), expected)
+	}
 }
