b5d55beb17026d6906a57e2045cc881a0002ee4e
José Carlos Chávez <jcchavezs@gmail.com>
2021-09-27 19:52:06 +0200

    chore(vanity-imports): adds vanity import support.

--- exporters/otlp/otlpmetric/otlpmetricgrpc/options.go
+++ exporters/otlp/otlpmetric/otlpmetricgrpc/options.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package otlpmetricgrpc
+package otlpmetricgrpc // import "go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
 
 import (
 	"fmt"
