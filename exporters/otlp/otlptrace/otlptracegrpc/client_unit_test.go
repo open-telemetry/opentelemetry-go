2ce320fc4fd3ba73c213cb26a8ba5cff9bd174ef
Tyler Yahn <codingalias@gmail.com>
2021-11-17 14:13:18 -0800

    Add unit tests for retryable and throttleDelay funcs

---
+++ exporters/otlp/otlptrace/otlptracegrpc/client_unit_test.go
@@ -0,0 +1,123 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package otlptracegrpc
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/durationpb"
+)
+
+func TestThrottleDuration(t *testing.T) {
+	c := codes.ResourceExhausted
+	testcases := []struct {
+		status   *status.Status
+		expected time.Duration
+	}{
+		{
+			status:   status.New(c, "no retry info"),
+			expected: 0,
+		},
+		{
+			status: func() *status.Status {
+				s, err := status.New(c, "single retry info").WithDetails(
+					&errdetails.RetryInfo{
+						RetryDelay: durationpb.New(15 * time.Millisecond),
+					},
+				)
+				require.NoError(t, err)
+				return s
+			}(),
+			expected: 15 * time.Millisecond,
+		},
+		{
+			status: func() *status.Status {
+				s, err := status.New(c, "error info").WithDetails(
+					&errdetails.ErrorInfo{Reason: "no throttle detail"},
+				)
+				require.NoError(t, err)
+				return s
+			}(),
+			expected: 0,
+		},
+		{
+			status: func() *status.Status {
+				s, err := status.New(c, "error and retry info").WithDetails(
+					&errdetails.ErrorInfo{Reason: "with throttle detail"},
+					&errdetails.RetryInfo{
+						RetryDelay: durationpb.New(13 * time.Minute),
+					},
+				)
+				require.NoError(t, err)
+				return s
+			}(),
+			expected: 13 * time.Minute,
+		},
+		{
+			status: func() *status.Status {
+				s, err := status.New(c, "double retry info").WithDetails(
+					&errdetails.RetryInfo{
+						RetryDelay: durationpb.New(13 * time.Minute),
+					},
+					&errdetails.RetryInfo{
+						RetryDelay: durationpb.New(15 * time.Minute),
+					},
+				)
+				require.NoError(t, err)
+				return s
+			}(),
+			expected: 13 * time.Minute,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.status.Message(), func(t *testing.T) {
+			require.Equal(t, tc.expected, throttleDelay(tc.status))
+		})
+	}
+}
+
+func TestRetryable(t *testing.T) {
+	retryableCodes := map[codes.Code]bool{
+		codes.OK:                 false,
+		codes.Canceled:           true,
+		codes.Unknown:            false,
+		codes.InvalidArgument:    false,
+		codes.DeadlineExceeded:   true,
+		codes.NotFound:           false,
+		codes.AlreadyExists:      false,
+		codes.PermissionDenied:   false,
+		codes.ResourceExhausted:  true,
+		codes.FailedPrecondition: false,
+		codes.Aborted:            true,
+		codes.OutOfRange:         true,
+		codes.Unimplemented:      false,
+		codes.Internal:           false,
+		codes.Unavailable:        true,
+		codes.DataLoss:           true,
+		codes.Unauthenticated:    false,
+	}
+
+	for c, want := range retryableCodes {
+		got, _ := retryable(status.Error(c, ""))
+		assert.Equalf(t, want, got, "evaluate(%s)", c)
+	}
+}
--- exporters/otlp/otlptrace/otlptracegrpc/go.mod
+++ exporters/otlp/otlptrace/otlptracegrpc/go.mod
@@ -11,6 +11,7 @@ require (
 	go.uber.org/goleak v1.1.12
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013
 	google.golang.org/grpc v1.42.0
+	google.golang.org/protobuf v1.27.1
 )
 
 replace go.opentelemetry.io/otel => ../../../..
