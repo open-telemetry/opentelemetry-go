8ec22429f80e85d12709c9b43297b66b7811985e
Joshua MacDonald <jmacd@lightstep.com>
2021-12-23 04:52:46 -0800

    more viewstate

--- sdk/metric/internal/viewstate/viewstate.go
+++ sdk/metric/internal/viewstate/viewstate.go
@@ -8,6 +8,9 @@ import (
 	"go.opentelemetry.io/otel/metric/sdkapi"
 	"go.opentelemetry.io/otel/metric/sdkapi/number"
 	"go.opentelemetry.io/otel/sdk/instrumentation"
+	"go.opentelemetry.io/otel/sdk/metric/aggregator/histogram"
+	"go.opentelemetry.io/otel/sdk/metric/aggregator/lastvalue"
+	"go.opentelemetry.io/otel/sdk/metric/aggregator/sum"
 	"go.opentelemetry.io/otel/sdk/metric/export"
 	"go.opentelemetry.io/otel/sdk/metric/export/aggregation"
 	"go.opentelemetry.io/otel/sdk/metric/views"
@@ -20,9 +23,10 @@ type (
 	}
 
 	CollectorFactory interface {
-		// Note allowed to modify input, beacuse the sync
-		// codepath has fingerprinted them as-is.
-		New(kvs []attribute.KeyValue) Collector
+		// New constructs a new collector.  This is not
+		// allowed to modify input, beacuse the sync codepath
+		// has fingerprinted them as-is.
+		New(kvs []attribute.KeyValue, desc *sdkapi.Descriptor) Collector
 	}
 
 	State struct {
@@ -43,14 +47,15 @@ type (
 	}
 
 	viewCollectorFactory struct {
-		state   *State
-		aggSet  map[aggregation.Kind]viewMatchState
-		matches []views.View
+		state  *State
+		aggSet map[aggregation.Kind]viewMatchState
 	}
 
 	viewMatchState struct {
-		// @@@ something to encode the number of distinct pipelines
-		// that receive aggregations of a particular kind.
+		matches []viewMatch
+	}
+
+	viewMatch struct {
 	}
 )
 
@@ -84,6 +89,14 @@ func New(lib instrumentation.Library, defs []views.View, hasDefault bool) *State
 	}
 }
 
+func configViewMatch(v views.View) viewMatch {
+	return viewMatch{}
+}
+
+func defaultViewMatch(k sdkapi.InstrumentKind) viewMatch {
+	return viewMatch{}
+}
+
 // NewFactory is called during NewInstrument by the Meter
 // implementation, the result saved in the instrument and used to
 // construct new Collectors throughout its lifetime.
@@ -92,62 +105,66 @@ func (v *State) NewFactory(desc sdkapi.Descriptor) (CollectorFactory, error) {
 	// the aggregator/exporter too, but we're not configuring them yet.
 
 	// Compute the set of matching views.
-	var matches []views.View
+	aggSet := map[aggregation.Kind]viewMatchState{}
 	for _, def := range v.definitions {
-		if def.Matches(v.library, desc) {
-			matches = append(matches, def)
+		if !def.Matches(v.library, desc) {
+			continue
 		}
-	}
 
-	// Compute the set of requested aggregations.
-	aggSet := map[aggregation.Kind]int{}
-	for _, match := range matches {
-		va := match.Aggregation()
-		if va == "" {
-			aggSet[aggregationKindFor(desc.InstrumentKind())]++
-		} else {
-			aggSet[va]++
+		var kind aggregation.Kind
+		switch def.Aggregation() {
+		case aggregation.SumKind, aggregation.HistogramKind, aggregation.LastValueKind:
+			kind = def.Aggregation()
+		default:
+			kind = aggregationKindFor(desc.InstrumentKind())
 		}
+		current := aggSet[kind]
+		current.matches = append(current.matches, configViewMatch(def))
+		aggSet[kind] = current
 	}
 	// If there were no matching views, set the default aggregation.
-	if matches == nil {
+	if len(aggSet) == 0 {
 		if !v.hasDefault {
 			return nil, nil
 		}
-		aggSet[aggregationKindFor(desc.InstrumentKind())] = struct{}{}
+		k := desc.InstrumentKind()
+		aggSet[aggregationKindFor(k)] = viewMatchState{
+			matches: []viewMatch{defaultViewMatch(k)},
+		}
 	}
 
 	// Develop the list of basic aggregations.
 	vcf := &viewCollectorFactory{
-		state:   v,
-		aggSet:  aggSet,
-		matches: matches,
+		state:  v,
+		aggSet: aggSet,
 	}
 
 	// Reconcile.  Build a representation of the output names.
 	v.lock.Lock()
 	defer v.lock.Unlock()
 
-	for _, match := range matches {
-		var outputName string
-		if match.HasName() {
-			outputName = match.Name()
-		} else {
-			outputName = desc.Name()
-		}
+	for _, vms := range vcf.aggSet {
+		for _, vm := range vms.matches {
+			var outputName string
+			if vm.HasName() {
+				outputName = vm.Name()
+			} else {
+				outputName = desc.Name()
+			}
 
-		if _, has := v.output[outputName]; has {
-			return nil, fmt.Errorf("duplicate view name configured")
+			if _, has := v.output[outputName]; has {
+				return nil, fmt.Errorf("duplicate view name configured")
+			}
+			v.output[outputName] = vcf
 		}
-		v.output[outputName] = vcf
 	}
 
 	return vcf, nil
 }
 
-func (v *viewCollectorFactory) New(kvs []attribute.KeyValue) Collector {
-	var aggs []export.Aggregator
-	for _, ak := range v.akinds {
+func (v *viewCollectorFactory) New(kvs []attribute.KeyValue, desc *sdkapi.Descriptor) Collector {
+	var inputs []export.Aggregator
+	for ak, vms := range v.aggSet {
 		// Notes:
 		//
 		// Need to know how many aggregators are needed to construct
@@ -155,26 +172,34 @@ func (v *viewCollectorFactory) New(kvs []attribute.KeyValue) Collector {
 		//
 		// 1 is the base case, for the collector itself
 		// 1 for each matching view
+		cnt := 1 + len(vms.matches)
 
-		var agg export.Aggregator
 		switch ak {
 		case aggregation.SumKind:
+			aggs := sum.New(cnt)
+			inputs = append(inputs, &aggs[0])
 		case aggregation.LastValueKind:
+			aggs := lastvalue.New(cnt)
+			inputs = append(inputs, &aggs[0])
 		case aggregation.HistogramKind:
+			aggs := histogram.New(cnt, desc)
+			inputs = append(inputs, &aggs[0])
 		}
-		aggs = append(aggs, agg)
 	}
-	return &viewCollector{}
+	return &viewCollector{
+		inputs: inputs,
+	}
 }
 
-func (v *viewCollector) Update(number number.Number, descriptor *sdkapi.Descriptor) {
+func (v *viewCollector) Update(number number.Number, desc *sdkapi.Descriptor) {
 	for _, input := range v.inputs {
-		input.Update(number, descriptor)
+		input.Update(number, desc)
 	}
 }
 
 func (v *viewCollector) Send(final bool) error {
 	// @@@ Call SynchronizedMove on each input
+	// @@@ Does _final_ matter?
 
 	return nil
 }
