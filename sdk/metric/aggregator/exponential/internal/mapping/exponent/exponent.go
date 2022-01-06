ef19ae0c9ff95d1b427a5cff79cb6fd2d8ceb59b
Joshua MacDonald <jmacd@lightstep.com>
2022-01-06 15:33:44 -0800

    vanity

--- sdk/metric/aggregator/exponential/internal/mapping/exponent/exponent.go
+++ sdk/metric/aggregator/exponential/internal/mapping/exponent/exponent.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package exponent
+package exponent // import "go.opentelemetry.io/otel/sdk/metric/aggregator/exponential/internal/mapping/exponent"
 
 import (
 	"fmt"
--- sdk/metric/aggregator/exponential/internal/mapping/mapping.go
+++ sdk/metric/aggregator/exponential/internal/mapping/mapping.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package mapping
+package mapping // import "go.opentelemetry.io/otel/sdk/metric/aggregator/exponential/internal/mapping"
 
 import "fmt"
 
