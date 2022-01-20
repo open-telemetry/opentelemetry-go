5ac033cb85168daa93c21e5c4b3c7085acf3350a
Joshua MacDonald <jmacd@lightstep.com>
2022-01-20 14:06:10 -0800

    mapping interface comments

--- sdk/metric/aggregator/exponential/mapping/mapping.go
+++ sdk/metric/aggregator/exponential/mapping/mapping.go
@@ -16,17 +16,33 @@ package mapping // import "go.opentelemetry.io/otel/sdk/metric/aggregator/expone
 
 import "fmt"
 
-// Mapping is the interface of a mapper.
+// Mapping is the interface of an exponential histogram mapper.
 type Mapping interface {
 	// MapToIndex maps positive floating point values to indexes
 	// corresponding to Scale().  Implementations are not expected
 	// to handle zeros, +Inf, NaN, or negative values.
 	MapToIndex(value float64) int32
+
+	// LowerBoundary returns the lower boundary of a given bucket
+	// index.  The index is expected to map onto a range that is
+	// at least partially inside the range of normalized floating
+	// point values.  If the corresponding bucket's upper boundary
+	// is less than or equal to 0x1p-1022, ErrUnderflow will be
+	// returned.  If the corresponding bucket's lower boundary is
+	// greater than math.MaxFloat64, ErrOverflow will be returned.
 	LowerBoundary(index int32) (float64, error)
+
+	// Scale returns the parameter that controls the resolution of
+	// this mapping.  For details see:
+	// https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/metrics/datamodel.md#exponential-scale
 	Scale() int32
 }
 
 var (
+	// ErrUnderflow is returned when computing the lower boundary
+	// of an index that maps into a denormalized floating point value.
 	ErrUnderflow = fmt.Errorf("underflow")
-	ErrOverflow  = fmt.Errorf("overflow")
+	// ErrOverflow is returned when computing the lower boundary
+	// of an index that maps into +Inf.
+	ErrOverflow = fmt.Errorf("overflow")
 )
