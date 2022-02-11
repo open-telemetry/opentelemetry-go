ab815ff5ccbc77b3c85bc750ce286023cef3f788
1046102779 <seachen@tencent.com>
2022-02-11 18:58:47 +0800

    feature/exporter: add Drop Counts for oltptracer's event and link
    Signed-off-by: 1046102779 <seachen@tencent.com>

--- exporters/otlp/otlptrace/internal/tracetransform/span.go
+++ exporters/otlp/otlptrace/internal/tracetransform/span.go
@@ -181,23 +181,16 @@ func spanEvents(es []tracesdk.Event) []*tracepb.Span_Event {
 	if evCount > maxEventsPerSpan {
 		evCount = maxEventsPerSpan
 	}
-	events := make([]*tracepb.Span_Event, 0, evCount)
-	nEvents := 0
+	events := make([]*tracepb.Span_Event, evCount)
 
 	// Transform message events
-	for _, e := range es {
-		if nEvents >= maxEventsPerSpan {
-			break
+	for i := 0; i < evCount; i++ {
+		events[i] = &tracepb.Span_Event{
+			Name:                   es[i].Name,
+			TimeUnixNano:           uint64(es[i].Time.UnixNano()),
+			Attributes:             KeyValues(es[i].Attributes),
+			DroppedAttributesCount: uint32(es[i].DroppedAttributeCount),
 		}
-		nEvents++
-		events = append(events,
-			&tracepb.Span_Event{
-				Name:                   e.Name,
-				TimeUnixNano:           uint64(e.Time.UnixNano()),
-				Attributes:             KeyValues(e.Attributes),
-				DroppedAttributesCount: uint32(e.DroppedAttributeCount),
-			},
-		)
 	}
 
 	return events
