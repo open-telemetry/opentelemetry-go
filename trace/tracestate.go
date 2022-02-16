48271581cca222a070791f6520498facea27cf11
1046102779 <seachen@tencent.com>
2022-02-16 09:21:14 +0800

    fix(tracestate): drop right-most member in tracestate
    Signed-off-by: 1046102779 <seachen@tencent.com>

--- trace/tracestate.go
+++ trace/tracestate.go
@@ -171,7 +171,7 @@ func (ts TraceState) Get(key string) string {
 // specification an error is returned with the original TraceState.
 //
 // If adding a new list-member means the TraceState would have more members
-// than is allowed an error is returned instead with the original TraceState.
+// then it will drop the right-most member and return an nil error when capacity is exceeded.
 func (ts TraceState) Insert(key, value string) (TraceState, error) {
 	m, err := newMember(key, value)
 	if err != nil {
