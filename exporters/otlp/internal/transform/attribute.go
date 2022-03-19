cac60a83550785c53d00f7d2dbc52b5180890efb
csuzhang <csuzhang@tencent.com>
2022-03-20 06:52:58 +0800

    fix vanity import path

--- exporters/otlp/internal/transform/attribute.go
+++ exporters/otlp/internal/transform/attribute.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package transform // import "go.opentelemetry.io/otel/exporters/otlp/transform"
+package transform // import "go.opentelemetry.io/otel/exporters/otlp/internal/transform"
 
 import (
 	"go.opentelemetry.io/otel/attribute"
