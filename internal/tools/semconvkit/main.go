12ad7e649a471417e97dcddb384f737e6c826fad
Tyler Yahn <codingalias@gmail.com>
2022-03-31 15:33:58 -0700

    Update template render

--- internal/tools/semconvkit/main.go
+++ internal/tools/semconvkit/main.go
@@ -5,55 +5,72 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"text/template"
 )
 
 var (
-	out     = flag.String("output", "./", "output directory")
-	version = flag.String("version", "", "semantic convention version")
-
-	templates      = template.Must(template.ParseGlob("templates/*"))
-	templateToFile = map[string]string{
-		"doc.go.tmpl": "doc.go",
-	}
+	out = flag.String("output", "./", "output directory")
+	tag = flag.String("tag", "", "OpenTelemetry tagged version")
 )
 
 // SemanticConventions are information about the semantic conventions being
 // generated.
 type SemanticConventions struct {
-	Version string
+	// SemVer is the semantic version (i.e. 1.7.0 and not v1.7.0).
+	SemVer string
+	// TagVer is the tagged version (i.e. v1.7.0 and not 1.7.0).
+	TagVer string
+}
+
+func render(dest string, sc *SemanticConventions) error {
+	const templateDir = "templates/"
+
+	f, err := os.Open(templateDir)
+	if err != nil {
+		return err
+	}
+	files, err := f.Readdir(0)
+	if err != nil {
+		return err
+	}
+	if err = f.Close(); err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		path := filepath.Join(templateDir, file.Name())
+		tmpl, err := template.ParseFiles(path)
+		if err != nil {
+			return err
+		}
+
+		target := filepath.Join(dest, strings.TrimSuffix(file.Name(), ".tmpl"))
+		wr, err := os.Create(target)
+		if err != nil {
+			return err
+		}
+
+		tmpl.Execute(wr, sc)
+	}
+
+	return nil
 }
 
 func main() {
 	flag.Parse()
 
-	if *version == "" {
-		log.Fatalf("invalid version: %q", *version)
+	if *tag == "" {
+		log.Fatalf("invalid tag: %q", *tag)
 	}
 
 	sc := &SemanticConventions{
-		Version: *version,
+		SemVer: strings.TrimPrefix(*tag, "v"),
+		TagVer: *tag,
 	}
 
-	templates, err := template.ParseGlob("./templates/*")
-	if err != nil {
+	if err := render(*out, sc); err != nil {
 		log.Fatal(err)
-	}
-
-	if _, err := os.Stat(*out); os.IsNotExist(err) {
-		err = os.Mkdir(*out, os.ModeDir)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	for tmpl, fName := range templateToFile {
-		path := filepath.Join(*out, fName)
-		fWriter, err := os.Create(path)
-		if err != nil {
-			log.Fatal(err)
-		}
-		templates.ExecuteTemplate(fWriter, tmpl, sc)
 	}
 }
