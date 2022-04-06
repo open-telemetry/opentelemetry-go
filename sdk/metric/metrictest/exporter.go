292d175897864cb4a0d4f4fcffc713f6df67d5f3
Aaron Clawson <3766680+MadVikingGod@users.noreply.github.com>
2022-04-06 16:26:18 +0000

    in memory exporter

---
+++ sdk/metric/metrictest/exporter.go
@@ -0,0 +1,170 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package metrictest // import "go.opentelemetry.io/otel/sdk/metric/metrictest"
+
+import (
+	"context"
+	"fmt"
+
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/sdk/instrumentation"
+	controller "go.opentelemetry.io/otel/sdk/metric/controller/basic"
+	"go.opentelemetry.io/otel/sdk/metric/export"
+	"go.opentelemetry.io/otel/sdk/metric/export/aggregation"
+	"go.opentelemetry.io/otel/sdk/metric/number"
+	processor "go.opentelemetry.io/otel/sdk/metric/processor/basic"
+	selector "go.opentelemetry.io/otel/sdk/metric/selector/simple"
+)
+
+type Exporter struct {
+	exports []ExportRecord
+	// resource *resource.Resource
+
+	controller *controller.Controller
+}
+
+type ExportRecord struct {
+	InstrumentName         string
+	InstrumentationLibrary Library
+	Labels                 []attribute.KeyValue
+	AggregationKind        aggregation.Kind
+	Sum                    number.Number
+	Count                  uint64
+	Histogram              aggregation.Buckets
+	LastValue              number.Number
+}
+
+// Export is called immediately after completing a collection
+// pass in the SDK.
+//
+// The Context comes from the controller that initiated
+// collection.
+//
+// The InstrumentationLibraryReader interface refers to the
+// Processor that just completed collection.
+func (e *Exporter) Collect(ctx context.Context) error {
+	e.exports = []ExportRecord{}
+
+	e.controller.Collect(ctx)
+
+	e.controller.ForEach(func(l instrumentation.Library, r export.Reader) error {
+		lib := Library{
+			InstrumentationName:    l.Name,
+			InstrumentationVersion: l.Version,
+			SchemaURL:              l.SchemaURL,
+		}
+
+		r.ForEach(aggregation.CumulativeTemporalitySelector(), func(rec export.Record) error {
+			record := ExportRecord{
+				InstrumentName:         rec.Descriptor().Name(),
+				InstrumentationLibrary: lib,
+				Labels:                 rec.Labels().ToSlice(),
+				AggregationKind:        rec.Aggregation().Kind(),
+			}
+
+			var err error
+			switch agg := rec.Aggregation().(type) {
+			case aggregation.Histogram:
+				record.AggregationKind = aggregation.HistogramKind
+				record.Histogram, err = agg.Histogram()
+				if err != nil {
+					return err
+				}
+				record.Sum, err = agg.Sum()
+				if err != nil {
+					return err
+				}
+				record.Count, err = agg.Count()
+				if err != nil {
+					return err
+				}
+			case aggregation.Count:
+				record.Count, err = agg.Count()
+				if err != nil {
+					return err
+				}
+			case aggregation.LastValue:
+				record.LastValue, _, err = agg.LastValue()
+				if err != nil {
+					return err
+				}
+			case aggregation.Sum:
+				record.Sum, err = agg.Sum()
+				if err != nil {
+					return err
+				}
+			}
+
+			e.exports = append(e.exports, record)
+			return nil
+		})
+		return nil
+	})
+	return nil
+}
+
+func (e *Exporter) GetRecords() []ExportRecord {
+	return e.exports
+}
+
+var ErrNotFound = fmt.Errorf("record not found")
+
+func (e *Exporter) GetByName(name string) (ExportRecord, error) {
+	for _, rec := range e.exports {
+		if rec.InstrumentName == name {
+			return rec, nil
+		}
+	}
+	return ExportRecord{}, ErrNotFound
+}
+
+func (e *Exporter) GetByNameAndLabels(name string, labels []attribute.KeyValue) (ExportRecord, error) {
+	for _, rec := range e.exports {
+		if rec.InstrumentName == name && labelsMatch(labels, rec.Labels) {
+			return rec, nil
+		}
+	}
+	return ExportRecord{}, ErrNotFound
+}
+
+func labelsMatch(labelsA, labelsB []attribute.KeyValue) bool {
+	if len(labelsA) != len(labelsB) {
+		return false
+	}
+	for i := range labelsA {
+		if labelsA[i] != labelsB[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func NewMeterProvider() (metric.MeterProvider, *Exporter) {
+	c := controller.New(
+		processor.NewFactory(
+			selector.NewWithHistogramDistribution(),
+			aggregation.CumulativeTemporalitySelector(),
+		),
+		controller.WithCollectPeriod(0),
+	)
+	exp := &Exporter{
+
+		controller: c,
+	}
+
+	return c, exp
+}
---
+++ sdk/metric/metrictest/exporter_test.go
@@ -0,0 +1,211 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package metrictest_test // import "go.opentelemetry.io/otel/sdk/metric/metrictest"
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric/instrument"
+	"go.opentelemetry.io/otel/sdk/metric/export/aggregation"
+	"go.opentelemetry.io/otel/sdk/metric/metrictest"
+)
+
+func TestSyncCounter(t *testing.T) {
+	ctx := context.Background()
+	mp, exp := metrictest.NewMeterProvider()
+	meter := mp.Meter("go.opentelemetry.io/otel/sdk/metric/metrictest/exporter_TestSyncCounter")
+
+	fcnt, err := meter.SyncFloat64().Counter("fCount")
+	require.NoError(t, err)
+	fudcnt, err := meter.SyncFloat64().UpDownCounter("fUDCount")
+	require.NoError(t, err)
+	fhis, err := meter.SyncFloat64().Histogram("fHist")
+	require.NoError(t, err)
+
+	icnt, err := meter.SyncInt64().Counter("iCount")
+	require.NoError(t, err)
+	iudcnt, err := meter.SyncInt64().UpDownCounter("iUDCount")
+	require.NoError(t, err)
+	ihis, err := meter.SyncInt64().Histogram("iHist")
+	require.NoError(t, err)
+
+	fcnt.Add(ctx, 2)
+	fudcnt.Add(ctx, 3)
+	fhis.Record(ctx, 4)
+	fhis.Record(ctx, 5)
+
+	icnt.Add(ctx, 22)
+	iudcnt.Add(ctx, 23)
+	ihis.Record(ctx, 24)
+	ihis.Record(ctx, 25)
+
+	err = exp.Collect(context.Background())
+	assert.NoError(t, err)
+
+	out, err := exp.GetByName("fCount")
+	assert.NoError(t, err)
+	assert.InDelta(t, 2.0, out.Sum.AsFloat64(), 0.0001)
+	assert.Equal(t, aggregation.SumKind, out.AggregationKind)
+
+	out, err = exp.GetByName("fUDCount")
+	assert.NoError(t, err)
+	assert.InDelta(t, 3.0, out.Sum.AsFloat64(), 0.0001)
+	assert.Equal(t, aggregation.SumKind, out.AggregationKind)
+
+	out, err = exp.GetByName("fHist")
+	assert.NoError(t, err)
+	assert.InDelta(t, 9.0, out.Sum.AsFloat64(), 0.0001)
+	assert.EqualValues(t, 2, out.Count)
+	assert.Equal(t, aggregation.HistogramKind, out.AggregationKind)
+
+	out, err = exp.GetByName("iCount")
+	assert.NoError(t, err)
+	assert.EqualValues(t, 22, out.Sum.AsInt64())
+	assert.Equal(t, aggregation.SumKind, out.AggregationKind)
+
+	out, err = exp.GetByName("iUDCount")
+	assert.NoError(t, err)
+	assert.EqualValues(t, 23, out.Sum.AsInt64())
+	assert.Equal(t, aggregation.SumKind, out.AggregationKind)
+
+	out, err = exp.GetByName("iHist")
+	assert.NoError(t, err)
+	assert.EqualValues(t, 49, out.Sum.AsInt64())
+	assert.EqualValues(t, 2, out.Count)
+	assert.Equal(t, aggregation.HistogramKind, out.AggregationKind)
+}
+
+func TestAsyncCounter(t *testing.T) {
+	ctx := context.Background()
+	mp, exp := metrictest.NewMeterProvider()
+	meter := mp.Meter("go.opentelemetry.io/otel/sdk/metric/metrictest/exporter_TestSyncCounter")
+
+	fcnt, err := meter.AsyncFloat64().Counter("fCount")
+	require.NoError(t, err)
+	fudcnt, err := meter.AsyncFloat64().UpDownCounter("fUDCount")
+	require.NoError(t, err)
+	fgauge, err := meter.AsyncFloat64().Gauge("fGauge")
+	require.NoError(t, err)
+
+	icnt, err := meter.AsyncInt64().Counter("iCount")
+	require.NoError(t, err)
+	iudcnt, err := meter.AsyncInt64().UpDownCounter("iUDCount")
+	require.NoError(t, err)
+	igauge, err := meter.AsyncInt64().Gauge("iGauge")
+	require.NoError(t, err)
+
+	meter.RegisterCallback(
+		[]instrument.Asynchronous{
+			fcnt,
+			fudcnt,
+			fgauge,
+			icnt,
+			iudcnt,
+			igauge,
+		}, func(context.Context) {
+			fcnt.Observe(ctx, 2)
+			fudcnt.Observe(ctx, 3)
+			fgauge.Observe(ctx, 4)
+			icnt.Observe(ctx, 22)
+			iudcnt.Observe(ctx, 23)
+			igauge.Observe(ctx, 25)
+		})
+
+	err = exp.Collect(context.Background())
+	assert.NoError(t, err)
+
+	out, err := exp.GetByName("fCount")
+	assert.NoError(t, err)
+	assert.InDelta(t, 2.0, out.Sum.AsFloat64(), 0.0001)
+	assert.Equal(t, aggregation.SumKind, out.AggregationKind)
+
+	out, err = exp.GetByName("fUDCount")
+	assert.NoError(t, err)
+	assert.InDelta(t, 3.0, out.Sum.AsFloat64(), 0.0001)
+	assert.Equal(t, aggregation.SumKind, out.AggregationKind)
+
+	out, err = exp.GetByName("fGauge")
+	assert.NoError(t, err)
+	assert.InDelta(t, 4.0, out.LastValue.AsFloat64(), 0.0001)
+	assert.Equal(t, aggregation.LastValueKind, out.AggregationKind)
+
+	out, err = exp.GetByName("iCount")
+	assert.NoError(t, err)
+	assert.EqualValues(t, 22, out.Sum.AsInt64())
+	assert.Equal(t, aggregation.SumKind, out.AggregationKind)
+
+	out, err = exp.GetByName("iUDCount")
+	assert.NoError(t, err)
+	assert.EqualValues(t, 23, out.Sum.AsInt64())
+	assert.Equal(t, aggregation.SumKind, out.AggregationKind)
+
+	out, err = exp.GetByName("iGauge")
+	assert.NoError(t, err)
+	assert.EqualValues(t, 25, out.LastValue.AsInt64())
+	assert.Equal(t, aggregation.LastValueKind, out.AggregationKind)
+}
+
+func ExampleExporter_GetByName() {
+	mp, exp := metrictest.NewMeterProvider()
+	meter := mp.Meter("go.opentelemetry.io/otel/sdk/metric/metrictest/exporter_TestSyncCounter")
+
+	cnt, err := meter.SyncFloat64().Counter("fCount")
+	if err != nil {
+		panic("could not acquire counter")
+	}
+
+	cnt.Add(context.Background(), 2.5)
+
+	err = exp.Collect(context.Background())
+	if err != nil {
+		panic("collection failed")
+	}
+
+	out, _ := exp.GetByName("fCount")
+
+	fmt.Println(out.Sum.AsFloat64())
+	// Output: 2.5
+}
+
+func ExampleExporter_GetByNameAndLabels() {
+	mp, exp := metrictest.NewMeterProvider()
+	meter := mp.Meter("go.opentelemetry.io/otel/sdk/metric/metrictest/exporter_TestSyncCounter")
+
+	cnt, err := meter.SyncFloat64().Counter("fCount")
+	if err != nil {
+		panic("could not acquire counter")
+	}
+
+	cnt.Add(context.Background(), 4, attribute.String("foo", "bar"))
+
+	err = exp.Collect(context.Background())
+	if err != nil {
+		panic("collection failed")
+	}
+
+	out, err := exp.GetByNameAndLabels("fCount", []attribute.KeyValue{attribute.String("foo", "bar")})
+	if err != nil {
+		println(err.Error())
+	}
+
+	fmt.Println(out.Sum.AsFloat64())
+	// Output: 4
+
+}
