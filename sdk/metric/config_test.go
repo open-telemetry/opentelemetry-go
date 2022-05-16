335fa4681a66a1527ef576682351f5492f4b3c41
Tyler Yahn <codingalias@gmail.com>
2022-05-16 14:12:44 -0700

    Run lint

--- sdk/metric/config_test.go
+++ sdk/metric/config_test.go
@@ -21,6 +21,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
 	"go.opentelemetry.io/otel/sdk/metric/export"
 	"go.opentelemetry.io/otel/sdk/resource"
 )
