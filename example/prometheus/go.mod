5f3e802ab4e3c4cdbd8e60625d2eec9bff244e85
csuzhang <csuzhang@tencent.com>
2022-05-20 15:35:10 +0800

    add back prometheus example code

---
+++ example/prometheus/go.mod
@@ -0,0 +1,10 @@
+module go.opentelemetry.io/otel/example/prometheus
+
+go 1.16
+
+require (
+	go.opentelemetry.io/otel v1.7.0
+	go.opentelemetry.io/otel/exporters/prometheus v0.30.0
+	go.opentelemetry.io/otel/metric v0.30.0
+	go.opentelemetry.io/otel/sdk/metric v0.30.0
+)
---
+++ example/prometheus/main.go
@@ -0,0 +1,132 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/prometheus"
+	"go.opentelemetry.io/otel/metric/global"
+	"go.opentelemetry.io/otel/metric/instrument"
+	"go.opentelemetry.io/otel/sdk/metric/aggregator/histogram"
+	controller "go.opentelemetry.io/otel/sdk/metric/controller/basic"
+	"go.opentelemetry.io/otel/sdk/metric/export/aggregation"
+	processor "go.opentelemetry.io/otel/sdk/metric/processor/basic"
+	selector "go.opentelemetry.io/otel/sdk/metric/selector/simple"
+)
+
+var (
+	lemonsKey = attribute.Key("ex.com/lemons")
+)
+
+func initMeter() {
+	config := prometheus.Config{
+		DefaultHistogramBoundaries: []float64{1, 2, 5, 10, 20, 50},
+	}
+	c := controller.New(
+		processor.NewFactory(
+			selector.NewWithHistogramDistribution(
+				histogram.WithExplicitBoundaries(config.DefaultHistogramBoundaries),
+			),
+			aggregation.CumulativeTemporalitySelector(),
+			processor.WithMemory(true),
+		),
+	)
+	exporter, err := prometheus.New(config, c)
+	if err != nil {
+		log.Panicf("failed to initialize prometheus exporter %v", err)
+	}
+
+	global.SetMeterProvider(exporter.MeterProvider())
+
+	http.HandleFunc("/", exporter.ServeHTTP)
+	go func() {
+		_ = http.ListenAndServe(":2222", nil)
+	}()
+
+	fmt.Println("Prometheus server running on :2222")
+}
+
+func main() {
+	initMeter()
+
+	meter := global.Meter("ex.com/basic")
+
+	observerLock := new(sync.RWMutex)
+	observerValueToReport := new(float64)
+	observerLabelsToReport := new([]attribute.KeyValue)
+
+	gaugeObserver, err := meter.AsyncFloat64().Gauge("ex.com.one")
+	if err != nil {
+		log.Panicf("failed to initialize instrument: %v", err)
+	}
+	_ = meter.RegisterCallback([]instrument.Asynchronous{gaugeObserver}, func(ctx context.Context) {
+		(*observerLock).RLock()
+		value := *observerValueToReport
+		labels := *observerLabelsToReport
+		(*observerLock).RUnlock()
+		gaugeObserver.Observe(ctx, value, labels...)
+	})
+
+	histogram, err := meter.SyncFloat64().Histogram("ex.com.two")
+	if err != nil {
+		log.Panicf("failed to initialize instrument: %v", err)
+	}
+	counter, err := meter.SyncFloat64().Counter("ex.com.three")
+	if err != nil {
+		log.Panicf("failed to initialize instrument: %v", err)
+	}
+
+	commonLabels := []attribute.KeyValue{lemonsKey.Int(10), attribute.String("A", "1"), attribute.String("B", "2"), attribute.String("C", "3")}
+	notSoCommonLabels := []attribute.KeyValue{lemonsKey.Int(13)}
+
+	ctx := context.Background()
+
+	(*observerLock).Lock()
+	*observerValueToReport = 1.0
+	*observerLabelsToReport = commonLabels
+	(*observerLock).Unlock()
+
+	histogram.Record(ctx, 2.0, commonLabels...)
+	counter.Add(ctx, 12.0, commonLabels...)
+
+	time.Sleep(5 * time.Second)
+
+	(*observerLock).Lock()
+	*observerValueToReport = 1.0
+	*observerLabelsToReport = notSoCommonLabels
+	(*observerLock).Unlock()
+	histogram.Record(ctx, 2.0, notSoCommonLabels...)
+	counter.Add(ctx, 22.0, notSoCommonLabels...)
+
+	time.Sleep(5 * time.Second)
+
+	(*observerLock).Lock()
+	*observerValueToReport = 13.0
+	*observerLabelsToReport = commonLabels
+	(*observerLock).Unlock()
+	histogram.Record(ctx, 12.0, commonLabels...)
+	counter.Add(ctx, 13.0, commonLabels...)
+
+	fmt.Println("Example finished updating, please visit :2222")
+
+	select {}
+}
