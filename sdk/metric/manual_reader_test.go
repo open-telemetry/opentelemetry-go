dc1d912728de74feff3cdb5c8ee59b37ccebfd78
Tyler Yahn <codingalias@gmail.com>
2022-06-13 14:16:49 -0700

    Restrict to Go 1.18

--- sdk/metric/manual_reader_test.go
+++ sdk/metric/manual_reader_test.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build go1.17
-// +build go1.17
+//go:build go1.18
+// +build go1.18
 
 package metric // import "go.opentelemetry.io/otel/sdk/metric/reader"
 
