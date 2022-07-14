425e9ce2ff08474a5f143b2006f54c2bce325d28
Tyler Yahn <MrAlias@users.noreply.github.com>
2022-07-14 04:44:45 -0700

    Use export.Aggregation instead of internal.Aggregation (#3007)
    * Use export.Aggregation instead of internal
    
    * Return an export.Aggregation instead of a slice

--- sdk/metric/internal/lastvalue.go
+++ sdk/metric/internal/lastvalue.go
@@ -17,7 +17,10 @@
 
 package internal // import "go.opentelemetry.io/otel/sdk/metric/internal"
 
-import "go.opentelemetry.io/otel/attribute"
+import (
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/sdk/metric/export"
+)
 
 // lastValue summarizes a set of measurements as the last one made.
 type lastValue[N int64 | float64] struct {
@@ -34,7 +37,7 @@ func (s *lastValue[N]) Aggregate(value N, attr attribute.Set) {
 	// TODO(#2971): implement.
 }
 
-func (s *lastValue[N]) Aggregations() []Aggregation {
+func (s *lastValue[N]) Aggregation() export.Aggregation {
 	// TODO(#2971): implement.
 	return nil
 }
