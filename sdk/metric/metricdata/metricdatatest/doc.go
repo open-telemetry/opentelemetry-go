f65075c354e0cf5059854f532bc8d4cc3e923cbe
Tyler Yahn <codingalias@gmail.com>
2022-07-18 14:18:17 -0700

    Expand pkg doc sentence

--- sdk/metric/metricdata/metricdatatest/doc.go
+++ sdk/metric/metricdata/metricdatatest/doc.go
@@ -15,6 +15,6 @@
 //go:build go1.18
 // +build go1.18
 
-// Package metricdatatest provides testing functionality for the metricdata
-// package.
+// Package metricdatatest provides testing functionality for use with the
+// metricdata package.
 package metricdatatest // import "go.opentelemetry.io/otel/sdk/metric/metricdata/metricdatatest"
