cfd7d8711e606844b2fc9aa300eaf17fea20302e
Tyler Yahn <codingalias@gmail.com>
2022-07-27 08:15:24 -0700

    Run make lint

--- sdk/metric/internal/sum_test.go
+++ sdk/metric/internal/sum_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/sdk/metric/metricdata"
