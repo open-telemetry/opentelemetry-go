7e797045ca046ef489be1bf97b3064c7218f0228
Tyler Yahn <codingalias@gmail.com>
2022-07-29 12:19:12 -0700

    Fix lint

--- sdk/metric/internal/histogram_test.go
+++ sdk/metric/internal/histogram_test.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/sdk/metric/aggregation"
 	"go.opentelemetry.io/otel/sdk/metric/metricdata"
