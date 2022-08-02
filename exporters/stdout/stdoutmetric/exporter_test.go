25f2f0ae646399456b1851b778d4e74e71dc9633
Tyler Yahn <codingalias@gmail.com>
2022-08-02 12:04:41 -0700

    Lint exporter_test.go

--- exporters/stdout/stdoutmetric/exporter_test.go
+++ exporters/stdout/stdoutmetric/exporter_test.go
@@ -24,6 +24,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
 	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
 	"go.opentelemetry.io/otel/sdk/metric/metricdata"
 )
