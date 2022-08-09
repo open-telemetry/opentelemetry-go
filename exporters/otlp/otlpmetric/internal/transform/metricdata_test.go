90ea943c4bc4b8438a4f53591ee021c163112e65
Tyler Yahn <codingalias@gmail.com>
2022-08-09 14:45:27 -0700

    goimported

--- exporters/otlp/otlpmetric/internal/transform/metricdata_test.go
+++ exporters/otlp/otlpmetric/internal/transform/metricdata_test.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric/unit"
 	"go.opentelemetry.io/otel/sdk/instrumentation"
