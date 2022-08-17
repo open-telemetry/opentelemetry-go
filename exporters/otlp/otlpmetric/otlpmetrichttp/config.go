060f304c202f233453027a16aec41cc519425018
Tyler Yahn <codingalias@gmail.com>
2022-08-17 15:16:16 -0700

    Update option docs
    Include info on envvars.

--- exporters/otlp/otlpmetric/otlpmetrichttp/config.go
+++ exporters/otlp/otlpmetric/otlpmetrichttp/config.go
@@ -38,7 +38,7 @@ const (
 	GzipCompression = Compression(oconf.GzipCompression)
 )
 
-// Option applies an option to the HTTP client.
+// Option applies an option to the Exporter.
 type Option interface {
 	applyHTTPOption(oconf.Config) oconf.Config
 }
@@ -51,8 +51,8 @@ func asHTTPOptions(opts []Option) []oconf.HTTPOption {
 	return converted
 }
 
-// RetryConfig defines configuration for retrying batches in case of export
-// failure using an exponential backoff.
+// RetryConfig defines configuration for retrying the export of metric data
+// that failed.
 type RetryConfig retry.Config
 
 type wrappedOption struct {
@@ -63,56 +63,123 @@ func (w wrappedOption) applyHTTPOption(cfg oconf.Config) oconf.Config {
 	return w.ApplyHTTPOption(cfg)
 }
 
-// WithEndpoint allows one to set the address of the collector endpoint that
-// the driver will use to send metrics. If unset, it will instead try to use
-// the default endpoint (localhost:4318). Note that the endpoint must not
-// contain any URL path.
+// WithEndpoint sets the target endpoint the Exporter will connect to. This
+// endpoint is specified as a host and optional port, no path or scheme should
+// be included (see WithInsecure and WithURLPath).
+//
+// If the OTEL_EXPORTER_OTLP_ENDPOINT or OTEL_EXPORTER_OTLP_METRICS_ENDPOINT
+// environment variable is set, and this option is not passed, that variable
+// value will be used. If both are set, OTEL_EXPORTER_OTLP_METRICS_ENDPOINT
+// will take precedence.
+//
+// By default, if an environment variable is not set, and this option is not
+// passed, "localhost:4318" will be used.
 func WithEndpoint(endpoint string) Option {
 	return wrappedOption{oconf.WithEndpoint(endpoint)}
 }
 
-// WithCompression tells the driver to compress the sent data.
+// WithCompression sets the compression strategy the Exporter will use to
+// compress the HTTP body.
+//
+// If the OTEL_EXPORTER_OTLP_COMPRESSION or
+// OTEL_EXPORTER_OTLP_METRICS_COMPRESSION environment variable is set, and
+// this option is not passed, that variable value will be used. That value can
+// be either "none" or "gzip". If both are set,
+// OTEL_EXPORTER_OTLP_METRICS_COMPRESSION will take precedence.
+//
+// By default, if an environment variable is not set, and this option is not
+// passed, no compression strategy will be used.
 func WithCompression(compression Compression) Option {
 	return wrappedOption{oconf.WithCompression(oconf.Compression(compression))}
 }
 
-// WithURLPath allows one to override the default URL path used
-// for sending metrics. If unset, default ("/v1/metrics") will be used.
+// WithURLPath sets the URL path the Exporter will send requests to.
+//
+// If the OTEL_EXPORTER_OTLP_ENDPOINT or OTEL_EXPORTER_OTLP_METRICS_ENDPOINT
+// environment variable is set, and this option is not passed, the path
+// contained in that variable value will be used. If both are set,
+// OTEL_EXPORTER_OTLP_METRICS_ENDPOINT will take precedence.
+//
+//
+// By default, if an environment variable is not set, and this option is not
+// passed, "/v1/metrics" will be used.
 func WithURLPath(urlPath string) Option {
 	return wrappedOption{oconf.WithURLPath(urlPath)}
 }
 
-// WithTLSClientConfig can be used to set up a custom TLS
-// configuration for the client used to send payloads to the
-// collector. Use it if you want to use a custom certificate.
+// WithTLSClientConfig sets the TLS configuration the Exporter will use for
+// HTTP requests.
+//
+// If the OTEL_EXPORTER_OTLP_CERTIFICATE or
+// OTEL_EXPORTER_OTLP_METRICS_CERTIFICATE environment variable is set, and
+// this option is not passed, that variable value will be used. The value will
+// be parsed the filepath of the TLS certificate chain to use. If both are
+// set, OTEL_EXPORTER_OTLP_METRICS_CERTIFICATE will take precedence.
+//
+// By default, if an environment variable is not set, and this option is not
+// passed, the system default configuration is used.
 func WithTLSClientConfig(tlsCfg *tls.Config) Option {
 	return wrappedOption{oconf.WithTLSClientConfig(tlsCfg)}
 }
 
-// WithInsecure tells the driver to connect to the collector using the
-// HTTP scheme, instead of HTTPS.
+// WithInsecure disables client transport security for the Exporter's HTTP
+// connection.
+//
+// If the OTEL_EXPORTER_OTLP_ENDPOINT or OTEL_EXPORTER_OTLP_METRICS_ENDPOINT
+// environment variable is set, and this option is not passed, that variable
+// value will be used to determine client security. If the endpoint has a
+// scheme of "http" or "unix" client security will be disabled. If both are
+// set, OTEL_EXPORTER_OTLP_METRICS_ENDPOINT will take precedence.
+//
+// By default, if an environment variable is not set, and this option is not
+// passed, client security will be used.
 func WithInsecure() Option {
 	return wrappedOption{oconf.WithInsecure()}
 }
 
-// WithHeaders allows one to tell the driver to send additional HTTP
-// headers with the payloads. Specifying headers like Content-Length,
-// Content-Encoding and Content-Type may result in a broken driver.
+// WithHeaders will send the provided headers with each HTTP requests.
+//
+// If the OTEL_EXPORTER_OTLP_HEADERS or OTEL_EXPORTER_OTLP_METRICS_HEADERS
+// environment variable is set, and this option is not passed, that variable
+// value will be used. The value will be parsed as a list of key value pairs.
+// These pairs are expected to be in the W3C Correlation-Context format
+// without additional semi-colon delimited metadata (i.e. "k1=v1,k2=v2"). If
+// both are set, OTEL_EXPORTER_OTLP_METRICS_HEADERS will take precedence.
+//
+// By default, if an environment variable is not set, and this option is not
+// passed, no user headers will be set.
 func WithHeaders(headers map[string]string) Option {
 	return wrappedOption{oconf.WithHeaders(headers)}
 }
 
-// WithTimeout tells the driver the max waiting time for the backend to process
-// each metrics batch.  If unset, the default will be 10 seconds.
+// WithTimeout sets the max amount of time an Exporter will attempt an export.
+//
+// This takes precedence over any retry settings defined by WithRetry. Once
+// this time limit has been reached the export is abandoned and the metric
+// data is dropped.
+//
+// If the OTEL_EXPORTER_OTLP_TIMEOUT or OTEL_EXPORTER_OTLP_METRICS_TIMEOUT
+// environment variable is set, and this option is not passed, that variable
+// value will be used. The value will be parsed as an integer representing the
+// timeout in milliseconds. If both are set,
+// OTEL_EXPORTER_OTLP_METRICS_TIMEOUT will take precedence.
+//
+// By default, if an environment variable is not set, and this option is not
+// passed, a timeout of 10 seconds will be used.
 func WithTimeout(duration time.Duration) Option {
 	return wrappedOption{oconf.WithTimeout(duration)}
 }
 
-// WithRetry configures the retry policy for transient errors that may occurs
-// when exporting traces. An exponential back-off algorithm is used to ensure
-// endpoints are not overwhelmed with retries. If unset, the default retry
-// policy will retry after 5 seconds and increase exponentially after each
-// error for a total of 1 minute.
+// WithRetry sets the retry policy for transient retryable errors that are
+// returned by the target endpoint.
+//
+// If the target endpoint responds with not only a retryable error, but
+// explicitly returns a backoff time in the response, that time will take
+// precedence over these settings.
+//
+// If unset, the default retry policy will be used. It will retry the export
+// 5 seconds after receiving a retryable error and increase exponentially
+// after each error for no more than a total time of 1 minute.
 func WithRetry(rc RetryConfig) Option {
 	return wrappedOption{oconf.WithRetry(retry.Config(rc))}
 }
