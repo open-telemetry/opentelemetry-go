2df85c572eab239fda3b59de49b87905a9327729
Tyler Yahn <codingalias@gmail.com>
2022-08-25 16:27:47 -0700

    Revert New to accept a context

--- exporters/otlp/otlpmetric/otlpmetrichttp/client.go
+++ exporters/otlp/otlpmetric/otlpmetrichttp/client.go
@@ -42,7 +42,7 @@ import (
 // New returns an OpenTelemetry metric Exporter. The Exporter can be used with
 // a PeriodicReader to export OpenTelemetry metric data to an OTLP receiving
 // endpoint using protobufs over HTTP.
-func New(opts ...Option) (metric.Exporter, error) {
+func New(_ context.Context, opts ...Option) (metric.Exporter, error) {
 	c, err := newClient(opts...)
 	if err != nil {
 		return nil, err
