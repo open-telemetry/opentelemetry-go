e9fc5d938f3728ca7520fa5fe59bb2732d41ce6c
Aaron Clawson <3766680+MadVikingGod@users.noreply.github.com>
2022-08-29 19:31:21 +0000

    Add Synchronous instruments

--- sdk/metric/instrument.go
+++ sdk/metric/instrument.go
@@ -24,11 +24,14 @@ import (
 	"go.opentelemetry.io/otel/metric/instrument"
 	"go.opentelemetry.io/otel/metric/instrument/asyncfloat64"
 	"go.opentelemetry.io/otel/metric/instrument/asyncint64"
+	"go.opentelemetry.io/otel/metric/instrument/syncfloat64"
+	"go.opentelemetry.io/otel/metric/instrument/syncint64"
 	"go.opentelemetry.io/otel/sdk/metric/internal"
 )
 
 type instrumentImpl[N int64 | float64] struct {
 	instrument.Asynchronous
+	instrument.Synchronous
 
 	aggregators []internal.Aggregator[N]
 }
@@ -39,6 +42,12 @@ var _ asyncfloat64.Gauge = &instrumentImpl[float64]{}
 var _ asyncint64.Counter = &instrumentImpl[int64]{}
 var _ asyncint64.UpDownCounter = &instrumentImpl[int64]{}
 var _ asyncint64.Gauge = &instrumentImpl[int64]{}
+var _ syncfloat64.Counter = &instrumentImpl[float64]{}
+var _ syncfloat64.UpDownCounter = &instrumentImpl[float64]{}
+var _ syncfloat64.Histogram = &instrumentImpl[float64]{}
+var _ syncint64.Counter = &instrumentImpl[int64]{}
+var _ syncint64.UpDownCounter = &instrumentImpl[int64]{}
+var _ syncint64.Histogram = &instrumentImpl[int64]{}
 
 func (i *instrumentImpl[N]) Observe(ctx context.Context, val N, attrs ...attribute.KeyValue) {
 	// Only record a value if this is being called from the MetricProvider.
@@ -46,6 +55,24 @@ func (i *instrumentImpl[N]) Observe(ctx context.Context, val N, attrs ...attribu
 	if !ok {
 		return
 	}
+	if err := ctx.Err(); err != nil {
+		return
+	}
+	for _, agg := range i.aggregators {
+		agg.Aggregate(val, attribute.NewSet(attrs...))
+	}
+}
+
+func (i *instrumentImpl[N]) Add(ctx context.Context, val N, attrs ...attribute.KeyValue) {
+	if err := ctx.Err(); err != nil {
+		return
+	}
+	for _, agg := range i.aggregators {
+		agg.Aggregate(val, attribute.NewSet(attrs...))
+	}
+}
+
+func (i *instrumentImpl[N]) Record(ctx context.Context, val N, attrs ...attribute.KeyValue) {
 	if err := ctx.Err(); err != nil {
 		return
 	}
