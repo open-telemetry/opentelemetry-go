524437599fd293a7892f65eef7d2b706e031d1b2
Tyler Yahn <codingalias@gmail.com>
2022-08-30 12:15:04 -0700

    Add Attributes field to instrumentation Scope

--- sdk/instrumentation/scope.go
+++ sdk/instrumentation/scope.go
@@ -21,7 +21,14 @@ For more information see
 */
 package instrumentation // import "go.opentelemetry.io/otel/sdk/instrumentation"
 
-// Scope represents the instrumentation scope.
+import "go.opentelemetry.io/otel/attribute"
+
+// Scope represents the instrumentation source of OpenTelemetry data.
+//
+// Code that uses OpenTelemetry APIs or data-models to produce telemetry needs
+// to be identifiable by the receiver of that that data. A Scope is used for
+// this purpose. It uniquely identify that code as the source and the extent to
+// which it is relevant.
 type Scope struct {
 	// Name is the name of the instrumentation scope. This should be the
 	// Go package name of that scope.
@@ -30,4 +37,13 @@ type Scope struct {
 	Version string
 	// SchemaURL of the telemetry emitted by the scope.
 	SchemaURL string
+	// Attributes describe the unique attributes of an instrumentation scope.
+	//
+	// These attributes are used to differentiate an instrumentation scope when
+	// it emits data that belong to different domains. For example, if both
+	// profiling data and client-side data are emitted as log records from the
+	// same instrumentation library, they may need to be differentiated by a
+	// telemetry receiver. In that case, these attributes are used to scope and
+	// differentiate the data.
+	Attributes attribute.Set
 }
