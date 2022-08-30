7b286ace27338df6ab701d5c7eb1e7e118c8e6c3
Tyler Yahn <codingalias@gmail.com>
2022-08-30 12:51:37 -0700

    Use scope attributes for new Tracer

--- sdk/trace/provider.go
+++ sdk/trace/provider.go
@@ -142,9 +142,10 @@ func (p *TracerProvider) Tracer(name string, opts ...trace.TracerOption) trace.T
 		name = defaultTracerName
 	}
 	is := instrumentation.Scope{
-		Name:      name,
-		Version:   c.InstrumentationVersion(),
-		SchemaURL: c.SchemaURL(),
+		Name:       name,
+		Version:    c.InstrumentationVersion(),
+		SchemaURL:  c.SchemaURL(),
+		Attributes: c.Attributes(),
 	}
 	t, ok := p.namedTracer[is]
 	if !ok {
@@ -153,7 +154,13 @@ func (p *TracerProvider) Tracer(name string, opts ...trace.TracerOption) trace.T
 			instrumentationScope: is,
 		}
 		p.namedTracer[is] = t
-		global.Info("Tracer created", "name", name, "version", c.InstrumentationVersion(), "schemaURL", c.SchemaURL())
+		global.Info(
+			"Tracer created",
+			"name", name,
+			"version", c.InstrumentationVersion(),
+			"schemaURL", c.SchemaURL(),
+			"attributes", c.Attributes(),
+		)
 	}
 	return t
 }
