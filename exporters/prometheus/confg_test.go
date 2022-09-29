406817dab0db3d17b0f6705751fae9201b249ba5
Aaron Clawson <3766680+MadVikingGod@users.noreply.github.com>
2022-09-29 16:52:30 +0000

    Fix lint

--- exporters/prometheus/confg_test.go
+++ exporters/prometheus/confg_test.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/assert"
+
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/metric/metricdata"
 )
