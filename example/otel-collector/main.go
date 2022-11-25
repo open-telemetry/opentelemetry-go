e726770c9a5945a338aec39b103ffd01534b7646
Yuri Shkuro <github@ysh.us>
2022-11-25 15:47:05 -0500

    Clarify HTTP/gRPC exporter examples
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- example/otel-collector/main.go
+++ example/otel-collector/main.go
@@ -57,10 +57,14 @@ func initProvider() (func(context.Context) error, error) {
 	// microk8s), it should be accessible through the NodePort service at the
 	// `localhost:30080` endpoint. Otherwise, replace `localhost` with the
 	// endpoint of your cluster. If you run the app inside k8s, then you can
-	// probably connect directly to the service through dns
+	// probably connect directly to the service through dns.
 	ctx, cancel := context.WithTimeout(ctx, time.Second)
 	defer cancel()
-	conn, err := grpc.DialContext(ctx, "localhost:30080", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+	conn, err := grpc.DialContext(ctx, "localhost:30080",
+		// Note the use of insecure transport here. TLS is recommended in production.
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create gRPC connection to collector: %w", err)
 	}
