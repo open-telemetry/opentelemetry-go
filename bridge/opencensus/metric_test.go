a724cf884287e04785eaa91513d26a6ef9699288
David Ashpole <dashpole@google.com>
2022-12-19 11:05:46 -0500

    Update OpenCensus metric bridge to use the metric.Producer interface (#3541)
    * update OpenCensus metric bridge to use the metric.Producer interface
    
    * return nil for empty metrics
    
    * fix nits
    
    Co-authored-by: Tyler Yahn <MrAlias@users.noreply.github.com>

--- bridge/opencensus/metric_test.go
+++ bridge/opencensus/metric_test.go
@@ -22,6 +22,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 	ocmetricdata "go.opencensus.io/metric/metricdata"
+	"go.opencensus.io/metric/metricproducer"
 	ocresource "go.opencensus.io/resource"
 
 	"go.opentelemetry.io/otel/attribute"
@@ -31,6 +32,134 @@ import (
 	"go.opentelemetry.io/otel/sdk/metric/metricdata/metricdatatest"
 	"go.opentelemetry.io/otel/sdk/resource"
 )
+
+func TestMetricProducer(t *testing.T) {
+	now := time.Now()
+	for _, tc := range []struct {
+		desc      string
+		input     []*ocmetricdata.Metric
+		expected  []metricdata.ScopeMetrics
+		expectErr bool
+	}{
+		{
+			desc:     "empty",
+			expected: nil,
+		},
+		{
+			desc: "success",
+			input: []*ocmetricdata.Metric{
+				{
+					Resource: &ocresource.Resource{
+						Labels: map[string]string{
+							"R1": "V1",
+							"R2": "V2",
+						},
+					},
+					TimeSeries: []*ocmetricdata.TimeSeries{
+						{
+							StartTime: now,
+							Points: []ocmetricdata.Point{
+								{Value: int64(123), Time: now},
+							},
+						},
+					},
+				},
+			},
+			expected: []metricdata.ScopeMetrics{{
+				Scope: instrumentation.Scope{
+					Name: scopeName,
+				},
+				Metrics: []metricdata.Metrics{
+					{
+						Data: metricdata.Gauge[int64]{
+							DataPoints: []metricdata.DataPoint[int64]{
+								{
+									Attributes: attribute.NewSet(),
+									StartTime:  now,
+									Time:       now,
+									Value:      123,
+								},
+							},
+						},
+					},
+				},
+			}},
+		},
+		{
+			desc: "partial success",
+			input: []*ocmetricdata.Metric{
+				{
+					Descriptor: ocmetricdata.Descriptor{
+						Name:        "foo.com/bad-point",
+						Description: "a bad type",
+						Unit:        ocmetricdata.UnitDimensionless,
+						Type:        ocmetricdata.TypeGaugeDistribution,
+					},
+				},
+				{
+					Resource: &ocresource.Resource{
+						Labels: map[string]string{
+							"R1": "V1",
+							"R2": "V2",
+						},
+					},
+					TimeSeries: []*ocmetricdata.TimeSeries{
+						{
+							StartTime: now,
+							Points: []ocmetricdata.Point{
+								{Value: int64(123), Time: now},
+							},
+						},
+					},
+				},
+			},
+			expected: []metricdata.ScopeMetrics{{
+				Scope: instrumentation.Scope{
+					Name: scopeName,
+				},
+				Metrics: []metricdata.Metrics{
+					{
+						Data: metricdata.Gauge[int64]{
+							DataPoints: []metricdata.DataPoint[int64]{
+								{
+									Attributes: attribute.NewSet(),
+									StartTime:  now,
+									Time:       now,
+									Value:      123,
+								},
+							},
+						},
+					},
+				},
+			}},
+			expectErr: true,
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			fakeProducer := &fakeOCProducer{metrics: tc.input}
+			metricproducer.GlobalManager().AddProducer(fakeProducer)
+			defer metricproducer.GlobalManager().DeleteProducer(fakeProducer)
+			output, err := NewMetricProducer().Produce(context.Background())
+			if tc.expectErr {
+				require.Error(t, err)
+			} else {
+				require.Nil(t, err)
+			}
+			require.Equal(t, len(output), len(tc.expected))
+			for i := range output {
+				metricdatatest.AssertEqual(t, tc.expected[i], output[i])
+			}
+		})
+	}
+}
+
+type fakeOCProducer struct {
+	metrics []*ocmetricdata.Metric
+}
+
+func (f *fakeOCProducer) Read() []*ocmetricdata.Metric {
+	return f.metrics
+}
 
 func TestPushMetricsExporter(t *testing.T) {
 	now := time.Now()
