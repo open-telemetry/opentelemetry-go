da5356e15e26458102188bbfcc83d4535231cc35
Tyler Yahn <codingalias@gmail.com>
2023-01-12 16:31:54 -0800

    Add metricdatatest tests

--- sdk/metric/metricdata/metricdatatest/assertion_test.go
+++ sdk/metric/metricdata/metricdatatest/assertion_test.go
@@ -73,6 +73,10 @@ var (
 		Value:      -1.0,
 	}
 
+	extremaA = metricdata.NewExtrema(99.)
+	extremaB = metricdata.NewExtrema(3.)
+	extremaC = metricdata.NewExtrema(99.)
+
 	histogramDataPointA = metricdata.HistogramDataPoint{
 		Attributes:   attrA,
 		StartTime:    startA,
@@ -89,8 +93,8 @@ var (
 		Count:        3,
 		Bounds:       []float64{0, 10, 100},
 		BucketCounts: []uint64{1, 1, 1},
-		Max:          metricdata.NewExtrema(99.),
-		Min:          metricdata.NewExtrema(3.),
+		Max:          extremaA,
+		Min:          extremaB,
 		Sum:          3,
 	}
 	histogramDataPointC = metricdata.HistogramDataPoint{
@@ -246,6 +250,7 @@ func TestAssertEqual(t *testing.T) {
 	t.Run("HistogramDataPoint", testDatatype(histogramDataPointA, histogramDataPointB, equalHistogramDataPoints))
 	t.Run("DataPointInt64", testDatatype(dataPointInt64A, dataPointInt64B, equalDataPoints[int64]))
 	t.Run("DataPointFloat64", testDatatype(dataPointFloat64A, dataPointFloat64B, equalDataPoints[float64]))
+	t.Run("Extrema", testDatatype(extremaA, extremaB, equalExtrema))
 }
 
 func TestAssertEqualIgnoreTime(t *testing.T) {
@@ -260,6 +265,7 @@ func TestAssertEqualIgnoreTime(t *testing.T) {
 	t.Run("HistogramDataPoint", testDatatypeIgnoreTime(histogramDataPointA, histogramDataPointC, equalHistogramDataPoints))
 	t.Run("DataPointInt64", testDatatypeIgnoreTime(dataPointInt64A, dataPointInt64C, equalDataPoints[int64]))
 	t.Run("DataPointFloat64", testDatatypeIgnoreTime(dataPointFloat64A, dataPointFloat64C, equalDataPoints[float64]))
+	t.Run("Extrema", testDatatypeIgnoreTime(extremaA, extremaC, equalExtrema))
 }
 
 type unknownAggregation struct {
@@ -315,6 +321,7 @@ func TestAssertAggregationsEqual(t *testing.T) {
 }
 
 func TestAssertAttributes(t *testing.T) {
+	AssertHasAttributes(t, extremaA, attribute.Bool("A", true)) // No-op, always pass.
 	AssertHasAttributes(t, dataPointInt64A, attribute.Bool("A", true))
 	AssertHasAttributes(t, dataPointFloat64A, attribute.Bool("A", true))
 	AssertHasAttributes(t, gaugeInt64A, attribute.Bool("A", true))
