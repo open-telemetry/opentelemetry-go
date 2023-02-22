4d5132437e2c68b3eb784ff501cd73a8d4d553af
ChillOrb <rakshitparashar1@gmail.com>
2023-02-21 22:57:42 -0800

    changing regex string as per recommendations
    Signed-off-by: ChillOrb <rakshitparashar1@gmail.com>

--- sdk/metric/version_test.go
+++ sdk/metric/version_test.go
@@ -22,11 +22,12 @@ import (
 )
 
 // regex taken from https://github.com/Masterminds/semver/tree/v3.1.1
-var versionRegex = regexp.MustCompile(`^v?([0-9]+)(\.[0-9]+)?(\.[0-9]+)?` +
-	`(-([0-9A-Za-z\-]+(\.[0-9A-Za-z\-]+)*))?` +
-	`(\+([0-9A-Za-z\-]+(\.[0-9A-Za-z\-]+)*))?$`)
+var versionRegex = regexp.MustCompile(`^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)` +
+	`(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)` +
+	`(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?` +
+	`(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$`)
 
 func TestVersionSemver(t *testing.T) {
 	v := version()
-	assert.Regexp(t, versionRegex.FindStringSubmatch(v), "version is not semver: %s", v)
+	assert.Regexp(t, versionRegex, v)
 }
--- sdk/trace/version_test.go
+++ sdk/trace/version_test.go
@@ -21,11 +21,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var versionRegex = regexp.MustCompile(`^v?([0-9]+)(\.[0-9]+)?(\.[0-9]+)?` +
-	`(-([0-9A-Za-z\-]+(\.[0-9A-Za-z\-]+)*))?` +
-	`(\+([0-9A-Za-z\-]+(\.[0-9A-Za-z\-]+)*))?$`)
+var versionRegex = regexp.MustCompile(`^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)` +
+	`(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)` +
+	`(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?` +
+	`(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$`)
 
 func TestVersionSemver(t *testing.T) {
 	v := version()
-	assert.Regexp(t, versionRegex.FindStringSubmatch(v), "version is not semver: %s", v)
+	assert.Regexp(t, versionRegex, v)
 }
