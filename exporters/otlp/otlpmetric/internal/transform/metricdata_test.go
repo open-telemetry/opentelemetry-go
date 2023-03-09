96aa2da8fb9042543e5b0870055d81f852f400a3
sinanulker <sinanulker386@gmail.com>
2023-03-09 22:21:00 +0100

    Pass tests for otlp exporter

--- exporters/otlp/otlpmetric/internal/transform/metricdata_test.go
+++ exporters/otlp/otlpmetric/internal/transform/metricdata_test.go
@@ -365,7 +365,7 @@ func TestTransformations(t *testing.T) {
 	require.Equal(t, pbScopeMetrics, sm)
 
 	// Resource Metrics.
-	rm, err := ResourceMetrics(otelResourceMetrics)
+	rm, err := ResourceMetrics(&otelResourceMetrics)
 	assert.ErrorIs(t, err, errUnknownTemporality)
 	assert.ErrorIs(t, err, errUnknownAggregation)
 	require.Equal(t, pbResourceMetrics, rm)
