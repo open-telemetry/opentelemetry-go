cf87b5c05d3dcde113927ac91744146a0e29448a
Tyler Yahn <codingalias@gmail.com>
2023-04-11 09:03:58 -0700

    Revert "Support a global MeterProvider in `go.opentelemetry.io/otel` (#3818)"
    This reverts commit 813936187e46d48924b717ed9f63767eef26f55a.

--- internal/global/instruments.go
+++ internal/global/instruments.go
@@ -12,13 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package global // import "go.opentelemetry.io/otel/metric/internal/global"
+package global // import "go.opentelemetry.io/otel/internal/global"
 
 import (
 	"context"
 	"sync/atomic"
 
-	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/metric/embedded"
@@ -46,7 +45,7 @@ var _ instrument.Float64ObservableCounter = (*afCounter)(nil)
 func (i *afCounter) setDelegate(m metric.Meter) {
 	ctr, err := m.Float64ObservableCounter(i.name, i.opts...)
 	if err != nil {
-		otel.Handle(err)
+		GetErrorHandler().Handle(err)
 		return
 	}
 	i.delegate.Store(ctr)
@@ -75,7 +74,7 @@ var _ instrument.Float64ObservableUpDownCounter = (*afUpDownCounter)(nil)
 func (i *afUpDownCounter) setDelegate(m metric.Meter) {
 	ctr, err := m.Float64ObservableUpDownCounter(i.name, i.opts...)
 	if err != nil {
-		otel.Handle(err)
+		GetErrorHandler().Handle(err)
 		return
 	}
 	i.delegate.Store(ctr)
@@ -104,7 +103,7 @@ var _ instrument.Float64ObservableGauge = (*afGauge)(nil)
 func (i *afGauge) setDelegate(m metric.Meter) {
 	ctr, err := m.Float64ObservableGauge(i.name, i.opts...)
 	if err != nil {
-		otel.Handle(err)
+		GetErrorHandler().Handle(err)
 		return
 	}
 	i.delegate.Store(ctr)
@@ -133,7 +132,7 @@ var _ instrument.Int64ObservableCounter = (*aiCounter)(nil)
 func (i *aiCounter) setDelegate(m metric.Meter) {
 	ctr, err := m.Int64ObservableCounter(i.name, i.opts...)
 	if err != nil {
-		otel.Handle(err)
+		GetErrorHandler().Handle(err)
 		return
 	}
 	i.delegate.Store(ctr)
@@ -162,7 +161,7 @@ var _ instrument.Int64ObservableUpDownCounter = (*aiUpDownCounter)(nil)
 func (i *aiUpDownCounter) setDelegate(m metric.Meter) {
 	ctr, err := m.Int64ObservableUpDownCounter(i.name, i.opts...)
 	if err != nil {
-		otel.Handle(err)
+		GetErrorHandler().Handle(err)
 		return
 	}
 	i.delegate.Store(ctr)
@@ -191,7 +190,7 @@ var _ instrument.Int64ObservableGauge = (*aiGauge)(nil)
 func (i *aiGauge) setDelegate(m metric.Meter) {
 	ctr, err := m.Int64ObservableGauge(i.name, i.opts...)
 	if err != nil {
-		otel.Handle(err)
+		GetErrorHandler().Handle(err)
 		return
 	}
 	i.delegate.Store(ctr)
@@ -219,7 +218,7 @@ var _ instrument.Float64Counter = (*sfCounter)(nil)
 func (i *sfCounter) setDelegate(m metric.Meter) {
 	ctr, err := m.Float64Counter(i.name, i.opts...)
 	if err != nil {
-		otel.Handle(err)
+		GetErrorHandler().Handle(err)
 		return
 	}
 	i.delegate.Store(ctr)
@@ -245,7 +244,7 @@ var _ instrument.Float64UpDownCounter = (*sfUpDownCounter)(nil)
 func (i *sfUpDownCounter) setDelegate(m metric.Meter) {
 	ctr, err := m.Float64UpDownCounter(i.name, i.opts...)
 	if err != nil {
-		otel.Handle(err)
+		GetErrorHandler().Handle(err)
 		return
 	}
 	i.delegate.Store(ctr)
@@ -271,7 +270,7 @@ var _ instrument.Float64Histogram = (*sfHistogram)(nil)
 func (i *sfHistogram) setDelegate(m metric.Meter) {
 	ctr, err := m.Float64Histogram(i.name, i.opts...)
 	if err != nil {
-		otel.Handle(err)
+		GetErrorHandler().Handle(err)
 		return
 	}
 	i.delegate.Store(ctr)
@@ -297,7 +296,7 @@ var _ instrument.Int64Counter = (*siCounter)(nil)
 func (i *siCounter) setDelegate(m metric.Meter) {
 	ctr, err := m.Int64Counter(i.name, i.opts...)
 	if err != nil {
-		otel.Handle(err)
+		GetErrorHandler().Handle(err)
 		return
 	}
 	i.delegate.Store(ctr)
@@ -323,7 +322,7 @@ var _ instrument.Int64UpDownCounter = (*siUpDownCounter)(nil)
 func (i *siUpDownCounter) setDelegate(m metric.Meter) {
 	ctr, err := m.Int64UpDownCounter(i.name, i.opts...)
 	if err != nil {
-		otel.Handle(err)
+		GetErrorHandler().Handle(err)
 		return
 	}
 	i.delegate.Store(ctr)
@@ -349,7 +348,7 @@ var _ instrument.Int64Histogram = (*siHistogram)(nil)
 func (i *siHistogram) setDelegate(m metric.Meter) {
 	ctr, err := m.Int64Histogram(i.name, i.opts...)
 	if err != nil {
-		otel.Handle(err)
+		GetErrorHandler().Handle(err)
 		return
 	}
 	i.delegate.Store(ctr)
---
+++ internal/global/instruments_test.go
@@ -0,0 +1,188 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package global
+
+import (
+	"context"
+	"testing"
+
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/metric/embedded"
+	"go.opentelemetry.io/otel/metric/instrument"
+	"go.opentelemetry.io/otel/metric/noop"
+)
+
+func testFloat64Race(interact func(context.Context, float64, ...attribute.KeyValue), setDelegate func(metric.Meter)) {
+	finish := make(chan struct{})
+	go func() {
+		for {
+			interact(context.Background(), 1)
+			select {
+			case <-finish:
+				return
+			default:
+			}
+		}
+	}()
+
+	setDelegate(noop.NewMeterProvider().Meter(""))
+	close(finish)
+}
+
+func testInt64Race(interact func(context.Context, int64, ...attribute.KeyValue), setDelegate func(metric.Meter)) {
+	finish := make(chan struct{})
+	go func() {
+		for {
+			interact(context.Background(), 1)
+			select {
+			case <-finish:
+				return
+			default:
+			}
+		}
+	}()
+
+	setDelegate(noop.NewMeterProvider().Meter(""))
+	close(finish)
+}
+
+func TestAsyncInstrumentSetDelegateRace(t *testing.T) {
+	// Float64 Instruments
+	t.Run("Float64", func(t *testing.T) {
+		t.Run("Counter", func(t *testing.T) {
+			delegate := &afCounter{}
+			f := func(context.Context, float64, ...attribute.KeyValue) { _ = delegate.Unwrap() }
+			testFloat64Race(f, delegate.setDelegate)
+		})
+
+		t.Run("UpDownCounter", func(t *testing.T) {
+			delegate := &afUpDownCounter{}
+			f := func(context.Context, float64, ...attribute.KeyValue) { _ = delegate.Unwrap() }
+			testFloat64Race(f, delegate.setDelegate)
+		})
+
+		t.Run("Gauge", func(t *testing.T) {
+			delegate := &afGauge{}
+			f := func(context.Context, float64, ...attribute.KeyValue) { _ = delegate.Unwrap() }
+			testFloat64Race(f, delegate.setDelegate)
+		})
+	})
+
+	// Int64 Instruments
+
+	t.Run("Int64", func(t *testing.T) {
+		t.Run("Counter", func(t *testing.T) {
+			delegate := &aiCounter{}
+			f := func(context.Context, int64, ...attribute.KeyValue) { _ = delegate.Unwrap() }
+			testInt64Race(f, delegate.setDelegate)
+		})
+
+		t.Run("UpDownCounter", func(t *testing.T) {
+			delegate := &aiUpDownCounter{}
+			f := func(context.Context, int64, ...attribute.KeyValue) { _ = delegate.Unwrap() }
+			testInt64Race(f, delegate.setDelegate)
+		})
+
+		t.Run("Gauge", func(t *testing.T) {
+			delegate := &aiGauge{}
+			f := func(context.Context, int64, ...attribute.KeyValue) { _ = delegate.Unwrap() }
+			testInt64Race(f, delegate.setDelegate)
+		})
+	})
+}
+
+func TestSyncInstrumentSetDelegateRace(t *testing.T) {
+	// Float64 Instruments
+	t.Run("Float64", func(t *testing.T) {
+		t.Run("Counter", func(t *testing.T) {
+			delegate := &sfCounter{}
+			testFloat64Race(delegate.Add, delegate.setDelegate)
+		})
+
+		t.Run("UpDownCounter", func(t *testing.T) {
+			delegate := &sfUpDownCounter{}
+			testFloat64Race(delegate.Add, delegate.setDelegate)
+		})
+
+		t.Run("Histogram", func(t *testing.T) {
+			delegate := &sfHistogram{}
+			testFloat64Race(delegate.Record, delegate.setDelegate)
+		})
+	})
+
+	// Int64 Instruments
+
+	t.Run("Int64", func(t *testing.T) {
+		t.Run("Counter", func(t *testing.T) {
+			delegate := &siCounter{}
+			testInt64Race(delegate.Add, delegate.setDelegate)
+		})
+
+		t.Run("UpDownCounter", func(t *testing.T) {
+			delegate := &siUpDownCounter{}
+			testInt64Race(delegate.Add, delegate.setDelegate)
+		})
+
+		t.Run("Histogram", func(t *testing.T) {
+			delegate := &siHistogram{}
+			testInt64Race(delegate.Record, delegate.setDelegate)
+		})
+	})
+}
+
+type testCountingFloatInstrument struct {
+	count int
+
+	instrument.Float64Observable
+	embedded.Float64Counter
+	embedded.Float64UpDownCounter
+	embedded.Float64Histogram
+	embedded.Float64ObservableCounter
+	embedded.Float64ObservableUpDownCounter
+	embedded.Float64ObservableGauge
+}
+
+func (i *testCountingFloatInstrument) observe() {
+	i.count++
+}
+func (i *testCountingFloatInstrument) Add(context.Context, float64, ...attribute.KeyValue) {
+	i.count++
+}
+func (i *testCountingFloatInstrument) Record(context.Context, float64, ...attribute.KeyValue) {
+	i.count++
+}
+
+type testCountingIntInstrument struct {
+	count int
+
+	instrument.Int64Observable
+	embedded.Int64Counter
+	embedded.Int64UpDownCounter
+	embedded.Int64Histogram
+	embedded.Int64ObservableCounter
+	embedded.Int64ObservableUpDownCounter
+	embedded.Int64ObservableGauge
+}
+
+func (i *testCountingIntInstrument) observe() {
+	i.count++
+}
+func (i *testCountingIntInstrument) Add(context.Context, int64, ...attribute.KeyValue) {
+	i.count++
+}
+func (i *testCountingIntInstrument) Record(context.Context, int64, ...attribute.KeyValue) {
+	i.count++
+}
