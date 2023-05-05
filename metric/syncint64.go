9c34448c3ed8ecdd8f28802ddeb0a1c9d10a63a7
Tyler Yahn <codingalias@gmail.com>
2023-05-05 12:49:31 -0700

    Update metric iface warnings
    Remove self-reference to the package in the expandable interfaces.

--- metric/syncint64.go
+++ metric/syncint64.go
@@ -23,9 +23,8 @@ import (
 // Int64Counter is an instrument that records increasing int64 values.
 //
 // Warning: Methods may be added to this interface in minor releases. See
-// [go.opentelemetry.io/otel/metric] package documentation on API
-// implementation for information on how to set default behavior for
-// unimplemented methods.
+// package documentation on API implementation for information on how to set
+// default behavior for unimplemented methods.
 type Int64Counter interface {
 	embedded.Int64Counter
 
@@ -70,9 +69,8 @@ type Int64CounterOption interface {
 // int64 values.
 //
 // Warning: Methods may be added to this interface in minor releases. See
-// [go.opentelemetry.io/otel/metric] package documentation on API
-// implementation for information on how to set default behavior for
-// unimplemented methods.
+// package documentation on API implementation for information on how to set
+// default behavior for unimplemented methods.
 type Int64UpDownCounter interface {
 	embedded.Int64UpDownCounter
 
@@ -118,9 +116,8 @@ type Int64UpDownCounterOption interface {
 // values.
 //
 // Warning: Methods may be added to this interface in minor releases. See
-// [go.opentelemetry.io/otel/metric] package documentation on API
-// implementation for information on how to set default behavior for
-// unimplemented methods.
+// package documentation on API implementation for information on how to set
+// default behavior for unimplemented methods.
 type Int64Histogram interface {
 	embedded.Int64Histogram
 
