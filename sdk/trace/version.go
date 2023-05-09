3b1bb7267f3a04e171ca85cac020f1d48c904ff5
Tyler Yahn <MrAlias@users.noreply.github.com>
2023-05-09 08:15:24 -0700

    Apply suggestions from code review

--- sdk/trace/version.go
+++ sdk/trace/version.go
@@ -16,5 +16,5 @@ package trace // import "go.opentelemetry.io/otel/sdk/trace"
 
 // version is the current release version of the metric SDK in use.
 func version() string {
-	return "1.15.0-rc.1"
+	return "1.16.0-rc.1"
 }
