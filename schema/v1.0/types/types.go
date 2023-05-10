acc6296164c8ce33bda549200a4e396f8c207dc9
Sean Marciniak <30928402+MovieStoreGuy@users.noreply.github.com>
2023-05-10 20:43:59 +0930

    Use type alias instead of type definition

--- schema/v1.0/types/types.go
+++ schema/v1.0/types/types.go
@@ -15,13 +15,13 @@
 package types // import "go.opentelemetry.io/otel/schema/v1.0/types"
 
 // TelemetryVersion is a version number key in the schema file (e.g. "1.7.0").
-type TelemetryVersion string
+type TelemetryVersion = string
 
 // SpanName is span name string.
-type SpanName string
+type SpanName = string
 
 // EventName is an event name string.
-type EventName string
+type EventName = string
 
 // MetricName is a metric name string.
-type MetricName string
+type MetricName = string
--- schema/v1.1/types/types.go
+++ schema/v1.1/types/types.go
@@ -17,10 +17,10 @@ package types // import "go.opentelemetry.io/otel/schema/v1.1/types"
 import types10 "go.opentelemetry.io/otel/schema/v1.0/types"
 
 // TelemetryVersion is a version number key in the schema file (e.g. "1.7.0").
-type TelemetryVersion types10.TelemetryVersion
+type TelemetryVersion = types10.TelemetryVersion
 
 // AttributeName is an attribute name string.
-type AttributeName string
+type AttributeName = string
 
 // AttributeValue is an attribute value.
 type AttributeValue interface{}
