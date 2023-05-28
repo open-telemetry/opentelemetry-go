3006d0551929e09056b35514487266dfc62275fc
9bany <bany.y0599@gmail.com>
2023-05-28 17:23:46 +0700

    update controller

--- example/CRUD/server/create_users.go
+++ example/CRUD/server/create_users.go
@@ -1,10 +1,24 @@
 package server
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	db "go.opentelemetry.io/otel/example/CRUD/db/sqlc"
 )
 
 func (server *Server) CreateUser(ctx *gin.Context) {
-	server.store.CreateUser(ctx, db.CreateUserParams{})
+	var user db.CreateUserParams
+	err := ctx.Bind(&user)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	u, err := server.store.CreateUser(ctx, user)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Can not create an user"})
+		return
+	}
+	ctx.JSON(http.StatusOK, u)
+
 }
--- example/CRUD/server/get_users.go
+++ example/CRUD/server/get_users.go
@@ -1,8 +1,17 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func (server *Server) GetUser(ctx *gin.Context) {
-	server.store.GetUser(ctx, "")
+	username := ctx.Params.ByName("username")
+	user, err := server.store.GetUser(ctx, username)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"err": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
 }
-	
