18b5f91cf9f6d6eb4164db8ecc3452efeceb0dbe
Aaron Clawson <3766680+MadVikingGod@users.noreply.github.com>
2023-07-17 13:13:56 -0500

    Fix lint

--- sdk/metric/internal/aggregate/exponential_histogram_test.go
+++ sdk/metric/internal/aggregate/exponential_histogram_test.go
@@ -21,6 +21,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/internal/global"
 	"go.opentelemetry.io/otel/sdk/metric/aggregation"
