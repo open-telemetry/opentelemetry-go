a37cb0504a319904c33085c454f34491e81bf0f5
Tyler Yahn <MrAlias@users.noreply.github.com>
2023-07-20 13:53:50 -0700

    Replace filter aggregator with direct filter on measure (#4342)
    * Replace filter aggregator with direct filter on measure
    
    Part of #4220
    
    Instead of using an aggregator to filter measured attributes, directly
    filter the attributes in the constructed Measure function the Builder
    creates.
    
    Include unit and integration testing of new filtering.
    
    * Update sdk/metric/internal/aggregate/aggregate.go
    
    Co-authored-by: David Ashpole <dashpole@google.com>
    
    ---------
    
    Co-authored-by: David Ashpole <dashpole@google.com>

--- sdk/metric/internal/aggregate/aggregate.go
+++ sdk/metric/internal/aggregate/aggregate.go
@@ -44,7 +44,11 @@ type Builder[N int64 | float64] struct {
 
 func (b Builder[N]) input(agg aggregator[N]) Measure[N] {
 	if b.Filter != nil {
-		agg = newFilter[N](agg, b.Filter)
+		fltr := b.Filter // Copy to make it immutable after assignment.
+		return func(_ context.Context, n N, a attribute.Set) {
+			fAttr, _ := a.Filter(fltr)
+			agg.Aggregate(n, fAttr)
+		}
 	}
 	return func(_ context.Context, n N, a attribute.Set) {
 		agg.Aggregate(n, a)
