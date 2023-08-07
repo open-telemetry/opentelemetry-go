33f31988404fb02d6bed100c19fa95c1ecb3346c
Robert Pająk <pellared@hotmail.com>
2023-08-07 13:13:56 +0200

    metric: Recommend defining units in UCUM

--- metric/instrument.go
+++ metric/instrument.go
@@ -167,6 +167,8 @@ func (o unitOpt) applyInt64ObservableGauge(c Int64ObservableGaugeConfig) Int64Ob
 }
 
 // WithUnit sets the instrument unit.
+//
+// It is recommended to define the unit in [UCUM](https://ucum.org).
 func WithUnit(u string) InstrumentOption { return unitOpt(u) }
 
 // AddOption applies options to an addition measurement. See
