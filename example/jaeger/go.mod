8da137d2055c3a5f28f2c0a1fe05710301c90b33
Robert Pająk <pellared@hotmail.com>
2023-08-09 18:15:57 +0200

    Apply suggestions from code review
    Co-authored-by: Tyler Yahn <MrAlias@users.noreply.github.com>

--- example/jaeger/go.mod
+++ example/jaeger/go.mod
@@ -1,4 +1,5 @@
-// Deprecated: [go.opentelemetry.io/otel/exporters/jaeger] is no longer supported.
+// Deprecated: This example is no longer supported as 
+[go.opentelemetry.io/otel/exporters/jaeger] is no longer supported.
 // OpenTelemetry dropped support for Jaeger exporter in July 2023.
 // Jaeger officially accepts and recommends using OTLP.
 // Use [go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp]
--- example/jaeger/main.go
+++ example/jaeger/main.go
@@ -15,7 +15,8 @@
 // Command jaeger is an example program that creates spans
 // and uploads to Jaeger.
 //
-// Deprecated: [go.opentelemetry.io/otel/exporters/jaeger] is no longer supported.
+// Deprecated:  This example is no longer supported as
+[go.opentelemetry.io/otel/exporters/jaeger] is no longer supported.
 // OpenTelemetry dropped support for Jaeger exporter in July 2023.
 // Jaeger officially accepts and recommends using OTLP.
 // Use [go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp]
