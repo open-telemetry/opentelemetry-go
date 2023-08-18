99f6b14157347a333c369201ec8810bb86ed4583
Tyler Yahn <codingalias@gmail.com>
2023-08-18 08:20:57 -0700

    Use Algorithm L for rand res

--- sdk/metric/internal/exemplar/rand.go
+++ sdk/metric/internal/exemplar/rand.go
@@ -16,6 +16,7 @@ package exemplar // import "go.opentelemetry.io/otel/sdk/metric/internal/exempla
 
 import (
 	"context"
+	"math"
 	"math/rand"
 	"time"
 
@@ -31,7 +32,9 @@ var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
 // will then randomly sample all additional measurement with a decreasing
 // probability.
 func FixedSize[N int64 | float64](n int) Reservoir[N] {
-	return &randRes[N]{fixedRes: newFixedRes[N](n)}
+	r := &randRes[N]{fixedRes: newFixedRes[N](n)}
+	r.reset()
+	return r
 }
 
 type randRes[N int64 | float64] struct {
@@ -39,28 +42,59 @@ type randRes[N int64 | float64] struct {
 
 	// count is the number of measurement seen.
 	count int64
+	// next is the next count that will store a measurement at a randon index
+	// once the reservoir has been filled.
+	next int64
+	// w is the largest random number in a distribution that is used to compute
+	// the next next.
+	w float64
 }
 
 func (r *randRes[N]) Offer(ctx context.Context, t time.Time, n N, a attribute.Set) {
-	// TODO: fix overflow error.
-	r.count++
-	if int(r.count) <= cap(r.store) {
-		r.store[r.count-1] = newMeasurement(ctx, t, n, a)
-		return
-	}
+	// The following algorithm is "Algorithm L" from Li, Kim-Hung (4 December
+	// 1994). "Reservoir-Sampling Algorithms of Time Complexity
+	// O(n(1+log(N/n)))". ACM Transactions on Mathematical Software. 20 (4):
+	// 481–493 (https://dl.acm.org/doi/10.1145/198429.198435).
+	//
+	// It is used because of its balance of simplicity and performance. In
+	// particular it has an asymptotic runtime of O(k(1 + log(n/k)) where n is
+	// the number of measurements offered and k is the reservoir size. This is
+	// much more optimal for large measurement sets than the algorithm
+	// recommended by the OTel spcification ("Algorithm R" as described in
+	// Vitter, Jeffrey S. (1 March 1985). "Random sampling with a reservoir"
+	// (http://www.cs.umd.edu/~samir/498/vitter.pdf)) which has an asymptotic
+	// runtime of O(n).
 
-	j := int(rng.Int63n(r.count))
-	if j < cap(r.store) {
-		r.store[j] = newMeasurement(ctx, t, n, a)
+	if int(r.count) < cap(r.store) {
+		r.store[r.count] = newMeasurement(ctx, t, n, a)
+	} else {
+		if r.count == r.next {
+			idx := int(rng.Int63n(int64(cap(r.store))))
+			r.store[idx] = newMeasurement(ctx, t, n, a)
+			r.advance()
+		}
 	}
+	r.count++
+}
+
+func (r *randRes[N]) reset() {
+	r.count = 0
+	r.next = int64(cap(r.store))
+	r.w = math.Exp(math.Log(rng.Float64()) / float64(cap(r.store)))
+	r.advance()
+}
+
+func (r *randRes[N]) advance() {
+	r.w *= math.Exp(math.Log(rng.Float64()) / float64(cap(r.store)))
+	r.next += int64(math.Log(rng.Float64())/math.Log(1-r.w)) + 1
 }
 
 func (r *randRes[N]) Collect(dest *[]metricdata.Exemplar[N], attrs attribute.Set) {
 	r.fixedRes.Collect(dest, attrs)
-	r.count = 0
+	r.reset()
 }
 
 func (r *randRes[N]) Flush(dest *[]metricdata.Exemplar[N], attrs attribute.Set) {
 	r.fixedRes.Flush(dest, attrs)
-	r.count = 0
+	r.reset()
 }
--- sdk/metric/internal/exemplar/rand_test.go
+++ sdk/metric/internal/exemplar/rand_test.go
@@ -14,7 +14,14 @@
 
 package exemplar
 
-import "testing"
+import (
+	"context"
+	"math"
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestFixedSize(t *testing.T) {
 	t.Run("Int64", testReservoir[int64](func(n int) (Reservoir[int64], int) {
@@ -24,4 +31,29 @@ func TestFixedSize(t *testing.T) {
 	t.Run("Float64", testReservoir[float64](func(n int) (Reservoir[float64], int) {
 		return FixedSize[float64](n), n
 	}))
+}
+
+func TestFixedSizeSamplingCorrectness(t *testing.T) {
+	intensity := 0.1
+	sampleSize := 1000
+
+	data := make([]float64, sampleSize*1000)
+	for i := range data {
+		data[i] = (-1.0 / intensity) * math.Log(rng.Float64())
+	}
+	// Sort to avoid position bias.
+	sort.Float64s(data)
+
+	r := FixedSize[float64](sampleSize)
+	for _, value := range data {
+		r.Offer(context.Background(), staticTime, value, alice)
+	}
+
+	var sum float64
+	for _, m := range r.(*randRes[float64]).store {
+		sum += m.Value
+	}
+	mean := sum / float64(sampleSize)
+
+	assert.InDelta(t, 1/mean, intensity, 0.01)
 }
