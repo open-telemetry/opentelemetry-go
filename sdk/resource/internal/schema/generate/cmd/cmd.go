163418220880fc511c0e336c7b64b6fad196904d
Tyler Yahn <codingalias@gmail.com>
2023-09-15 10:36:35 -0700

    Merge generate/cmd into just cmd.go

--- sdk/resource/internal/schema/generate/cmd/cmd.go
+++ sdk/resource/internal/schema/generate/cmd/cmd.go
@@ -15,9 +15,30 @@
 package cmd // import "go.opentelemetry.io/otel/sdk/resource/internal/schema/generate/cmd"
 
 import (
+	"bytes"
+	"embed"
 	"fmt"
+	"go/format"
+	"io"
+	"net/http"
 	"os"
+	"sort"
+	"text/template"
+
+	"github.com/Masterminds/semver/v3"
+	ast10 "go.opentelemetry.io/otel/schema/v1.0/ast"
+	sUtil "go.opentelemetry.io/otel/schema/v1.1"
+	"go.opentelemetry.io/otel/schema/v1.1/ast"
+	"go.opentelemetry.io/otel/schema/v1.1/types"
 )
+
+var schemaURL = "https://opentelemetry.io/schemas/1.21.0"
+
+// Template source.
+const src = "transforms.go.tmpl"
+
+//go:embed transforms.go.tmpl
+var tmpl embed.FS
 
 // Run runs the loads the OpenTelemetry schema and renders the appropriate code
 // to dest.
@@ -38,4 +59,112 @@ func Run(dest, local string) error {
 		return fmt.Errorf("failed to render template: %w", err)
 	}
 	return nil
+}
+
+type entry struct {
+	Version   *semver.Version
+	All       ast10.Attributes
+	Resources ast10.Attributes
+}
+
+func newEntry(ver types.TelemetryVersion, def ast.VersionDef) (entry, error) {
+	v, err := semver.NewVersion(string(ver))
+	return entry{
+		Version:   v,
+		All:       def.All,
+		Resources: def.Resources,
+	}, err
+}
+
+func entries(s *ast.Schema) ([]entry, error) {
+	if s == nil {
+		return nil, nil
+	}
+
+	es := make([]entry, 0, len(s.Versions))
+	for v, def := range s.Versions {
+		e, err := newEntry(v, def)
+		if err != nil {
+			return nil, err
+		}
+
+		es = append(es, e)
+	}
+
+	sort.Slice(es, func(i, j int) bool {
+		return es[i].Version.LessThan(es[j].Version)
+	})
+
+	return es, nil
+}
+
+func load(local string) (any, error) {
+	var (
+		data []entry
+		err  error
+	)
+	if local != "" {
+		data, err = loadLocal(local)
+	} else {
+		data, err = loadRemote()
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func loadLocal(local string) (data []entry, err error) {
+	f, err := os.Open(local)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	s, err := sUtil.Parse(f)
+	if err != nil {
+		return nil, err
+	}
+	return entries(s)
+}
+
+func loadRemote() (data []entry, err error) {
+	resp, err := http.Get(schemaURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("request error: %d", resp.StatusCode)
+	}
+
+	s, err := sUtil.Parse(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return entries(s)
+}
+
+func render(dest io.Writer, data any) error {
+	t, err := template.ParseFS(tmpl, src)
+	if err != nil {
+		return err
+	}
+
+	var out bytes.Buffer
+	err = t.Execute(&out, data)
+	if err != nil {
+		return err
+	}
+
+	src, err := format.Source(out.Bytes())
+	if err != nil {
+		return err
+	}
+
+	_, err = dest.Write(src)
+	return err
 }
