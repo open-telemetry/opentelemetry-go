714dc911e4b96f98fe7b7d61fa16a531c2748541
Robert Pająk <pellared@hotmail.com>
2023-09-21 17:16:52 +0200

    Address comments

--- sdk/metric/example_test.go
+++ sdk/metric/example_test.go
@@ -65,11 +65,11 @@ func Example() {
 		}
 	}()
 
-	// Register as global meter provider so that it can
-	// that can used via otel.Meter and accessed using otel.GetMeterProvider.
+	// Register as global meter provider so that it can be used via otel.Meter
+	// and accessed using otel.GetMeterProvider.
 	// Most instrumentation libraries use the global meter provider as default.
 	// If the global meter provider is not set then a no-op implementation
-	// is used and which fails to generate data.
+	// is used, which fails to generate data.
 	otel.SetMeterProvider(meterProvider)
 }
 
