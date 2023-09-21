99e448f22a7914ba57957c8dcbc92f369625319f
Robert Pająk <pellared@hotmail.com>
2023-09-21 21:50:24 +0200

    Update example/dice/otel.go
    Co-authored-by: Tyler Yahn <MrAlias@users.noreply.github.com>

--- example/dice/otel.go
+++ example/dice/otel.go
@@ -29,7 +29,7 @@ import (
 )
 
 // setupOTelSDK bootstraps the OpenTelemetry pipeline.
-// If it does not return an error, ake sure to call shutdown for proper cleanup.
+// If it does not return an error, make sure to call shutdown for proper cleanup.
 func setupOTelSDK(ctx context.Context, serviceName, serviceVersion string) (shutdown func(context.Context) error, err error) {
 	var shutdownFuncs []func(context.Context) error
 
