30cd9e8f9512ca885cc66b428dc8bda5f601ace9
Robert Pająk <pellared@hotmail.com>
2023-09-22 15:50:45 +0200

    Update doc.go

--- example/dice/doc.go
+++ example/dice/doc.go
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// dice is the "Roll the dice" getting started example application.
+// Dice is the "Roll the dice" getting started example application.
 package main
