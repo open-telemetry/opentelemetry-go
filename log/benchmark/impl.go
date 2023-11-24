700c91f4d2a0d7d655acbf84718fd2f57df25af7
Robert Pająk <pellared@hotmail.com>
2023-11-24 14:46:29 +0100

    Add missing vanity import

--- log/benchmark/impl.go
+++ log/benchmark/impl.go
@@ -5,7 +5,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package benchmark
+package benchmark // import "go.opentelemetry.io/otel/log/benchmark"
 
 import (
 	"context"
