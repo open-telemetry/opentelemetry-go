1d571d5f927aaeaf57caf326e5748e831c340488
Tyler Yahn <codingalias@gmail.com>
2023-12-15 11:02:11 -0800

    Update default fixed bucket exemplar sizes

--- sdk/metric/exemplar.go
+++ sdk/metric/exemplar.go
@@ -16,6 +16,7 @@ package metric // import "go.opentelemetry.io/otel/sdk/metric"
 
 import (
 	"os"
+	"runtime"
 
 	"go.opentelemetry.io/otel/sdk/metric/internal/exemplar"
 	"go.opentelemetry.io/otel/sdk/metric/internal/x"
@@ -47,12 +48,10 @@ func reservoirFunc[N int64 | float64](agg Aggregation) func() exemplar.Reservoir
 		fltr = exemplar.TraceBasedSample[N]
 	}
 
-	// TODO: This is not defined by the specification, nor is the mechanism to
-	// configure it.
-	const defaultFixedSize = 1
-
 	// https://github.com/open-telemetry/opentelemetry-specification/blob/d4b241f451674e8f611bb589477680341006ad2b/specification/metrics/sdk.md#exemplar-defaults
 	resF := func() func() exemplar.Reservoir[N] {
+		// Explicit bucket histogram aggregation with more than 1 bucket will
+		// use AlignedHistogramBucketExemplarReservoir.
 		a, ok := agg.(AggregationExplicitBucketHistogram)
 		if ok && len(a.Boundaries) > 1 {
 			cp := make([]float64, len(a.Boundaries))
@@ -63,8 +62,32 @@ func reservoirFunc[N int64 | float64](agg Aggregation) func() exemplar.Reservoir
 			}
 		}
 
+		var n int
+		if a, ok := agg.(AggregationBase2ExponentialHistogram); ok {
+			// Base2 Exponential Histogram Aggregation SHOULD use a
+			// SimpleFixedSizeExemplarReservoir with a reservoir equal to the
+			// smaller of the maximum number of buckets configured on the
+			// aggregation or twenty (e.g. min(20, max_buckets)).
+			n = int(a.MaxSize)
+			if n > 20 {
+				n = 20
+			}
+		} else {
+			// https://github.com/open-telemetry/opentelemetry-specification/blob/e94af89e3d0c01de30127a0f423e912f6cda7bed/specification/metrics/sdk.md#simplefixedsizeexemplarreservoir
+			//   This Exemplar reservoir MAY take a configuration parameter for
+			//   the size of the reservoir. If no size configuration is
+			//   provided, the default size MAY be the number of possible
+			//   concurrent threads (e.g. numer of CPUs) to help reduce
+			//   contention. Otherwise, a default size of 1 SHOULD be used.
+			n = runtime.NumCPU()
+			if n < 1 {
+				// Should never be the case, but be defensive.
+				n = 1
+			}
+		}
+
 		return func() exemplar.Reservoir[N] {
-			return exemplar.FixedSize[N](defaultFixedSize)
+			return exemplar.FixedSize[N](n)
 		}
 	}()
 
