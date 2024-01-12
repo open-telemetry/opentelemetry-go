e18a523fcc5fc19cd02817decfb79d7f5806d36a
Elena Nuretdinova <elena.nuretdinova@booking.com>
2024-01-12 16:41:38 +0100

    Add comment & fix lint

--- exporters/otlp/otlpmetric/otlpmetrichttp/internal/otest/collector.go
+++ exporters/otlp/otlpmetric/otlpmetrichttp/internal/otest/collector.go
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package otest
+package otest // import "go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp/internal/otest"
 
 import (
 	"bytes"
--- exporters/otlp/otlptrace/otlptracehttp/internal/otlpconfig/options.go
+++ exporters/otlp/otlptrace/otlptracehttp/internal/otlpconfig/options.go
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package otlpconfig
+package otlpconfig // import "go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp/internal/otlpconfig"
 
 import (
 	"crypto/tls"
