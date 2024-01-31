8fd68f674a14e740cca8944e8ad6b57d950d5ff6
sanket <telisanket2002@gmail.com>
2024-02-01 01:50:40 +0530

    add experimental features to doc.go for better visibility

--- sdk/metric/doc.go
+++ sdk/metric/doc.go
@@ -41,7 +41,39 @@
 // should be used to describe the unique runtime environment instrumented code
 // is being run on. That way when multiple instances of the code are collected
 // at a single endpoint their origin is decipherable.
-//
+
+
+
+//    Experimental Features
+
+// The metric SDK contains features that have not yet stabilized in the OpenTelemetry specification. These features are added to the OpenTelemetry Go metric SDK prior to stabilization in the specification so that users can start experimenting with them and provide feedback.
+
+// These feature may change in backwards incompatible ways as feedback is applied. See the Compatibility and Stability section for more information.
+
+// Features
+// Cardinality Limit
+// Cardinality Limit
+// The cardinality limit is the hard limit on the number of metric streams that can be collected for a single instrument.
+
+// This experimental feature can be enabled by setting the OTEL_GO_X_CARDINALITY_LIMIT environment value. The value must be an integer value. All other values are ignored.
+
+// If the value set is less than or equal to 0, no limit will be applied.
+
+// Examples
+// Set the cardinality limit to 2000.
+
+// export OTEL_GO_X_CARDINALITY_LIMIT=2000
+// Set an infinite cardinality limit (functionally equivalent to disabling the feature).
+
+// export OTEL_GO_X_CARDINALITY_LIMIT=-1
+// Disable the cardinality limit.
+
+// unset OTEL_GO_X_CARDINALITY_LIMIT
+// Compatibility and Stability
+// Experimental features do not fall within the scope of the OpenTelemetry Go versioning and stability policy. These features may be removed or modified in successive version releases, including patch versions.
+
+// When an experimental feature is promoted to a stable feature, a migration path will be included in the changelog entry of the release. There is no guarantee that any environment variable feature flags that enabled the experimental feature will be supported by the stable version. If they are supported, they may be accompanied with a deprecation notice stating a timeline for the removal of that support.
+
 // See [go.opentelemetry.io/otel/metric] for more information about
 // the metric API.
 package metric // import "go.opentelemetry.io/otel/sdk/metric"
