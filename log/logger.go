d3dcb3999c5689a7bb803cb0529e55a651ed14f1
Tyler Yahn <MrAlias@users.noreply.github.com>
2024-02-16 07:09:58 -0800

    Add initial Logs Bridge API scaffolding (#4907)
    * Add go.mod
    
    * Exclude otel/log in versions.yaml
    
    * Add package documentation stub
    
    * Update dependabot config
    
    * Add initial log API scaffolding

---
+++ log/logger.go
@@ -0,0 +1,65 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package log // import "go.opentelemetry.io/otel/log"
+
+import (
+	"context"
+
+	"go.opentelemetry.io/otel/attribute"
+)
+
+// Logger emits log records.
+//
+// Warning: Methods may be added to this interface in minor releases. See
+// package documentation on API implementation for information on how to set
+// default behavior for unimplemented methods.
+type Logger interface {
+	// TODO (#4909): embed an embedded type from otel/log/embedded.
+
+	// Emit emits a log record.
+	//
+	// The record may be held by the implementation. Callers should not mutate
+	// the record after passed.
+	//
+	// Implementations of this method need to be safe for a user to call
+	// concurrently.
+	Emit(ctx context.Context, record Record)
+}
+
+// LoggerOption applies configuration options to a [Logger].
+type LoggerOption interface {
+	// applyLogger is used to set a LoggerOption value of a LoggerConfig.
+	applyLogger(LoggerConfig) LoggerConfig
+}
+
+// LoggerConfig contains options for a [Logger].
+type LoggerConfig struct {
+	// Ensure forward compatibility by explicitly making this not comparable.
+	noCmp [0]func() //nolint: unused  // This is indeed used.
+}
+
+// NewLoggerConfig returns a new [LoggerConfig] with all the opts applied.
+func NewLoggerConfig(opts ...LoggerOption) LoggerConfig { return LoggerConfig{} } // TODO (#4911): implement.
+
+// InstrumentationVersion returns the version of the library providing
+// instrumentation.
+func (cfg LoggerConfig) InstrumentationVersion() string { return "" } // TODO (#4911): implement.
+
+// InstrumentationAttributes returns the attributes associated with the library
+// providing instrumentation.
+func (cfg LoggerConfig) InstrumentationAttributes() attribute.Set { return attribute.NewSet() } // TODO (#4911): implement.
+
+// SchemaURL returns the schema URL of the library providing instrumentation.
+func (cfg LoggerConfig) SchemaURL() string { return "" } // TODO (#4911): implement.
