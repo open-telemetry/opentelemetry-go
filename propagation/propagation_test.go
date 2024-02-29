ec8e6ea61fa0815f7938ff947d884802c188ffb8
Tyler Yahn <MrAlias@users.noreply.github.com>
2024-02-28 23:36:03 -0800

    Use slices.Sort in propagators test instead of sort.Slice (#4991)

--- propagation/propagation_test.go
+++ propagation/propagation_test.go
@@ -5,7 +5,7 @@ package propagation_test
 
 import (
 	"context"
-	"sort"
+	"slices"
 	"strings"
 	"testing"
 
@@ -119,6 +119,6 @@ func TestMapCarrierKeys(t *testing.T) {
 	}
 
 	keys := carrier.Keys()
-	sort.Strings(keys)
+	slices.Sort(keys)
 	assert.Equal(t, []string{"baz", "foo"}, keys)
 }
