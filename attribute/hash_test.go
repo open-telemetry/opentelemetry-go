55e3717298e33edd7a4c43252d4881a2c5bb500f
Tyler Yahn <codingalias@gmail.com>
2024-03-05 11:39:22 -0800

    Simplify hash_test

--- attribute/hash_test.go
+++ attribute/hash_test.go
@@ -11,43 +11,29 @@ import (
 	"go.opentelemetry.io/otel/attribute/internal/fnv"
 )
 
-type generator[T any] struct {
-	New   func(string, T) KeyValue
-	Value T
-}
-
-func (g generator[T]) Build(key string) KeyValue {
-	return g.New(key, g.Value)
-}
-
-type builder interface {
-	Build(key string) KeyValue
-}
-
 // keyVals is all the KeyValue generators that are used for testing. This is
-// not []KeyValue so tests detect if internal fields of the KeyValue that
-// differ for different instances are used in the hashing.
-var keyVals = []builder{
-	generator[bool]{Bool, true},
-	generator[bool]{Bool, false},
-	generator[[]bool]{BoolSlice, []bool{false, true}},
-	generator[[]bool]{BoolSlice, []bool{true, true, false}},
-	generator[int]{Int, -1278},
-	generator[int]{Int, 0}, // Should be different than false above.
-	generator[[]int]{IntSlice, []int{3, 23, 21, -8, 0}},
-	generator[[]int]{IntSlice, []int{1}},
-	generator[int64]{Int64, 1}, // Should be different from true and []int{1}.
-	generator[int64]{Int64, 29369},
-	generator[[]int64]{Int64Slice, []int64{3826, -38, -29, -1}},
-	generator[[]int64]{Int64Slice, []int64{8, -328, 29, 0}},
-	generator[float64]{Float64, -0.3812381},
-	generator[float64]{Float64, 1e32},
-	generator[[]float64]{Float64Slice, []float64{0.1, -3.8, -29., 0.3321}},
-	generator[[]float64]{Float64Slice, []float64{-13e8, -32.8, 4., 1e28}},
-	generator[string]{String, "foo"},
-	generator[string]{String, "bar"},
-	generator[[]string]{StringSlice, []string{"foo", "bar", "baz"}},
-	generator[[]string]{StringSlice, []string{"[]i1"}},
+// not []KeyValue so different keys can be used with the test Values.
+var keyVals = []func(string) KeyValue{
+	func(k string) KeyValue { return Bool(k, true) },
+	func(k string) KeyValue { return Bool(k, false) },
+	func(k string) KeyValue { return BoolSlice(k, []bool{false, true}) },
+	func(k string) KeyValue { return BoolSlice(k, []bool{true, true, false}) },
+	func(k string) KeyValue { return Int(k, -1278) },
+	func(k string) KeyValue { return Int(k, 0) }, // Should be different than false above.
+	func(k string) KeyValue { return IntSlice(k, []int{3, 23, 21, -8, 0}) },
+	func(k string) KeyValue { return IntSlice(k, []int{1}) },
+	func(k string) KeyValue { return Int64(k, 1) }, // Should be different from true and []int{1}.
+	func(k string) KeyValue { return Int64(k, 29369) },
+	func(k string) KeyValue { return Int64Slice(k, []int64{3826, -38, -29, -1}) },
+	func(k string) KeyValue { return Int64Slice(k, []int64{8, -328, 29, 0}) },
+	func(k string) KeyValue { return Float64(k, -0.3812381) },
+	func(k string) KeyValue { return Float64(k, 1e32) },
+	func(k string) KeyValue { return Float64Slice(k, []float64{0.1, -3.8, -29., 0.3321}) },
+	func(k string) KeyValue { return Float64Slice(k, []float64{-13e8, -32.8, 4., 1e28}) },
+	func(k string) KeyValue { return String(k, "foo") },
+	func(k string) KeyValue { return String(k, "bar") },
+	func(k string) KeyValue { return StringSlice(k, []string{"foo", "bar", "baz"}) },
+	func(k string) KeyValue { return StringSlice(k, []string{"[]i1"}) },
 }
 
 func TestHashKVsEquality(t *testing.T) {
@@ -66,23 +52,23 @@ func TestHashKVsEquality(t *testing.T) {
 
 	for _, key := range keys {
 		for i := 0; i < len(keyVals); i++ {
-			kvs := []KeyValue{keyVals[i].Build(key)}
+			kvs := []KeyValue{keyVals[i](key)}
 			hash := hashKVs(kvs)
 			result = append(result, testcase{hash, kvs})
 
 			for j := 0; j < len(keyVals); j++ {
 				kvs := []KeyValue{
-					keyVals[i].Build(key),
-					keyVals[j].Build(key),
+					keyVals[i](key),
+					keyVals[j](key),
 				}
 				hash := hashKVs(kvs)
 				result = append(result, testcase{hash, kvs})
 
 				for k := 0; k < len(keyVals); k++ {
 					kvs := []KeyValue{
-						keyVals[i].Build(key),
-						keyVals[j].Build(key),
-						keyVals[k].Build(key),
+						keyVals[i](key),
+						keyVals[j](key),
+						keyVals[k](key),
 					}
 					hash := hashKVs(kvs)
 					result = append(result, testcase{hash, kvs})
@@ -149,7 +135,7 @@ func (m msg) slice(kvs []KeyValue) string {
 func BenchmarkHashKVs(b *testing.B) {
 	attrs := make([]KeyValue, len(keyVals))
 	for i := range keyVals {
-		attrs[i] = keyVals[i].Build("k")
+		attrs[i] = keyVals[i]("k")
 	}
 
 	var h fnv.Hash
