d3c396a015d49f8ed5f360d5a6f7968a1d1a0088
Robert Pająk <pellared@hotmail.com>
2024-03-08 09:29:34 +0100

    Refine exporter docs

--- sdk/log/exporter.go
+++ sdk/log/exporter.go
@@ -18,17 +18,19 @@ import (
 	"context"
 )
 
-// Exporter handles the delivery of log records to external receivers. This is
-// the final component in the log export pipeline.
+// Exporter handles the delivery of log records to external receivers.
+//
+// Any of the Exporter's methods may be called concurrently with itself
+// or with other methods. It is the responsibility of the Exporter to manage
+// this concurrency.
 type Exporter interface {
 	// DO NOT CHANGE: any modification will not be backwards compatible and
 	// must never be done outside of a new major release.
 
 	// Export transmits log records to a receiver.
 	//
-	// This is called synchronously, there is no concurrency safety
-	// requirement. Because of this, it is critical that all timeouts and
-	// cancellations of the passed context be honored.
+	// The deadline or cancellation of the passed context must be honored. An
+	// appropriate error should be returned in these situations.
 	//
 	// All retry logic must be contained in this function. The SDK does not
 	// implement any retry logic. All errors returned by this function are
@@ -36,6 +38,9 @@ type Exporter interface {
 	// Handler.
 	//
 	// Implementations must not retain the records slice.
+	//
+	// Implementations should consider cloning the records before modifying
+	// them to avoid possible data races.
 	Export(ctx context.Context, records []*Record) error
 	// DO NOT CHANGE: any modification will not be backwards compatible and
 	// must never be done outside of a new major release.
@@ -43,17 +48,20 @@ type Exporter interface {
 	// Shutdown is called when the SDK shuts down. Any cleanup or release of
 	// resources held by the exporter should be done in this call.
 	//
-	// Calls Export, Shutdown, or ForceFlush after this has been called
-	// should be ignore and return nil error.
+	// The deadline or cancellation of the passed context must be honored. An
+	// appropriate error should be returned in these situations.
 	//
-	// All timeouts and cancellations contained in ctx must be honored, this
-	// should not block indefinitely.
+	// After Shutdown is called, calls to Export, Shutdown, or ForceFlush
+	// should perform no operation and return nil error.
 	Shutdown(ctx context.Context) error
 	// DO NOT CHANGE: any modification will not be backwards compatible and
 	// must never be done outside of a new major release.
 
 	// ForceFlush exports log records to the configured Exporter that have not yet
 	// been exported.
+	//
+	// The deadline or cancellation of the passed context must be honored. An
+	// appropriate error should be returned in these situations.
 	ForceFlush(ctx context.Context) error
 	// DO NOT CHANGE: any modification will not be backwards compatible and
 	// must never be done outside of a new major release.
