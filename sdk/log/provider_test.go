9927047a0da3fad007abb460d638e53da62f38b5
Tyler Yahn <codingalias@gmail.com>
2024-03-14 08:12:37 -0700

    Add test for NewLoggerProvider configuration

---
+++ sdk/log/provider_test.go
@@ -0,0 +1,120 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package log // import "go.opentelemetry.io/otel/sdk/log"
+
+import (
+	"context"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/sdk/resource"
+)
+
+type processor struct {
+	name string
+}
+
+func (processor) OnEmit(context.Context, Record) error { return nil }
+func (processor) Shutdown(context.Context) error       { return nil }
+func (processor) ForceFlush(context.Context) error     { return nil }
+
+func TestNewLoggerProviderConfiguration(t *testing.T) {
+	t.Cleanup(func(orig otel.ErrorHandler) func() {
+		otel.SetErrorHandler(otel.ErrorHandlerFunc(func(err error) {
+			t.Log(err)
+		}))
+		return func() { otel.SetErrorHandler(orig) }
+	}(otel.GetErrorHandler()))
+
+	res := resource.NewSchemaless(attribute.String("key", "value"))
+	p0, p1 := processor{name: "0"}, processor{name: "1"}
+	attrCntLim := 12
+	attrValLenLim := 21
+
+	testcases := []struct {
+		name    string
+		envars  map[string]string
+		options []LoggerProviderOption
+		want    *LoggerProvider
+	}{
+		{
+			name: "Defaults",
+			want: &LoggerProvider{
+				resource:                  resource.Default(),
+				attributeCountLimit:       defaultAttrCntLim,
+				attributeValueLengthLimit: defaultAttrValLenLim,
+			},
+		},
+		{
+			name: "Options",
+			options: []LoggerProviderOption{
+				WithResource(res),
+				WithProcessor(p0),
+				WithProcessor(p1),
+				WithAttributeCountLimit(attrCntLim),
+				WithAttributeValueLengthLimit(attrValLenLim),
+			},
+			want: &LoggerProvider{
+				resource:                  res,
+				processors:                []Processor{p0, p1},
+				attributeCountLimit:       attrCntLim,
+				attributeValueLengthLimit: attrValLenLim,
+			},
+		},
+		{
+			name: "Environment",
+			envars: map[string]string{
+				envarAttrCntLim:    strconv.Itoa(attrCntLim),
+				envarAttrValLenLim: strconv.Itoa(attrValLenLim),
+			},
+			want: &LoggerProvider{
+				resource:                  resource.Default(),
+				attributeCountLimit:       attrCntLim,
+				attributeValueLengthLimit: attrValLenLim,
+			},
+		},
+		{
+			name: "InvalidEnvironment",
+			envars: map[string]string{
+				envarAttrCntLim:    "invalid attributeCountLimit",
+				envarAttrValLenLim: "invalid attributeValueLengthLimit",
+			},
+			want: &LoggerProvider{
+				resource:                  resource.Default(),
+				attributeCountLimit:       defaultAttrCntLim,
+				attributeValueLengthLimit: defaultAttrValLenLim,
+			},
+		},
+		{
+			name: "Precedence",
+			envars: map[string]string{
+				envarAttrCntLim:    strconv.Itoa(100),
+				envarAttrValLenLim: strconv.Itoa(101),
+			},
+			options: []LoggerProviderOption{
+				// These override the environment variables.
+				WithAttributeCountLimit(attrCntLim),
+				WithAttributeValueLengthLimit(attrValLenLim),
+			},
+			want: &LoggerProvider{
+				resource:                  resource.Default(),
+				attributeCountLimit:       attrCntLim,
+				attributeValueLengthLimit: attrValLenLim,
+			},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			for key, value := range tc.envars {
+				t.Setenv(key, value)
+			}
+			assert.Equal(t, tc.want, NewLoggerProvider(tc.options...))
+		})
+	}
+}
