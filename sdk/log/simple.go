e18608a48a89d8269bdd405370a0047c5b987d85
Tyler Yahn <codingalias@gmail.com>
2024-03-15 09:19:27 -0700

    Implement the SimpleProcessor

--- sdk/log/simple.go
+++ sdk/log/simple.go
@@ -5,13 +5,17 @@ package log // import "go.opentelemetry.io/otel/sdk/log"
 
 import (
 	"context"
+	"sync"
 )
 
 // Compile-time check SimpleProcessor implements Processor.
 var _ Processor = (*SimpleProcessor)(nil)
 
 // SimpleProcessor is an processor that synchronously exports log records.
-type SimpleProcessor struct{}
+type SimpleProcessor struct {
+	exporterMu sync.Mutex
+	exporter   Exporter
+}
 
 // NewSimpleProcessor is a simple Processor adapter.
 //
@@ -25,14 +29,18 @@ type SimpleProcessor struct{}
 // computation resource usage overhead. [NewBatchingProcessor] is recommended
 // for production use instead.
 func NewSimpleProcessor(exporter Exporter) *SimpleProcessor {
-	// TODO (#5062): Implement.
-	return nil
+	if exporter == nil {
+		// Do not panic on nil exporter.
+		exporter = noopExporter{}
+	}
+	return &SimpleProcessor{exporter: exporter}
 }
 
 // OnEmit batches provided log record.
 func (s *SimpleProcessor) OnEmit(ctx context.Context, r Record) error {
-	// TODO (#5062): Implement.
-	return nil
+	s.exporterMu.Lock()
+	defer s.exporterMu.Unlock()
+	return s.exporter.Export(ctx, []Record{r})
 }
 
 // Enabled returns true.
@@ -42,12 +50,14 @@ func (s *SimpleProcessor) Enabled(context.Context, Record) bool {
 
 // Shutdown shuts down the expoter.
 func (s *SimpleProcessor) Shutdown(ctx context.Context) error {
-	// TODO (#5062): Implement.
-	return nil
+	s.exporterMu.Lock()
+	defer s.exporterMu.Unlock()
+	return s.exporter.Shutdown(ctx)
 }
 
 // ForceFlush flushes the exporter.
 func (s *SimpleProcessor) ForceFlush(ctx context.Context) error {
-	// TODO (#5062): Implement.
-	return nil
+	s.exporterMu.Lock()
+	defer s.exporterMu.Unlock()
+	return s.exporter.ForceFlush(ctx)
 }
---
+++ sdk/log/simple_test.go
@@ -0,0 +1,94 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package log_test
+
+import (
+	"context"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"go.opentelemetry.io/otel/sdk/log"
+)
+
+type exporter struct {
+	records []log.Record
+
+	exportCalled     bool
+	shutdownCalled   bool
+	forceFlushCalled bool
+}
+
+func (e *exporter) Export(_ context.Context, r []log.Record) error {
+	e.records = r
+	e.exportCalled = true
+	return nil
+}
+
+func (e *exporter) Shutdown(context.Context) error {
+	e.shutdownCalled = true
+	return nil
+}
+
+func (e *exporter) ForceFlush(context.Context) error {
+	e.forceFlushCalled = true
+	return nil
+}
+
+func TestSimpleProcessorOnEmit(t *testing.T) {
+	e := new(exporter)
+	s := log.NewSimpleProcessor(e)
+
+	var r log.Record
+	r.SetSeverityText("test")
+	_ = s.OnEmit(context.Background(), r)
+
+	require.True(t, e.exportCalled, "exporter Export not called")
+	assert.Equal(t, []log.Record{r}, e.records)
+}
+
+func TestSimpleProcessorEnabled(t *testing.T) {
+	s := log.NewSimpleProcessor(nil)
+	assert.True(t, s.Enabled(context.Background(), log.Record{}))
+}
+
+func TestSimpleProcessorShutdown(t *testing.T) {
+	e := new(exporter)
+	s := log.NewSimpleProcessor(e)
+	_ = s.Shutdown(context.Background())
+	require.True(t, e.shutdownCalled, "exporter Shutdown not called")
+}
+
+func TestSimpleProcessorForceFlush(t *testing.T) {
+	e := new(exporter)
+	s := log.NewSimpleProcessor(e)
+	_ = s.ForceFlush(context.Background())
+	require.True(t, e.forceFlushCalled, "exporter ForceFlush not called")
+}
+
+func TestSimpleProcessorConcurrentSafe(t *testing.T) {
+	const goRoutineN = 10
+
+	var wg sync.WaitGroup
+	wg.Add(goRoutineN)
+
+	var r log.Record
+	r.SetSeverityText("test")
+	ctx := context.Background()
+	s := log.NewSimpleProcessor(nil)
+	for i := 0; i < goRoutineN; i++ {
+		go func() {
+			defer wg.Done()
+
+			_ = s.OnEmit(ctx, r)
+			_ = s.Enabled(ctx, r)
+			_ = s.Shutdown(ctx)
+			_ = s.ForceFlush(ctx)
+		}()
+	}
+
+	wg.Wait()
+}
