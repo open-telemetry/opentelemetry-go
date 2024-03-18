e76df8c9e6285d0c76ac6e2e37a8f17f138ca31d
Tyler Yahn <codingalias@gmail.com>
2024-03-18 14:31:13 -0700

    Shorten state_test.go

--- log/internal/global/state_test.go
+++ log/internal/global/state_test.go
@@ -13,61 +13,52 @@ import (
 	"go.opentelemetry.io/otel/log/noop"
 )
 
-func resetGlobalLoggerProvider() {
-	globalLoggerProvider = defaultLoggerProvider()
-	delegateLoggerOnce = sync.Once{}
-}
-
-type nonComparableLoggerProvider struct {
-	log.LoggerProvider
-
-	nonComparable [0]func() //nolint:structcheck,unused  // This is not called.
-}
-
 func TestSetLoggerProvider(t *testing.T) {
-	t.Cleanup(resetGlobalLoggerProvider)
+	reset := func() {
+		globalLoggerProvider = defaultLoggerProvider()
+		delegateLoggerOnce = sync.Once{}
+	}
 
 	t.Run("Set With default is a noop", func(t *testing.T) {
-		t.Cleanup(resetGlobalLoggerProvider)
+		t.Cleanup(reset)
 		SetLoggerProvider(GetLoggerProvider())
 
 		provider, ok := GetLoggerProvider().(*loggerProvider)
 		if !ok {
 			t.Fatal("Global GetLoggerProvider should be the default logger provider")
 		}
-
 		if provider.delegate != nil {
 			t.Fatal("logger provider should not delegate when setting itself")
 		}
 	})
 
 	t.Run("First Set() should replace the delegate", func(t *testing.T) {
-		t.Cleanup(resetGlobalLoggerProvider)
+		t.Cleanup(reset)
 
 		SetLoggerProvider(noop.NewLoggerProvider())
-
-		_, ok := GetLoggerProvider().(*loggerProvider)
-		if ok {
+		if _, ok := GetLoggerProvider().(*loggerProvider); ok {
 			t.Fatal("Global GetLoggerProvider was not changed")
 		}
 	})
 
 	t.Run("Set() should delegate existing Logger Providers", func(t *testing.T) {
-		t.Cleanup(resetGlobalLoggerProvider)
+		t.Cleanup(reset)
 
 		provider := GetLoggerProvider()
-
 		SetLoggerProvider(noop.NewLoggerProvider())
 
-		dmp := provider.(*loggerProvider)
-
-		if dmp.delegate == nil {
+		if del := provider.(*loggerProvider); del.delegate == nil {
 			t.Fatal("The delegated logger providers should have a delegate")
 		}
 	})
 
 	t.Run("non-comparable types should not panic", func(t *testing.T) {
-		t.Cleanup(resetGlobalLoggerProvider)
+		t.Cleanup(reset)
+
+		type nonComparableLoggerProvider struct {
+			log.LoggerProvider
+			noCmp [0]func() //nolint:structcheck,unused  // This is indeed used.
+		}
 
 		provider := nonComparableLoggerProvider{}
 		SetLoggerProvider(provider)
