ed479a543a3818e6b5adf43df4704771d874ca42
Tyler Yahn <codingalias@gmail.com>
2024-03-25 14:00:23 -0700

    Concurrent safe testExporter

--- sdk/log/exporter_test.go
+++ sdk/log/exporter_test.go
@@ -5,12 +5,19 @@ package log
 
 import (
 	"context"
+	"slices"
+	"sync/atomic"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+type instruction struct {
+	Record *[]Record
+	Flush  chan [][]Record
+}
 
 type testExporter struct {
 	// Err is the error returned by all methods of the testExporter.
@@ -20,14 +27,53 @@ type testExporter struct {
 	ExportTrigger chan struct{}
 
 	// Counts of method calls.
-	ExportN, ShutdownN, ForceFlushN int
-	// Records are the Records passed to export.
-	Records [][]Record
+	exportN, shutdownN, forceFlushN *int32
+
+	input chan instruction
+	done  chan struct{}
+}
+
+func newTestExporter(err error) *testExporter {
+	e := &testExporter{
+		Err:         err,
+		exportN:     new(int32),
+		shutdownN:   new(int32),
+		forceFlushN: new(int32),
+		input:       make(chan instruction),
+	}
+	e.done = run(e.input)
+
+	return e
+}
+
+func run(input chan instruction) chan struct{} {
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+
+		var records [][]Record
+		for in := range input {
+			if in.Record != nil {
+				records = append(records, *in.Record)
+			}
+			if in.Flush != nil {
+				cp := slices.Clone(records)
+				records = records[:0]
+				in.Flush <- cp
+			}
+		}
+	}()
+	return done
+}
+
+func (e *testExporter) Records() [][]Record {
+	out := make(chan [][]Record, 1)
+	e.input <- instruction{Flush: out}
+	return <-out
 }
 
 func (e *testExporter) Export(ctx context.Context, r []Record) error {
-	e.ExportN++
-	e.Records = append(e.Records, r)
+	atomic.AddInt32(e.exportN, 1)
 	if e.ExportTrigger != nil {
 		select {
 		case <-e.ExportTrigger:
@@ -35,35 +81,59 @@ func (e *testExporter) Export(ctx context.Context, r []Record) error {
 			return ctx.Err()
 		}
 	}
+	e.input <- instruction{Record: &r}
 	return e.Err
+}
+
+func (e *testExporter) ExportN() int {
+	return int(atomic.LoadInt32(e.exportN))
+}
+
+func (e *testExporter) Stop() {
+	close(e.input)
+	<-e.done
 }
 
 func (e *testExporter) Shutdown(ctx context.Context) error {
-	e.ShutdownN++
+	atomic.AddInt32(e.shutdownN, 1)
 	return e.Err
 }
 
+func (e *testExporter) ShutdownN() int {
+	return int(atomic.LoadInt32(e.shutdownN))
+}
+
 func (e *testExporter) ForceFlush(ctx context.Context) error {
-	e.ForceFlushN++
+	atomic.AddInt32(e.forceFlushN, 1)
 	return e.Err
+}
+
+func (e *testExporter) ForceFlushN() int {
+	return int(atomic.LoadInt32(e.forceFlushN))
 }
 
 func TestChunker(t *testing.T) {
 	t.Run("ForceFlush", func(t *testing.T) {
-		exp := &testExporter{}
+		exp := newTestExporter(nil)
+		t.Cleanup(exp.Stop)
 		_ = chunker{Exporter: exp}.ForceFlush(context.Background())
-		assert.Equal(t, 1, exp.ForceFlushN, "ForceFlush not passed through")
+		assert.Equal(t, 1, exp.ForceFlushN(), "ForceFlush not passed through")
 	})
 
 	t.Run("Shutdown", func(t *testing.T) {
-		exp := &testExporter{}
+		exp := newTestExporter(nil)
+		t.Cleanup(exp.Stop)
 		_ = chunker{Exporter: exp}.Shutdown(context.Background())
-		assert.Equal(t, 1, exp.ShutdownN, "Shutdown not passed through")
+		assert.Equal(t, 1, exp.ShutdownN(), "Shutdown not passed through")
 	})
 
 	t.Run("Timeout", func(t *testing.T) {
 		trigger := make(chan struct{})
-		exp := &testExporter{ExportTrigger: trigger}
+		t.Cleanup(func() { close(trigger) })
+
+		exp := newTestExporter(nil)
+		t.Cleanup(exp.Stop)
+		exp.ExportTrigger = trigger
 		c := chunker{Exporter: exp, Timeout: time.Nanosecond}
 
 		out := make(chan error, 1)
@@ -83,24 +153,26 @@ func TestChunker(t *testing.T) {
 
 		assert.ErrorIs(t, err, context.DeadlineExceeded)
 		close(out)
-		close(trigger)
 	})
 
 	t.Run("Chunk", func(t *testing.T) {
-		exp := &testExporter{}
+		exp := newTestExporter(nil)
+		t.Cleanup(exp.Stop)
 		c := chunker{Exporter: exp, Size: 10}
 		assert.NoError(t, c.Export(context.Background(), make([]Record, 5)))
 		assert.NoError(t, c.Export(context.Background(), make([]Record, 25)))
 
 		wantLens := []int{5, 10, 10, 5}
-		require.Len(t, exp.Records, len(wantLens), "chunks")
+		records := exp.Records()
+		require.Len(t, records, len(wantLens), "chunks")
 		for i, n := range wantLens {
-			assert.Lenf(t, exp.Records[i], n, "chunk %d", i)
+			assert.Lenf(t, records[i], n, "chunk %d", i)
 		}
 	})
 
 	t.Run("ExportError", func(t *testing.T) {
-		exp := &testExporter{Err: assert.AnError}
+		exp := newTestExporter(assert.AnError)
+		t.Cleanup(exp.Stop)
 		c := chunker{Exporter: exp}
 		ctx := context.Background()
 		records := make([]Record, 25)
