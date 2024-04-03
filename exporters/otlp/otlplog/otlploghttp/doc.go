1dbf009acbb97824e3a5a5f18efb5ecbe24884d0
Tyler Yahn <codingalias@gmail.com>
2024-04-03 15:02:19 -0700

    Add skeleton doc

---
+++ exporters/otlp/otlplog/otlploghttp/doc.go
@@ -0,0 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// Package otlploghttp provides an OTLP log exporter. The exporter uses HTTP to
+// transport OTLP protobuf payloads.
+package otlploghttp // import "go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp"
