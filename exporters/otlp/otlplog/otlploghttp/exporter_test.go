727f03e2204a4203dda75df0c719e792b66bd5df
Tyler Yahn <MrAlias@users.noreply.github.com>
2024-04-10 07:47:24 -0700

    otlploghttp: Implement Exporter.Export (#5182)

--- exporters/otlp/otlplog/otlploghttp/exporter_test.go
+++ exporters/otlp/otlplog/otlploghttp/exporter_test.go
@@ -5,6 +5,7 @@ package otlploghttp
 
 import (
 	"context"
+	"errors"
 	"runtime"
 	"sync"
 	"sync/atomic"
@@ -14,7 +15,62 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"go.opentelemetry.io/otel/sdk/log"
+	logpb "go.opentelemetry.io/proto/otlp/logs/v1"
 )
+
+func TestExporterExportErrors(t *testing.T) {
+	var (
+		errUpload = errors.New("upload")
+		errTForm  = errors.New("transform")
+	)
+
+	c := &client{
+		uploadLogs: func(context.Context, *logpb.ResourceLogs) error {
+			return errUpload
+		},
+	}
+
+	orig := transformResourceLogs
+	transformResourceLogs = func(r []log.Record) (*logpb.ResourceLogs, error) {
+		return new(logpb.ResourceLogs), errTForm
+	}
+	t.Cleanup(func() { transformResourceLogs = orig })
+
+	e, err := newExporter(c, config{})
+	require.NoError(t, err, "New")
+
+	err = e.Export(context.Background(), make([]log.Record, 1))
+	assert.ErrorIs(t, err, errUpload)
+	assert.ErrorIs(t, err, errTForm)
+}
+
+func TestExporterExport(t *testing.T) {
+	var uploads int
+	c := &client{
+		uploadLogs: func(context.Context, *logpb.ResourceLogs) error {
+			uploads++
+			return nil
+		},
+	}
+
+	orig := transformResourceLogs
+	var got []log.Record
+	transformResourceLogs = func(r []log.Record) (*logpb.ResourceLogs, error) {
+		got = r
+		return new(logpb.ResourceLogs), nil
+	}
+	t.Cleanup(func() { transformResourceLogs = orig })
+
+	e, err := newExporter(c, config{})
+	require.NoError(t, err, "New")
+
+	ctx := context.Background()
+	want := make([]log.Record, 1)
+	assert.NoError(t, e.Export(ctx, want))
+
+	assert.Equal(t, 1, uploads, "client UploadLogs calls")
+	assert.Equal(t, want, got, "transformed log records")
+}
 
 func TestExporterShutdown(t *testing.T) {
 	ctx := context.Background()
