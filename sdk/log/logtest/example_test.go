4df52f021385253d06dc62f71d03dea0ec589dcf
Robert Pająk <pellared@hotmail.com>
2024-04-22 12:37:31 +0200

    Add ExampleRecordBuilder

---
+++ sdk/log/logtest/example_test.go
@@ -0,0 +1,54 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// Package logtest is a testing helper package.
+package logtest_test
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	logapi "go.opentelemetry.io/otel/log"
+	"go.opentelemetry.io/otel/sdk/instrumentation"
+	"go.opentelemetry.io/otel/sdk/log"
+	"go.opentelemetry.io/otel/sdk/log/logtest"
+)
+
+func ExampleRecordBuilder() {
+	exp := exporter{os.Stdout}
+	b := logtest.RecordBuilder{}.SetInstrumentationScope(instrumentation.Scope{Name: "myapp"})
+	r1 := b.SetBody(logapi.StringValue("foo")).Record()
+	r2 := b.SetBody(logapi.StringValue("bar")).Record()
+
+	_ = exp.Export(context.Background(), []log.Record{r1, r2})
+
+	// Output:
+	// scope=myapp msg=foo
+	// scope=myapp msg=bar
+}
+
+// Compile time check exporter implements log.Exporter.
+var _ log.Exporter = exporter{}
+
+type exporter struct{ io.Writer }
+
+func (e exporter) Export(ctx context.Context, records []log.Record) error {
+	for i, r := range records {
+		if i != 0 {
+			e.Write([]byte("\n"))
+		}
+		fmt.Fprintf(e, "scope=%s msg=%s", r.InstrumentationScope().Name, r.Body().String())
+	}
+	return nil
+}
+
+func (e exporter) Shutdown(context.Context) error {
+	return nil
+}
+
+// appropriate error should be returned in these situations.
+func (e exporter) ForceFlush(context.Context) error {
+	return nil
+}
