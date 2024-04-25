fb697e8a8b3b672369284ca3b4cf0fa024d5305b
Sam Xie <sam@samxie.me>
2024-04-25 09:57:24 -0700

    Add otlploggrpc exporter skeleton (#5246)

---
+++ exporters/otlp/otlplog/otlploggrpc/doc.go
@@ -0,0 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// Package otlploggrpc provides an OTLP log exporter. The exporter uses gRPC to
+// transport OTLP protobuf payloads.
+package otlploggrpc // import "go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc"
