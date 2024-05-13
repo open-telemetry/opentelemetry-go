7e870313999a93c24f67d429627a27ed77d1a7b7
Tyler Yahn <codingalias@gmail.com>
2024-05-13 10:58:03 -0700

    Add logs to dice example
    Use slog to log a message on the server handler.

--- example/dice/rolldice.go
+++ example/dice/rolldice.go
@@ -4,20 +4,29 @@
 package main
 
 import (
+	"fmt"
 	"io"
-	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
 
+	"go.opentelemetry.io/contrib/bridges/otelslog"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/sdk/instrumentation"
 )
 
+const name = "rolldice"
+
 var (
-	tracer  = otel.Tracer("rolldice")
-	meter   = otel.Meter("rolldice")
+	tracer = otel.Tracer(name)
+	meter  = otel.Meter(name)
+	logger = otelslog.NewLogger(
+		otelslog.WithInstrumentationScope(instrumentation.Scope{
+			Name: name,
+		}),
+	)
 	rollCnt metric.Int64Counter
 )
 
@@ -37,12 +46,20 @@ func rolldice(w http.ResponseWriter, r *http.Request) {
 
 	roll := 1 + rand.Intn(6)
 
+	var msg string
+	if player := r.PathValue("player"); player != "" {
+		msg = fmt.Sprintf("%s is rolling the dice", player)
+	} else {
+		msg = "Anonymous player is rolling the dice"
+	}
+	logger.InfoContext(ctx, msg, "result", roll)
+
 	rollValueAttr := attribute.Int("roll.value", roll)
 	span.SetAttributes(rollValueAttr)
 	rollCnt.Add(ctx, 1, metric.WithAttributes(rollValueAttr))
 
 	resp := strconv.Itoa(roll) + "\n"
 	if _, err := io.WriteString(w, resp); err != nil {
-		log.Printf("Write failed: %v\n", err)
+		logger.ErrorContext(ctx, "Write failed", "error", err)
 	}
 }
