c1fc89919160522efccefcdb46a7fe8a273367da
Tyler Yahn <codingalias@gmail.com>
2024-05-21 12:32:58 -0700

    Add semconv/v1.26.0

---
+++ semconv/v1.26.0/trace.go
@@ -0,0 +1,8 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// Code generated from semantic convention specification. DO NOT EDIT.
+
+package semconv // import "go.opentelemetry.io/otel/semconv/v1.26.0"
+
+import "go.opentelemetry.io/otel/attribute"
