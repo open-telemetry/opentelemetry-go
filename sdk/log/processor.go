11e2351e00a23ffd2d0f690a2f5a3df68382decc
Robert Pająk <pellared@hotmail.com>
2024-06-03 17:59:17 +0200

    Update processor.go

--- sdk/log/processor.go
+++ sdk/log/processor.go
@@ -26,7 +26,10 @@ type Processor interface {
 	// considered unrecoverable and will be reported to a configured error
 	// Handler.
 	//
-	// Implementations must not modify the record after OnEmit returns.
+	// Implementations may synchronously modify the record so that the changes
+	// are visible in the next registered processor.
+	// Implementations must not modify the record asynchronously as [Record]
+	// is not concurrent safe.
 	OnEmit(ctx context.Context, record *Record) error
 	// Enabled returns whether the Processor will process for the given context
 	// and record.
@@ -42,6 +45,9 @@ type Processor interface {
 	// state. An implementation should default to returning true for an
 	// indeterminate state, but may return false if valid reasons in particular
 	// circumstances exist (e.g. performance, correctness).
+	//
+	// Before modifying a Record, the implementation must use Record.Clone
+	// to create a copy that shares no state with the original.
 	Enabled(ctx context.Context, record Record) bool
 	// Shutdown is called when the SDK shuts down. Any cleanup or release of
 	// resources held by the exporter should be done in this call.
