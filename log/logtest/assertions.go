3d301840a57e0be66ca743ff29b0f41f4f49bc56
dmathieu <damien.mathieu@elastic.co>
2024-06-12 09:04:52 +0200

    return a bool with the assertion result

--- log/logtest/assertions.go
+++ log/logtest/assertions.go
@@ -12,40 +12,49 @@ import (
 
 // AssertRecordEqual compares two log records, and fails the test if they are
 // not equal.
-func AssertRecordEqual(t testing.TB, want, got log.Record) {
+func AssertRecordEqual(t testing.TB, want, got log.Record) bool {
 	t.Helper()
 
 	if !want.Timestamp().Equal(got.Timestamp()) {
 		t.Errorf("Timestamp value is not equal:\nwant: %v\ngot:  %v", want.Timestamp(), got.Timestamp())
+		return false
 	}
 	if !want.ObservedTimestamp().Equal(got.ObservedTimestamp()) {
 		t.Errorf("ObservedTimestamp value is not equal:\nwant: %v\ngot:  %v", want.ObservedTimestamp(), got.ObservedTimestamp())
+		return false
 	}
 	if want.Severity() != got.Severity() {
 		t.Errorf("Severity value is not equal:\nwant: %v\ngot:  %v", want.Severity(), got.Severity())
+		return false
 	}
 	if want.SeverityText() != got.SeverityText() {
 		t.Errorf("SeverityText value is not equal:\nwant: %v\ngot:  %v", want.SeverityText(), got.SeverityText())
+		return false
 	}
-	assertBody(t, want.Body(), got)
+	if !assertBody(t, want.Body(), got) {
+		return false
+	}
 
 	var attrs []log.KeyValue
 	want.WalkAttributes(func(kv log.KeyValue) bool {
 		attrs = append(attrs, kv)
 		return true
 	})
-	assertAttributes(t, attrs, got)
+	return assertAttributes(t, attrs, got)
 }
 
-func assertBody(t testing.TB, want log.Value, r log.Record) {
+func assertBody(t testing.TB, want log.Value, r log.Record) bool {
 	t.Helper()
 	got := r.Body()
 	if !got.Equal(want) {
 		t.Errorf("Body value is not equal:\nwant: %v\ngot:  %v", want, got)
+		return false
 	}
+
+	return true
 }
 
-func assertAttributes(t testing.TB, want []log.KeyValue, r log.Record) {
+func assertAttributes(t testing.TB, want []log.KeyValue, r log.Record) bool {
 	t.Helper()
 	var got []log.KeyValue
 	r.WalkAttributes(func(kv log.KeyValue) bool {
@@ -54,5 +63,8 @@ func assertAttributes(t testing.TB, want []log.KeyValue, r log.Record) {
 	})
 	if !slices.EqualFunc(want, got, log.KeyValue.Equal) {
 		t.Errorf("Attributes are not equal:\nwant: %v\ngot:  %v", want, got)
+		return false
 	}
+
+	return true
 }
