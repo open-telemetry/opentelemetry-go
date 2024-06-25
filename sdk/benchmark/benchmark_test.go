a1d11ab7decfd6b546578153966af52701db6f78
David Ashpole <dashpole@google.com>
2024-06-25 16:54:59 +0000

    add noop benchmark tests
    BenchmarkOTELCounter-24                 	464676590	         5.082 ns/op	       0 B/op	       0 allocs/op
    BenchmarkOTELCounterWithLabel-24        	48377798	        51.73 ns/op	      16 B/op	       1 allocs/op
    BenchmarkNoOpOTELCounter-24             	1000000000	         1.901 ns/op	       0 B/op	       0 allocs/op
    BenchmarkNoOpOTELCounterWithLabel-24    	64543243	        34.60 ns/op	      16 B/op	       1 allocs/op

--- sdk/benchmark/benchmark_test.go
+++ sdk/benchmark/benchmark_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/metric/noop"
 	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 )
 
@@ -59,4 +60,30 @@ func otelCounter(b *testing.B) metric.Int64Counter {
 	counter, err := meter.Int64Counter("test_counter")
 	require.NoError(b, err)
 	return counter
+}
+
+func BenchmarkNoOpOTELCounter(b *testing.B) {
+	meter := noop.NewMeterProvider().Meter("test")
+	counter, err := meter.Int64Counter("test_counter")
+	require.NoError(b, err)
+	ctx := context.Background()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		counter.Add(ctx, 1)
+	}
+}
+
+func BenchmarkNoOpOTELCounterWithLabel(b *testing.B) {
+	meter := noop.NewMeterProvider().Meter("test")
+	counter, err := meter.Int64Counter("test_counter")
+	require.NoError(b, err)
+	ctx := context.Background()
+	attrSet := attribute.NewSet(attribute.String("tag1", "value1"))
+	attrOpt := metric.WithAttributeSet(attrSet)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		counter.Add(ctx, 1, attrOpt)
+	}
 }
