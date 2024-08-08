d4e9e0f450943b896b722396a3467e092636ae39
Tyler Yahn <codingalias@gmail.com>
2024-08-08 14:17:19 -0700

    Fix import comment for sdk/log/internal/x

--- sdk/log/internal/x/x.go
+++ sdk/log/internal/x/x.go
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // Package x contains support for OTel log SDK experimental features.
-package x // import "go.opentelemetry.io/otel/sdk/metric/internal/x"
+package x // import "go.opentelemetry.io/otel/sdk/log/internal/x"
 
 import (
 	"context"
