a5ab131e49daaf41b86fab8e857df88aea0373d3
Robert Pająk <pellared@hotmail.com>
2024-11-04 12:10:01 +0100

    log: Update package documentation

--- log/logger.go
+++ log/logger.go
@@ -28,6 +28,9 @@ type Logger interface {
 	//
 	// Implementations of this method need to be safe for a user to call
 	// concurrently.
+	//
+	// Notice: Emit is intended to be used be log bridges.
+	// Is should not be used for writing instrumentation.
 	Emit(ctx context.Context, record Record)
 
 	// Enabled returns whether the Logger emits for the given context and
