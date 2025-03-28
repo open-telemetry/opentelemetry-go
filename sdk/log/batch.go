12038f5a73fadf4c5eba99b4d5660acaa9afa42f
wmdanor <wmdanor@proton.me>
2025-03-28 18:28:03 +0000

    revert skip behaviour

--- sdk/log/batch.go
+++ sdk/log/batch.go
@@ -156,18 +156,18 @@ func (b *BatchProcessor) poll(interval time.Duration) (done chan struct{}) {
 				global.Warn("dropped log records", "dropped", d)
 			}
 
+			qLen := b.q.Len()
 			// Don't copy data from queue unless exporter can accept more, it is very expensive.
-			if b.exporter.IsQueueFull() {
-				continue
+			if !b.exporter.IsQueueFull() {
+				qLen = b.q.TryDequeue(buf, func(r []Record) bool {
+					ok := b.exporter.EnqueueExport(r)
+					if ok {
+						buf = slices.Clone(buf)
+					}
+					return ok
+				})
 			}
 
-			qLen := b.q.TryDequeue(buf, func(r []Record) bool {
-				ok := b.exporter.EnqueueExport(r)
-				if ok {
-					buf = slices.Clone(buf)
-				}
-				return ok
-			})
 			if qLen >= b.batchSize {
 				// There is another full batch ready. Immediately trigger
 				// another export attempt.
@@ -275,6 +275,13 @@ func newQueue(size int) *queue {
 		read:  r,
 		write: r,
 	}
+}
+
+func (q *queue) Len() int {
+	q.Lock()
+	defer q.Unlock()
+
+	return q.len
 }
 
 // Dropped returns the number of Records dropped during enqueueing since the
