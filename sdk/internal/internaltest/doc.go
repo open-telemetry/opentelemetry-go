6cee5525ec06f255fdcd8c0d56ffe09b6c87e7b4
stoo johnston <stoo@pulumi.com>
2025-04-02 17:13:22 +0100

    Remove harness from internaltest and add doc template

---
+++ sdk/internal/internaltest/doc.go
@@ -0,0 +1,8 @@
+// Code created by gotmpl. DO NOT MODIFY.
+// source: internal/shared/internaltest/harness.go.tmpl
+
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// Package internaltest provides testing functionality.
+package internaltest // import "go.opentelemetry.io/otel/sdk/internal/internaltest"
