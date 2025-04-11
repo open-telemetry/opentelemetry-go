d61f2dfa77ac6d81a953d3b82209ca0676e53d7c
stoo johnston <stoo@pulumi.com>
2025-04-11 10:18:08 +0100

    remove only package go doc comment

--- sdk/internal/internaltest/harness.go
+++ sdk/internal/internaltest/harness.go
@@ -4,7 +4,6 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-// Package internaltest provides testing functionality.
 package internaltest // import "go.opentelemetry.io/otel/sdk/internal/internaltest"
 
 import (
