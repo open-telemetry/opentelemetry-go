65fdbf47419d4de967224cdbc6b052e3b0a7952a
leoff00 <leof0180@gmail.com>
2025-04-14 17:10:28 -0300

    solve conflict

--- bridge/opentracing/mock_test.go
+++ bridge/opentracing/mock_test.go
@@ -1,37 +1,25 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package opentracing
+package opentracing // import "go.opentelemetry.io/otel/bridge/opentracing"
 
 import (
 	"context"
 	"math/rand"
-	"net"
 	"reflect"
 	"sync"
-	"testing"
 	"time"
-
-	otgrpc "github.com/opentracing-contrib/go-grpc"
-	testpb "github.com/opentracing-contrib/go-grpc/test/otgrpc_testing"
-	ot "github.com/opentracing/opentracing-go"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/bridge/opentracing/migration"
 	"go.opentelemetry.io/otel/codes"
-	"go.opentelemetry.io/otel/propagation"
 	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 	"go.opentelemetry.io/otel/trace"
 	"go.opentelemetry.io/otel/trace/embedded"
 	"go.opentelemetry.io/otel/trace/noop"
 )
 
-type testGRPCServer struct{}
-
+//nolint:revive // ignoring missing comments for unexported global variables in an internal package.
 var (
 	statusCodeKey    = attribute.Key("status.code")
 	statusMessageKey = attribute.Key("status.message")
@@ -56,6 +44,11 @@ type mockTracer struct {
 	randLock sync.Mutex
 	rand     *rand.Rand
 }
+
+var (
+	_ trace.Tracer                                  = &mockTracer{}
+	_ migration.DeferredContextSetupTracerExtension = &mockTracer{}
+)
 
 func newMockTracer() *mockTracer {
 	return &mockTracer{
@@ -203,6 +196,11 @@ type mockSpan struct {
 	Links        []mockLink
 }
 
+var (
+	_ trace.Span                            = &mockSpan{}
+	_ migration.OverrideTracerSpanExtension = &mockSpan{}
+)
+
 func (s *mockSpan) SpanContext() trace.SpanContext {
 	return s.spanContext
 }
@@ -304,68 +302,3 @@ func (s *mockSpan) OverrideTracer(tracer trace.Tracer) {
 }
 
 func (s *mockSpan) TracerProvider() trace.TracerProvider { return noop.NewTracerProvider() }
-
-func (*testGRPCServer) UnaryCall(ctx context.Context, r *testpb.SimpleRequest) (*testpb.SimpleResponse, error) {
-	return &testpb.SimpleResponse{Payload: r.Payload * 2}, nil
-}
-
-func (*testGRPCServer) StreamingOutputCall(*testpb.SimpleRequest, testpb.TestService_StreamingOutputCallServer) error {
-	return nil
-}
-
-func (*testGRPCServer) StreamingInputCall(testpb.TestService_StreamingInputCallServer) error {
-	return nil
-}
-
-func (*testGRPCServer) StreamingBidirectionalCall(testpb.TestService_StreamingBidirectionalCallServer) error {
-	return nil
-}
-
-func startTestGRPCServer(t *testing.T, tracer ot.Tracer) (*grpc.Server, net.Addr) {
-	lis, _ := net.Listen("tcp", ":0")
-	server := grpc.NewServer(
-		grpc.UnaryInterceptor(otgrpc.OpenTracingServerInterceptor(tracer)),
-	)
-	testpb.RegisterTestServiceServer(server, &testGRPCServer{})
-
-	go func() {
-		err := server.Serve(lis)
-		require.NoError(t, err)
-	}()
-
-	return server, lis.Addr()
-}
-
-func TestBridgeTracer_ExtractAndInject_gRPC(t *testing.T) {
-	tracer := newMockTracer()
-	bridge := NewBridgeTracer()
-	bridge.SetOpenTelemetryTracer(tracer)
-	bridge.SetTextMapPropagator(propagation.TraceContext{})
-
-	srv, addr := startTestGRPCServer(t, bridge)
-	defer srv.Stop()
-
-	conn, err := grpc.NewClient(
-		addr.String(),
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithUnaryInterceptor(otgrpc.OpenTracingClientInterceptor(bridge)),
-	)
-	require.NoError(t, err)
-	cli := testpb.NewTestServiceClient(conn)
-
-	ctx, cx := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cx()
-	res, err := cli.UnaryCall(ctx, &testpb.SimpleRequest{Payload: 42})
-	require.NoError(t, err)
-	assert.EqualValues(t, 84, res.Payload)
-
-	checkSpans := func() bool {
-		return len(tracer.FinishedSpans) == 2
-	}
-	require.Eventuallyf(t, checkSpans, 5*time.Second, 5*time.Millisecond, "expecting two spans")
-	assert.Equal(t,
-		tracer.FinishedSpans[0].SpanContext().TraceID(),
-		tracer.FinishedSpans[1].SpanContext().TraceID(),
-		"expecting same trace ID",
-	)
-}
