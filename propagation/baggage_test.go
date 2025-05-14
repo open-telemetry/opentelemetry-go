0d124feb82cd3ad1e25348bc62c76310219c26b0
James Moessis <james.moessis1@gmail.com>
2025-05-14 12:02:54 +1000

    Update propagation/baggage_test.go
    Co-authored-by: Robert Pająk <pellared@hotmail.com>

--- propagation/baggage_test.go
+++ propagation/baggage_test.go
@@ -133,7 +133,7 @@ func TestExtractValidBaggage(t *testing.T) {
 			assert.Equal(t, expected, baggage.FromContext(ctx), "should extract baggage for HeaderCarrier")
 
 			// test with map carrier (which does not implement ValuesGetter)
-			ctx = prop.Extract(context.Background(), mapCarr)
+			ctx = prop.Extract(context.Background(), mapCarr, "should extract baggage for MapCarrier")
 			expected = tt.want.Baggage(t)
 			assert.Equal(t, expected, baggage.FromContext(ctx))
 		})
