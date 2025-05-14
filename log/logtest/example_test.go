7622877d9d13bb148597c39f795f2318e15b8107
Robert Pajak <pellared@hotmail.com>
2025-05-14 16:51:11 +0200

    Add testable example

---
+++ log/logtest/example_test.go
@@ -0,0 +1,53 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package logtest_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"go.opentelemetry.io/otel/log"
+	"go.opentelemetry.io/otel/log/logtest"
+)
+
+func Example() {
+	t := &testing.T{} // Provided by testing framework.
+
+	// Create a recorder.
+	rec := logtest.NewRecorder()
+
+	// Emit a log record (code under test).
+	l := rec.Logger("Example")
+	ctx := context.Background()
+	r := log.Record{}
+	r.SetTimestamp(time.Now())
+	r.SetSeverity(log.SeverityInfo)
+	r.SetBody(log.StringValue("Hello there"))
+	r.AddAttributes(log.String("foo", "bar"))
+	r.AddAttributes(log.Int("n", 1))
+	l.Emit(ctx, r)
+
+	// Verify that the expected and actual log records match.
+	want := logtest.Recording{
+		logtest.Scope{Name: "Example"}: []logtest.Record{
+			{
+				Context:  context.Background(),
+				Severity: log.SeverityInfo,
+				Body:     log.StringValue("Hello there"),
+				Attributes: []log.KeyValue{
+					log.Int("n", 1),
+					log.String("foo", "bar"),
+				},
+			},
+		},
+	}
+	got := rec.Result()
+	logtest.AssertEqual(t, want, got,
+		logtest.WithCmpOpts(cmpopts.IgnoreTypes(time.Time{})), // Ignore Timestamps.)
+	)
+	// Output:
+	//
+}
