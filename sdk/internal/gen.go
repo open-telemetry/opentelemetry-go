94ab03f569b9fe1553ede411996bcc3444a636d8
Joe Stephenson <Mojachieee@users.noreply.github.com>
2025-06-17 15:49:22 +0100

    Remove internal matchers (#6907)
    Modifies the only remaining use of the internal matchers to use
    `require` and closes #6541

--- sdk/internal/gen.go
+++
@@ -1,9 +0,0 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
-// Package internal provides internal functionality for the sdk package.
-package internal // import "go.opentelemetry.io/otel/sdk/internal"
-
-//go:generate gotmpl --body=../../internal/shared/matchers/expectation.go.tmpl "--data={}" --out=matchers/expectation.go
-//go:generate gotmpl --body=../../internal/shared/matchers/expecter.go.tmpl "--data={}" --out=matchers/expecter.go
-//go:generate gotmpl --body=../../internal/shared/matchers/temporal_matcher.go.tmpl "--data={}" --out=matchers/temporal_matcher.go
