5d5524076b78f8982a66d1e2cc3260eb812ae789
bachgarash <bakhtiyar.garashov@zendesk.com>
2025-07-03 18:33:21 +0300

    feat: add ErrorType for semconv package 1.34.0 (#6904)

---
+++ semconv/v1.34.0/errorType.go
@@ -0,0 +1,29 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package semconv // import "go.opentelemetry.io/otel/semconv/v1.34.0"
+
+import (
+	"fmt"
+	"reflect"
+
+	"go.opentelemetry.io/otel/attribute"
+)
+
+// ErrorType returns an attribute KeyValue that identifies the type of the given error.
+// It uses reflection to determine the fully qualified type name for observability and monitoring.
+func ErrorType(err error) attribute.KeyValue {
+	t := reflect.TypeOf(err)
+	var value string
+	if t.PkgPath() == "" && t.Name() == "" {
+		// Likely a builtin type.
+		value = t.String()
+	} else {
+		value = fmt.Sprintf("%s.%s", t.PkgPath(), t.Name())
+	}
+
+	if value == "" {
+		return ErrorTypeOther
+	}
+	return ErrorTypeKey.String(value)
+}
