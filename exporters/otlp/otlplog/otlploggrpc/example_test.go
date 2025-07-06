832921d54980422c33c9485918c027a8794ee568
Guilin Liang <guilin.liang@qq.com>
2025-07-06 10:57:55 +0800

    split the mixed example into 2 separate examples for tls and mtls respectively

--- exporters/otlp/otlplog/otlploggrpc/example_test.go
+++ exporters/otlp/otlplog/otlploggrpc/example_test.go
@@ -38,8 +38,44 @@ func Example() {
 	// telemetry.
 }
 
-// Demonstrates how to configure the exporter using self-signed certificates.
-func Example_selfSignedCertificates() {
+// Demonstrates how to configure the exporter using self-signed certificates for TLS connections.
+func Example_selfSignedCertificates_TLS() {
+	ctx := context.Background()
+	var grpcExpOpt []otlploggrpc.Option
+	// the filepath to the server's CA certificate
+	caFile := os.Getenv("CUSTOM_SERVER_CA_CERTIFICATE")
+	if caFile == "" {
+		panic(errors.New("environment variable CUSTOM_SERVER_CA_CERTIFICATE is required for TLS connections"))
+	}
+
+	// TLS connection
+	creds, err := credentials.NewClientTLSFromFile(caFile, "")
+	if err != nil {
+		panic(err)
+	}
+	option := otlploggrpc.WithTLSCredentials(creds)
+	grpcExpOpt = append(grpcExpOpt, option)
+	exp, err := otlploggrpc.New(ctx, grpcExpOpt...)
+	if err != nil {
+		panic(err)
+	}
+
+	processor := log.NewBatchProcessor(exp)
+	provider := log.NewLoggerProvider(log.WithProcessor(processor))
+	defer func() {
+		if err := provider.Shutdown(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	global.SetLoggerProvider(provider)
+
+	// From here, the provider can be used by instrumentation to collect
+	// telemetry.
+}
+
+// Demonstrates how to configure the exporter using self-signed certificates for mutual TLS (mTLS) connections.
+func Example_selfSignedCertificates_mTLS() {
 	ctx := context.Background()
 	var grpcExpOpt []otlploggrpc.Option
 	// the filepath to the server's CA certificate
@@ -48,42 +84,40 @@ func Example_selfSignedCertificates() {
 	clientCert := os.Getenv("CUSTOM_CLIENT_CERTIFICATE")
 	// the filepath to the client's private key
 	clientKey := os.Getenv("CUSTOM_CLIENT_KEY")
-	if caFile != "" && clientCert != "" && clientKey != "" {
-		// mTLS connection
-		tlsCfg := tls.Config{}
-		// loads CA certificate
-		pool := x509.NewCertPool()
-		data, err := os.ReadFile(caFile)
-		if err != nil {
-			panic(err)
-		}
-		if !pool.AppendCertsFromPEM(data) {
-			panic(errors.New("failed to add CA certificate to root CA pool"))
-		}
-		tlsCfg.RootCAs = pool
-		// load client cert and key
-		keypair, err := tls.LoadX509KeyPair(clientCert, clientKey)
-		if err != nil {
-			panic(err)
-		}
-		tlsCfg.Certificates = []tls.Certificate{keypair}
-		creds := credentials.NewTLS(&tlsCfg)
-		option := otlploggrpc.WithTLSCredentials(creds)
-		grpcExpOpt = append(grpcExpOpt, option)
-	} else if caFile != "" {
-		// TLS connection
-		creds, err := credentials.NewClientTLSFromFile(caFile, "")
-		if err != nil {
-			panic(err)
-		}
-		option := otlploggrpc.WithTLSCredentials(creds)
-		grpcExpOpt = append(grpcExpOpt, option)
+	if caFile == "" {
+		panic(errors.New("CUSTOM_SERVER_CA_CERTIFICATE environment variable is required for mTLS connections"))
 	}
+	if clientCert == "" {
+		panic(errors.New("CUSTOM_CLIENT_CERTIFICATE environment variable is required for mTLS connections"))
+	}
+	if clientKey == "" {
+		panic(errors.New("CUSTOM_CLIENT_KEY environment variable is required for mTLS connections"))
+	}
+	// mTLS connection
+	tlsCfg := tls.Config{}
+	// loads CA certificate
+	pool := x509.NewCertPool()
+	data, err := os.ReadFile(caFile)
+	if err != nil {
+		panic(err)
+	}
+	if !pool.AppendCertsFromPEM(data) {
+		panic(errors.New("failed to add CA certificate to root CA pool"))
+	}
+	tlsCfg.RootCAs = pool
+	// load client cert and key
+	keypair, err := tls.LoadX509KeyPair(clientCert, clientKey)
+	if err != nil {
+		panic(err)
+	}
+	tlsCfg.Certificates = []tls.Certificate{keypair}
+	creds := credentials.NewTLS(&tlsCfg)
+	option := otlploggrpc.WithTLSCredentials(creds)
+	grpcExpOpt = append(grpcExpOpt, option)
 	exp, err := otlploggrpc.New(ctx, grpcExpOpt...)
 	if err != nil {
 		panic(err)
 	}
-
 	processor := log.NewBatchProcessor(exp)
 	provider := log.NewLoggerProvider(log.WithProcessor(processor))
 	defer func() {
