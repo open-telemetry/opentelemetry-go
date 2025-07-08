4986a7304ec222154bf92800ff0f2cbea29c83f8
Corentin Musard <corentin.musard@tilebox.com>
2025-07-08 14:06:23 +0200

    Clarify DefaultMaxQueueSize and DefaultScheduleDelay usage

--- sdk/trace/batch_span_processor.go
+++ sdk/trace/batch_span_processor.go
@@ -18,8 +18,10 @@ import (
 
 // Defaults for BatchSpanProcessorOptions.
 const (
-	DefaultMaxQueueSize       = 2048
-	DefaultScheduleDelay      = 5000
+	DefaultMaxQueueSize = 2048
+	// DefaultScheduleDelay is 5000 msec. It must be multiplied by time.Millisecond when passed to WithBatchTimeout.
+	DefaultScheduleDelay = 5000
+	// DefaultExportTimeout is 30000 msec. It must be multiplied by time.Millisecond when passed to WithExportTimeout.
 	DefaultExportTimeout      = 30000
 	DefaultMaxExportBatchSize = 512
 )
