3eeba4c27524eb5b368d236297434cf85b9e4422
Robert Pajak <pellared@hotmail.com>
2025-07-15 15:23:37 +0200

    add docs

--- sdk/trace/doc.go
+++ sdk/trace/doc.go
@@ -6,5 +6,21 @@ Package trace contains support for OpenTelemetry distributed tracing.
 
 The following assumes a basic familiarity with OpenTelemetry concepts.
 See https://opentelemetry.io.
+
+# Self-Observability (Experimental)
+
+The SDK provides a self-observability feature that allows you to monitor the SDK itself.
+
+This feature is experimental and may change in future releases.
+
+To opt-in, set the environment variable OTEL_GO_X_SELF_OBSERVABILITY to "true".
+
+When enabled, the SDK will create following metrics using the global MeterProvider:
+  - otel.sdk.span.live
+  - otel.sdk.span.started
+
+Please see the [Semantic conventions for OpenTelemetry SDK metrics] documentation for more details on these metrics.
+
+[Semantic conventions for OpenTelemetry SDK metrics]: https://github.com/open-telemetry/semantic-conventions/blob/v1.36.0/docs/otel/sdk-metrics.md
 */
 package trace // import "go.opentelemetry.io/otel/sdk/trace"
