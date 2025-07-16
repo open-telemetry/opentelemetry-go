16df7b80d23146d40a721ede35f994d41c2e69dd
Robert Pajak <pellared@hotmail.com>
2025-07-16 12:48:51 +0200

    Move experimental docs to x/internal

--- sdk/trace/doc.go
+++ sdk/trace/doc.go
@@ -7,20 +7,7 @@ Package trace contains support for OpenTelemetry distributed tracing.
 The following assumes a basic familiarity with OpenTelemetry concepts.
 See https://opentelemetry.io.
 
-# Self-Observability (Experimental)
-
-This feature is experimental and may change in backwards incompatible ways in future releases.
-
-The SDK provides a self-observability feature that allows you to monitor the SDK itself.
-
-To opt-in, set the environment variable OTEL_GO_X_SELF_OBSERVABILITY to "true".
-
-When enabled, the SDK will create following metrics using the global MeterProvider:
-  - otel.sdk.span.live
-  - otel.sdk.span.started
-
-Please see the [Semantic conventions for OpenTelemetry SDK metrics] documentation for more details on these metrics.
-
-[Semantic conventions for OpenTelemetry SDK metrics]: https://github.com/open-telemetry/semantic-conventions/blob/v1.36.0/docs/otel/sdk-metrics.md
+See [go.opentelemetry.io/otel/sdk/trace/internal/x] for information about
+the experimental features.
 */
 package trace // import "go.opentelemetry.io/otel/sdk/trace"
---
+++ sdk/trace/internal/x/doc.go
@@ -0,0 +1,34 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+/*
+Package x contains and documents support for OpenTelemetry Trace SDK experimental features.
+
+# Compatibility and Stability
+
+Experimental features may change in backwards incompatible ways as feedback is applied.
+
+Experimental features do not fall within the scope of the [OpenTelemetry Go versioning and stability policy].
+These features may be removed or modified in successive version releases, including patch versions.
+
+When an experimental feature is promoted to a stable feature, a migration path will be included in the changelog entry of the release.
+There is no guarantee that any environment variable feature flags that enabled the experimental feature will be supported by the stable version.
+If they are supported, they may be accompanied with a deprecation notice stating a timeline for the removal of that support.
+
+[OpenTelemetry Go versioning and stability policy: https://github.com/open-telemetry/opentelemetry-go/blob/main/VERSIONING.md
+
+# Self-Observability (Experimental)
+
+The SDK provides a self-observability feature that allows you to monitor the SDK itself.
+
+To opt-in, set the environment variable OTEL_GO_X_SELF_OBSERVABILITY to "true".
+
+When enabled, the SDK will create following metrics using the global MeterProvider:
+  - otel.sdk.span.live
+  - otel.sdk.span.started
+
+Please see the [Semantic conventions for OpenTelemetry SDK metrics] documentation for more details on these metrics.
+
+[Semantic conventions for OpenTelemetry SDK metrics]: https://github.com/open-telemetry/semantic-conventions/blob/v1.36.0/docs/otel/sdk-metrics.md
+*/
+package x // import "go.opentelemetry.io/otel/sdk/trace/internal/x"
