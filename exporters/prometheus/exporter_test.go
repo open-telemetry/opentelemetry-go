0825d3f1a82c7cae308823ba9d66a7d9da217eae
David Ashpole <dashpole@google.com>
2025-07-18 15:06:43 +0000

    test for bracketed units

--- exporters/prometheus/exporter_test.go
+++ exporters/prometheus/exporter_test.go
@@ -131,6 +131,35 @@ func TestPrometheusExporter(t *testing.T) {
 			},
 		},
 		{
+			name:         "counter with bracketed unit",
+			expectedFile: "testdata/counter_no_unit.txt",
+			recordMetrics: func(ctx context.Context, meter otelmetric.Meter) {
+				opt := otelmetric.WithAttributes(
+					attribute.Key("A").String("B"),
+					attribute.Key("C").String("D"),
+					attribute.Key("E").Bool(true),
+					attribute.Key("F").Int(42),
+				)
+				counter, err := meter.Float64Counter(
+					"foo",
+					otelmetric.WithDescription("a simple counter"),
+					otelmetric.WithUnit("{spans}"),
+				)
+				require.NoError(t, err)
+				counter.Add(ctx, 5, opt)
+				counter.Add(ctx, 10.3, opt)
+				counter.Add(ctx, 9, opt)
+
+				attrs2 := attribute.NewSet(
+					attribute.Key("A").String("D"),
+					attribute.Key("C").String("B"),
+					attribute.Key("E").Bool(true),
+					attribute.Key("F").Int(42),
+				)
+				counter.Add(ctx, 5, otelmetric.WithAttributeSet(attrs2))
+			},
+		},
+		{
 			name:         "counter that already has a total suffix",
 			expectedFile: "testdata/counter.txt",
 			disableUTF8:  true,
