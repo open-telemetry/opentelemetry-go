6cdbfe224e3d996252b6d1d0237a0e6159ff2bd5
Praful <holiodin@gmail.com>
2025-08-07 21:07:34 +0530

    Add self-observability metrics to stdoutlog exporter
    Signed-off-by: Praful <holiodin@gmail.com>

---
+++ exporters/stdout/stdoutlog/selfobservability.go
@@ -0,0 +1,55 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package stdoutlog
+
+import (
+	"context"
+	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/sdk"
+	"go.opentelemetry.io/otel/sdk/log"
+	semconv "go.opentelemetry.io/otel/semconv/v1.36.0"
+	"go.opentelemetry.io/otel/semconv/v1.36.0/otelconv"
+)
+
+func newSelfObservability() *selfObservability {
+	mp := otel.GetMeterProvider()
+	m := mp.Meter("go.opentelemetry.io/otel/exporters/stdout/stdoutlog",
+		metric.WithInstrumentationVersion(sdk.Version()),
+		metric.WithSchemaURL(semconv.SchemaURL))
+
+	so := selfObservability{}
+
+	var err error
+	if so.inflight, err = otelconv.NewSDKExporterLogInflight(m); err != nil {
+		otel.Handle(err)
+	}
+	if so.exported, err = otelconv.NewSDKExporterLogExported(m); err != nil {
+		otel.Handle(err)
+	}
+	if so.duration, err = otelconv.NewSDKExporterOperationDuration(m); err != nil {
+		otel.Handle(err)
+	}
+	return &so
+}
+
+func (e *Exporter) initSelfObservability(ctx context.Context, records *[]log.Record) {
+	if records == nil {
+		return
+	} else if e.selfObservability == nil {
+		return
+	}
+
+	e.selfObservability.inflight.Add(ctx, int64(len(*records)))
+
+	start := time.Now()
+	defer func() {
+		dur := float64(time.Since(start).Nanoseconds())
+		e.selfObservability.duration.Record(ctx, dur)
+	}()
+
+	e.selfObservability.exported.Add(ctx, int64(len(*records)))
+}
