460f3031b17ad15753b5915dc8b065991a777932
Mahendra Bishnoi <mahendrabishnoi2@gmail.com>
2025-08-16 12:04:56 +0530

    addressed review comment: use named return to make code more readable

--- exporters/stdout/stdoutmetric/exporter.go
+++ exporters/stdout/stdoutmetric/exporter.go
@@ -82,13 +82,12 @@ func (e *exporter) Aggregation(k metric.InstrumentKind) metric.Aggregation {
 	return e.aggregationSelector(k)
 }
 
-func (e *exporter) Export(ctx context.Context, data *metricdata.ResourceMetrics) error {
-	var err error
+func (e *exporter) Export(ctx context.Context, data *metricdata.ResourceMetrics) (err error) {
 	trackExportFunc := e.trackExport(context.Background(), countDataPoints(data))
 	defer func() { trackExportFunc(err) }()
 	err = ctx.Err()
 	if err != nil {
-		return err
+		return
 	}
 	if e.redactTimestamps {
 		redactTimestamps(data)
@@ -97,10 +96,7 @@ func (e *exporter) Export(ctx context.Context, data *metricdata.ResourceMetrics)
 	global.Debug("STDOUT exporter export", "Data", data)
 
 	err = e.encVal.Load().(encoderHolder).Encode(data)
-	if err != nil {
-		return err
-	}
-	return nil
+	return
 }
 
 func (e *exporter) trackExport(ctx context.Context, count int64) func(err error) {
