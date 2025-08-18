11b6130ef0d9dcc594a090a3ee4d9b19c4bda0c5
Yevhenii Solomchenko <ysolomchenko@splunk.com>
2025-08-18 11:35:41 +0200

    Update sdk/metric/doc.go
    Co-authored-by: Robert Pająk <pellared@hotmail.com>

--- sdk/metric/doc.go
+++ sdk/metric/doc.go
@@ -52,7 +52,7 @@
 // these sets are aggregated into
 // a special attribute set containing "otel.metric.overflow" attribute with "true" value.
 // This ensures total metric values (e.g., Sum, Count) remain correct for the
-// collection interval, but information about the specific dropped sets
+// collection cycle, but information about the specific dropped sets
 // is not preserved.
 //
 // Recommendations:
