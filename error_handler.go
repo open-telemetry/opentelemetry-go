ef98dfd98663ebca98aae166a483f86f3c2a69b3
flc1125 <four_leaf_clover@foxmail.com>
2025-08-25 08:39:59 +0800

    refactor: update ErrorHandler type to be an alias of global.ErrorHandler
    - Changed ErrorHandler type definition to be an alias of global.ErrorHandler
    - Added comment to clarify the purpose of the ErrorHandler type

--- error_handler.go
+++ error_handler.go
@@ -6,7 +6,8 @@ package otel // import "go.opentelemetry.io/otel"
 import "go.opentelemetry.io/otel/internal/global"
 
 // ErrorHandler handles irremediable events.
-type ErrorHandler global.ErrorHandler
+// It's an alias for [global.ErrorHandler].
+type ErrorHandler = global.ErrorHandler
 
 // ErrorHandlerFunc is a convenience adapter to allow the use of a function
 // as an ErrorHandler.
