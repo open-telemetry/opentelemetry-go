691b4b46e151af32170c1bf8d6d1a532269d89dd
Flc゛ <four_leaf_clover@foxmail.com>
2025-08-25 23:51:06 +0800

    fix: correct package import paths (#7244)

--- exporters/stdout/stdouttrace/internal/gen.go
+++ exporters/stdout/stdouttrace/internal/gen.go
@@ -5,5 +5,5 @@
 // package.
 package internal // import "go.opentelemetry.io/otel/exporters/stdout/stdouttrace/internal"
 
-//go:generate gotmpl --body=../../../../internal/shared/counter/counter.go.tmpl "--data={}" --out=counter/counter.go
+//go:generate gotmpl --body=../../../../internal/shared/counter/counter.go.tmpl "--data={ \"pkg\": \"go.opentelemetry.io/otel/exporters/stdout/stdouttrace/internal/counter\" }" --out=counter/counter.go
 //go:generate gotmpl --body=../../../../internal/shared/counter/counter_test.go.tmpl "--data={}" --out=counter/counter_test.go
