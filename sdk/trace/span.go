2486759f47cf41918eac9950c1eb94f3eb335808
Tyler Yahn <codingalias@gmail.com>
2025-08-29 08:28:44 -0700

    Fix merge

--- sdk/trace/span.go
+++ sdk/trace/span.go
@@ -21,7 +21,6 @@ import (
 	"go.opentelemetry.io/otel/sdk/instrumentation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
-	"go.opentelemetry.io/otel/semconv/v1.37.0/otelconv"
 	"go.opentelemetry.io/otel/trace"
 	"go.opentelemetry.io/otel/trace/embedded"
 )
