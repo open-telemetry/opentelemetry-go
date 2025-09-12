3b3d9aedb2aee06f2bf442e61571714d009cf586
yumosx <zhengel2022@163.com>
2025-09-12 16:07:47 +0800

    fix the version header and version value

--- exporters/otlp/otlplog/otlploggrpc/internal/version.go
+++ exporters/otlp/otlplog/otlploggrpc/internal/version.go
@@ -1,5 +1,8 @@
-package internal
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package internal // import "go.opentelemetry.io/otel/exporters/otlp/otlpgrpc/internal"
 
 // Version is the current release version of the OpenTelemetry otlploggrpc
 // exporter in use.
-const Version = "1.38.0"
+const Version = "0.14.0"
