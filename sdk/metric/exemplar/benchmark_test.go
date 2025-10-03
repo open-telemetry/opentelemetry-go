open-telemetry/opentelemetry-go

commit ea38204f2051d5997a1556e9db22dcb7cd49e481

Allow optimizing locking for built-in exemplar reservoirs  (#7423)
Fixes https://github.com/open-telemetry/opentelemetry-go/issues/7388

Benchmarks seem like mostly noise. It isn't actually necessary to lock
in the exemplar reservoir today because of our SDK design, but this
allows us to make optimizations in the future. After
https://github.com/open-telemetry/opentelemetry-go/pull/7427,
improvements to exemplar reservoir locking will greatly improve the
ExemplarEnabled benchmarks.

Parallel benchmarks:
```
goos: linux
goarch: amd64
pkg: go.opentelemetry.io/otel/sdk/metric
cpu: Intel(R) Xeon(R) CPU @ 2.20GHz
                                                                                  │  main24.txt  │           exemplar24.txt            │
                                                                                  │    sec/op    │    sec/op     vs base               │
SyncMeasure/NoView/ExemplarsDisabled/Int64Counter/Attributes/0-24                   399.5n ± 16%   367.8n ± 17%        ~ (p=0.310 n=6)
SyncMeasure/NoView/ExemplarsDisabled/Int64Counter/Attributes/1-24                   369.4n ± 27%   410.2n ± 11%        ~ (p=0.240 n=6)
SyncMeasure/NoView/ExemplarsDisabled/Int64Counter/Attributes/10-24                  372.6n ± 23%   398.9n ±  8%        ~ (p=0.394 n=6)
SyncMeasure/NoView/ExemplarsDisabled/Float64Counter/Attributes/0-24                 313.4n ± 12%   357.7n ± 20%        ~ (p=0.065 n=6)
SyncMeasure/NoView/ExemplarsDisabled/Float64Counter/Attributes/1-24                 389.9n ± 12%   379.5n ±  9%        ~ (p=0.818 n=6)
SyncMeasure/NoView/ExemplarsDisabled/Float64Counter/Attributes/10-24                441.4n ± 13%   359.1n ± 18%  -18.64% (p=0.009 n=6)
SyncMeasure/NoView/ExemplarsDisabled/Int64UpDownCounter/Attributes/0-24             415.8n ± 22%   400.3n ± 16%        ~ (p=0.937 n=6)
SyncMeasure/NoView/ExemplarsDisabled/Int64UpDownCounter/Attributes/1-24             346.9n ±  8%   364.6n ± 19%        ~ (p=0.240 n=6)

sdk/metric/exemplar/fixed_size_reservoir.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/sdk/metric/internal/reservoir"
 )
 
 // FixedSizeReservoirProvider returns a provider of [FixedSizeReservoir].
@@ -34,6 +35,7 @@ var _ Reservoir = &FixedSizeReservoir{}
 // If there are more than k, the Reservoir will then randomly sample all
 // additional measurement with a decreasing probability.
 type FixedSizeReservoir struct {
+	reservoir.ConcurrentSafe
 	*storage
 
 	// count is the number of measurement seen.
@@ -123,12 +125,12 @@ func (r *FixedSizeReservoir) Offer(ctx context.Context, t time.Time, n Value, a 
 	// https://github.com/MrAlias/reservoir-sampling for a performance
 	// comparison of reservoir sampling algorithms.
 
-	if int(r.count) < cap(r.store) {
-		r.store[r.count] = newMeasurement(ctx, t, n, a)
+	if int(r.count) < cap(r.measurements) {
+		r.store(int(r.count), newMeasurement(ctx, t, n, a))
 	} else if r.count == r.next {
 		// Overwrite a random existing measurement with the one offered.
-		idx := int(rand.Int64N(int64(cap(r.store))))
-		r.store[idx] = newMeasurement(ctx, t, n, a)
+		idx := int(rand.Int64N(int64(cap(r.measurements))))
+		r.store(idx, newMeasurement(ctx, t, n, a))
 		r.advance()
 	}
 	r.count++
@@ -139,7 +141,7 @@ func (r *FixedSizeReservoir) reset() {
 	// This resets the number of exemplars known.
 	r.count = 0
 	// Random index inserts should only happen after the storage is full.
-	r.next = int64(cap(r.store))
+	r.next = int64(cap(r.measurements))
 
 	// Initial random number in the series used to generate r.next.
 	//
@@ -150,7 +152,7 @@ func (r *FixedSizeReservoir) reset() {
 	// This maps the uniform random number in (0,1) to a geometric distribution
 	// over the same interval. The mean of the distribution is inversely
 	// proportional to the storage capacity.
-	r.w = math.Exp(math.Log(r.randomFloat64()) / float64(cap(r.store)))
+	r.w = math.Exp(math.Log(r.randomFloat64()) / float64(cap(r.measurements)))
 
 	r.advance()
 }
@@ -170,7 +172,7 @@ func (r *FixedSizeReservoir) advance() {
 	// therefore the next r.w will be based on the same distribution (i.e.
 	// `max(u_1,u_2,...,u_k)`). Therefore, we can sample the next r.w by
 	// computing the next random number `u` and take r.w as `w * u^(1/k)`.
-	r.w *= math.Exp(math.Log(r.randomFloat64()) / float64(cap(r.store)))
+	r.w *= math.Exp(math.Log(r.randomFloat64()) / float64(cap(r.measurements)))
 	// Use the new random number in the series to calculate the count of the
 	// next measurement that will be stored.
 	//

sdk/metric/exemplar/histogram_reservoir.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/sdk/metric/internal/reservoir"
 )
 
 // HistogramReservoirProvider is a provider of [HistogramReservoir].
@@ -39,6 +40,7 @@ var _ Reservoir = &HistogramReservoir{}
 // falls within a histogram bucket. The histogram bucket upper-boundaries are
 // define by bounds.
 type HistogramReservoir struct {
+	reservoir.ConcurrentSafe
 	*storage
 
 	// bounds are bucket bounds in ascending order.
@@ -57,14 +59,14 @@ type HistogramReservoir struct {
 // parameters are the value and dropped (filtered) attributes of the
 // measurement respectively.
 func (r *HistogramReservoir) Offer(ctx context.Context, t time.Time, v Value, a []attribute.KeyValue) {
-	var x float64
+	var n float64
 	switch v.Type() {
 	case Int64ValueType:
-		x = float64(v.Int64())
+		n = float64(v.Int64())
 	case Float64ValueType:
-		x = v.Float64()
+		n = v.Float64()
 	default:
 		panic("unknown value type")
 	}
-	r.store[sort.SearchFloat64s(r.bounds, x)] = newMeasurement(ctx, t, v, a)
+	r.store(sort.SearchFloat64s(r.bounds, n), newMeasurement(ctx, t, v, a))
 }

sdk/metric/exemplar/storage.go
@@ -5,6 +5,7 @@ package exemplar // import "go.opentelemetry.io/otel/sdk/metric/exemplar"
 
 import (
 	"context"
+	"sync"
 	"time"
 
 	"go.opentelemetry.io/otel/attribute"
@@ -13,24 +14,33 @@ import (
 
 // storage is an exemplar storage for [Reservoir] implementations.
 type storage struct {
-	// store are the measurements sampled.
+	mu sync.Mutex
+	// measurements are the measurements sampled.
 	//
 	// This does not use []metricdata.Exemplar because it potentially would
 	// require an allocation for trace and span IDs in the hot path of Offer.
-	store []measurement
+	measurements []measurement
 }
 
 func newStorage(n int) *storage {
-	return &storage{store: make([]measurement, n)}
+	return &storage{measurements: make([]measurement, n)}
+}
+
+func (r *storage) store(idx int, m measurement) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.measurements[idx] = m
 }
 
 // Collect returns all the held exemplars.
 //
 // The Reservoir state is preserved after this call.
 func (r *storage) Collect(dest *[]Exemplar) {
-	*dest = reset(*dest, len(r.store), len(r.store))
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	*dest = reset(*dest, len(r.measurements), len(r.measurements))
 	var n int
-	for _, m := range r.store {
+	for _, m := range r.measurements {
 		if !m.valid {
 			continue
 		}

sdk/metric/exemplar/fixed_size_reservoir_test.go
@@ -45,7 +45,7 @@ func TestNewFixedSizeReservoirSamplingCorrectness(t *testing.T) {
 	}
 
 	var sum float64
-	for _, m := range r.store {
+	for _, m := range r.measurements {
 		sum += m.Value.Float64()
 	}
 	mean := sum / float64(sampleSize)

sdk/metric/internal/aggregate/filtered_reservoir.go
@@ -5,10 +5,12 @@ package aggregate // import "go.opentelemetry.io/otel/sdk/metric/internal/aggreg
 
 import (
 	"context"
+	"sync"
 	"time"
 
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/sdk/metric/exemplar"
+	"go.opentelemetry.io/otel/sdk/metric/internal/reservoir"
 )
 
 // FilteredExemplarReservoir wraps a [exemplar.Reservoir] with a filter.
@@ -29,6 +31,11 @@ type FilteredExemplarReservoir[N int64 | float64] interface {
 type filteredExemplarReservoir[N int64 | float64] struct {
 	filter    exemplar.Filter
 	reservoir exemplar.Reservoir
+	// The exemplar.Reservoir is not required to be concurrent safe, but
+	// implementations can indicate that they are concurrent-safe by embedding
+	// reservoir.ConcurrentSafe in order to improve performance.
+	reservoirMux   sync.Mutex
+	concurrentSafe bool
 }
 
 // NewFilteredExemplarReservoir creates a [FilteredExemplarReservoir] which only offers values
@@ -37,17 +44,30 @@ func NewFilteredExemplarReservoir[N int64 | float64](
 	f exemplar.Filter,
 	r exemplar.Reservoir,
 ) FilteredExemplarReservoir[N] {
+	_, concurrentSafe := r.(reservoir.ConcurrentSafe)
 	return &filteredExemplarReservoir[N]{
-		filter:    f,
-		reservoir: r,
+		filter:         f,
+		reservoir:      r,
+		concurrentSafe: concurrentSafe,
 	}
 }
 
 func (f *filteredExemplarReservoir[N]) Offer(ctx context.Context, val N, attr []attribute.KeyValue) {
 	if f.filter(ctx) {
+		ts := time.Now()
+		if !f.concurrentSafe {
+			f.reservoirMux.Lock()
+			defer f.reservoirMux.Unlock()
+		}
 		// only record the current time if we are sampling this measurement.
-		f.reservoir.Offer(ctx, time.Now(), exemplar.NewValue(val), attr)
+		f.reservoir.Offer(ctx, ts, exemplar.NewValue(val), attr)
 	}
 }
 
-func (f *filteredExemplarReservoir[N]) Collect(dest *[]exemplar.Exemplar) { f.reservoir.Collect(dest) }
+func (f *filteredExemplarReservoir[N]) Collect(dest *[]exemplar.Exemplar) {
+	if !f.concurrentSafe {
+		f.reservoirMux.Lock()
+		defer f.reservoirMux.Unlock()
+	}
+	f.reservoir.Collect(dest)
+}

sdk/metric/internal/aggregate/filtered_reservoir_test.go
@@ -1,0 +1,103 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package aggregate // import "go.opentelemetry.io/otel/sdk/metric/internal/aggregate"
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/sdk/metric/exemplar"
+	"go.opentelemetry.io/otel/sdk/metric/internal/reservoir"
+)
+
+func TestConcurrentSafeFilteredReservoir(t *testing.T) {
+	for _, tc := range []struct {
+		desc                 string
+		reservoir            exemplar.Reservoir
+		expectConcurrentSafe bool
+	}{
+		{
+			desc:                 "concurrent safe",
+			reservoir:            &concurrentSafeReservoir{},
+			expectConcurrentSafe: true,
+		},
+		{
+			desc:                 "not concurrent safe",
+			reservoir:            &notConcurrentSafeReservoir{},
+			expectConcurrentSafe: false,
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			reservoir := NewFilteredExemplarReservoir[int64](exemplar.AlwaysOnFilter, tc.reservoir)
+			var wg sync.WaitGroup
+			for range 5 {
+				wg.Add(1)
+				go func() {
+					reservoir.Offer(t.Context(), 25, []attribute.KeyValue{})
+					wg.Done()
+				}()
+			}
+			into := []exemplar.Exemplar{}
+			for range 2 {
+				reservoir.Collect(&into)
+			}
+			wg.Wait()
+			assert.Len(t, into, 1)
+			assert.Equal(t, reservoir.(*filteredExemplarReservoir[int64]).concurrentSafe, tc.expectConcurrentSafe)
+		})
+	}
+}
+
+type notConcurrentSafeReservoir struct {
+	ex exemplar.Exemplar
+}
+
+func (r *notConcurrentSafeReservoir) Offer(
+	_ context.Context,
+	t time.Time,
+	val exemplar.Value,
+	attr []attribute.KeyValue,
+) {
+	r.ex = exemplar.Exemplar{
+		FilteredAttributes: attr,
+		Time:               t,
+		Value:              val,
+	}
+}
+
+func (r *notConcurrentSafeReservoir) Collect(dest *[]exemplar.Exemplar) {
+	*dest = make([]exemplar.Exemplar, 1)
+	(*dest)[0].FilteredAttributes = r.ex.FilteredAttributes
+	(*dest)[0].Time = r.ex.Time
+	(*dest)[0].Value = r.ex.Value
+	*dest = (*dest)[:1]
+}
+
+type concurrentSafeReservoir struct {
+	base notConcurrentSafeReservoir
+	sync.Mutex
+	reservoir.ConcurrentSafe
+}
+
+func (r *concurrentSafeReservoir) Offer(
+	ctx context.Context,
+	t time.Time,
+	val exemplar.Value,
+	attr []attribute.KeyValue,
+) {
+	r.Lock()
+	defer r.Unlock()
+	r.base.Offer(ctx, t, val, attr)
+}
+
+func (r *concurrentSafeReservoir) Collect(dest *[]exemplar.Exemplar) {
+	r.Lock()
+	defer r.Unlock()
+	r.base.Collect(dest)
+}

sdk/metric/internal/reservoir/concurrent_safe.go
@@ -1,0 +1,11 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package reservoir // import "go.opentelemetry.io/otel/sdk/metric/internal/reservoir"
+
+// ConcurrentSafe is an interface that can be embedded in an
+// exemplar.Reservoir to indicate to the SDK that it is safe to invoke its
+// methods concurrently. If this interface is not embedded, the SDK assumes it
+// is not safe to call concurrently and locks around Reservoir methods. This
+// is currently only used by the built-in reservoirs.
+type ConcurrentSafe interface{ concurrentSafe() }

sdk/metric/internal/reservoir/doc.go
@@ -1,0 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// Package reservoir contains experimental features used by built-in exemplar
+// reservoirs which require coordination with the metrics SDK.
+package reservoir // import "go.opentelemetry.io/otel/sdk/metric/internal/reservoir"

commit bd1b3da91c632f59d5e21ad175f90e390d193668

Add exemplar reservoir parallel benchmarks (#7441)
This also fixes a bug introduced in
https://github.com/open-telemetry/opentelemetry-go/pull/7423, where we
were only locking around storage and not around other shared fields
(e.g. count). Fixing the bug is required for benchmarks to run properly,
but wasn't caught by concurrent safe tests because the SDK does not
currently call exemplar methods concurrently.

```
goos: linux
goarch: amd64
pkg: go.opentelemetry.io/otel/sdk/metric/exemplar
cpu: Intel(R) Xeon(R) CPU @ 2.20GHz
BenchmarkFixedSizeReservoirOffer-24    	  498955	       248.4 ns/op	       0 B/op	       0 allocs/op
BenchmarkHistogramReservoirOffer-24    	  478068	       250.1 ns/op	       0 B/op	       0 allocs/op
```

sdk/metric/exemplar/fixed_size_reservoir.go
@@ -125,6 +125,8 @@ func (r *FixedSizeReservoir) Offer(ctx context.Context, t time.Time, n Value, a 
 	// https://github.com/MrAlias/reservoir-sampling for a performance
 	// comparison of reservoir sampling algorithms.
 
+	r.mu.Lock()
+	defer r.mu.Unlock()
 	if int(r.count) < cap(r.measurements) {
 		r.store(int(r.count), newMeasurement(ctx, t, n, a))
 	} else if r.count == r.next {

sdk/metric/exemplar/histogram_reservoir.go
@@ -68,5 +68,11 @@ func (r *HistogramReservoir) Offer(ctx context.Context, t time.Time, v Value, a 
 	default:
 		panic("unknown value type")
 	}
-	r.store(sort.SearchFloat64s(r.bounds, n), newMeasurement(ctx, t, v, a))
+
+	idx := sort.SearchFloat64s(r.bounds, n)
+	m := newMeasurement(ctx, t, v, a)
+
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.store(idx, m)
 }

sdk/metric/exemplar/storage.go
@@ -27,8 +27,6 @@ func newStorage(n int) *storage {
 }
 
 func (r *storage) store(idx int, m measurement) {
-	r.mu.Lock()
-	defer r.mu.Unlock()
 	r.measurements[idx] = m
 }
 

sdk/metric/exemplar/benchmark_test.go
@@ -1,0 +1,50 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package exemplar // import "go.opentelemetry.io/otel/sdk/metric/exemplar"
+
+import (
+	"runtime"
+	"testing"
+	"time"
+)
+
+func BenchmarkFixedSizeReservoirOffer(b *testing.B) {
+	ts := time.Now()
+	val := NewValue[int64](25)
+	ctx := b.Context()
+	reservoir := NewFixedSizeReservoir(runtime.NumCPU())
+	b.RunParallel(func(pb *testing.PB) {
+		i := 0
+		for pb.Next() {
+			reservoir.Offer(ctx, ts, val, nil)
+			// Periodically trigger a reset, because the algorithm for fixed-size
+			// reservoirs records exemplars very infrequently after a large
+			// number of collect calls.
+			if i%100 == 99 {
+				reservoir.mu.Lock()
+				reservoir.reset()
+				reservoir.mu.Unlock()
+			}
+			i++
+		}
+	})
+}
+
+func BenchmarkHistogramReservoirOffer(b *testing.B) {
+	ts := time.Now()
+	ctx := b.Context()
+	buckets := []float64{0, 5, 10, 25, 50, 75, 100, 250, 500, 750, 1000, 2500, 5000, 7500, 10000}
+	values := make([]Value, len(buckets))
+	for i, bucket := range buckets {
+		values[i] = NewValue[float64](bucket + 1)
+	}
+	res := NewHistogramReservoir(buckets)
+	b.RunParallel(func(pb *testing.PB) {
+		i := 0
+		for pb.Next() {
+			res.Offer(ctx, ts, values[i%len(values)], nil)
+			i++
+		}
+	})
+}