49d3c44519a0c37d7acfe4d48b3f4d411be3cc4a
Saharsh Tibrewala <saharsh1947@gmail.com>
2026-01-17 01:14:16 +0530

    39 to 37

--- exporters/otlp/otlplog/otlploggrpc/internal/observ/instrumentation.go
+++ exporters/otlp/otlplog/otlploggrpc/internal/observ/instrumentation.go
@@ -20,8 +20,8 @@ import (
 	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc/internal/x"
 	"go.opentelemetry.io/otel/internal/global"
 	"go.opentelemetry.io/otel/metric"
-	semconv "go.opentelemetry.io/otel/semconv/v1.39.0"
-	"go.opentelemetry.io/otel/semconv/v1.39.0/otelconv"
+	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
+	"go.opentelemetry.io/otel/semconv/v1.37.0/otelconv"
 )
 
 const (
