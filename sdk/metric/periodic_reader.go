1f8dea38cc4d450eaea854833db3278abc12c563
Tyler Yahn <codingalias@gmail.com>
2026-03-02 17:22:04 -0800

    Ignore lint errors

--- sdk/metric/periodic_reader.go
+++ sdk/metric/periodic_reader.go
@@ -107,7 +107,9 @@ func WithInterval(d time.Duration) PeriodicReaderOption {
 // exporter. That is left to the user to accomplish.
 func NewPeriodicReader(exporter Exporter, options ...PeriodicReaderOption) *PeriodicReader {
 	conf := newPeriodicReaderConfig(options)
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithCancel(
+		context.Background(),
+	) //nolint:gosec  // cancel called during PeriodicReader shutdown.
 	r := &PeriodicReader{
 		interval: conf.interval,
 		timeout:  conf.timeout,
