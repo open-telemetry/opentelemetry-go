5b5c2c5d6dc92256eb3f4ff8323cf40a99db4186
Tyler Yahn <MrAlias@users.noreply.github.com>
2026-03-04 03:13:40 -0800

    Upgrade to semconv/v1.40.0 (#7991)

--- exporters/zipkin/model_test.go
+++ exporters/zipkin/model_test.go
@@ -23,7 +23,7 @@ import (
 	"go.opentelemetry.io/otel/sdk/trace/tracetest"
 	semconv125 "go.opentelemetry.io/otel/semconv/v1.25.0"
 	semconv138 "go.opentelemetry.io/otel/semconv/v1.38.0"
-	semconv "go.opentelemetry.io/otel/semconv/v1.39.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.40.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
