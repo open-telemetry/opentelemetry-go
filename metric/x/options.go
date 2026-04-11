29bb63f6752bb783fd1a32f21d87980389a564ab
David Ashpole <dashpole@google.com>
2026-04-11 01:59:35 +0000

    add Resettable to allow re-using attribute options

--- metric/x/options.go
+++ metric/x/options.go
@@ -27,3 +27,30 @@ func (o defaultAttributesOption) AllowedKeys() []attribute.Key {
 func WithDefaultAttributes(keys ...attribute.Key) metric.InstrumentOption {
 	return defaultAttributesOption{keys: keys}
 }
+
+// Resettable is an optional interface that Options can implement
+// to allow reuse without additional allocations.
+//
+// Example usage with sync.Pool:
+//
+//	var optionPool = sync.Pool{
+//		New: func() any {
+//			return metric.WithAttributeSet(*attribute.EmptySet())
+//		},
+//	}
+//
+//	func record(ctx context.Context, counter metric.Int64Counter, set attribute.Set) {
+//		opt := optionPool.Get().(metric.MeasurementOption)
+//		defer optionPool.Put(opt)
+//
+//		if r, ok := opt.(x.Resettable[attribute.Set]); ok {
+//			r.Reset(set)
+//		}
+//		counter.Add(ctx, 1, opt)
+//	}
+//
+// WARNING: It is the user's responsibility to ensure that the option is not
+// concurrently reset while being passed to the API or used by another goroutine.
+type Resettable[T any] interface {
+	Reset(T)
+}
