f1208ad61e9a77d606d57a15fc9dd696e9d33571
David Ashpole <dashpole@google.com>
2026-04-13 15:39:46 -0400

    Apply suggestion from @dashpole

--- sdk/metric/pipeline_test.go
+++ sdk/metric/pipeline_test.go
@@ -402,7 +402,7 @@ func TestInserterCachedAggregatorNameConflict(t *testing.T) {
 }
 
 func TestExemplars(t *testing.T) {
-	nCPU := runtime.GOMAXPROCS(0)
+	nCPU := max(runtime.GOMAXPROCS(0), 1) 
 	setup := func(name string) (metric.Meter, Reader) {
 		r := NewManualReader()
 		v := NewView(Instrument{Name: "int64-expo-histogram"}, Stream{
