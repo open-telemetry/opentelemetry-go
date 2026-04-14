fea0b60dc1999bae48404e4fc3562616c017e3d7
David Ashpole <dashpole@google.com>
2026-04-14 03:55:25 +0000

    update to latest semconv

--- exporters/otlp/otlpmetric/otlpmetrichttp/internal/observ/instrumentation.go
+++ exporters/otlp/otlpmetric/otlpmetrichttp/internal/observ/instrumentation.go
@@ -23,8 +23,8 @@ import (
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp/internal/x"
 	"go.opentelemetry.io/otel/internal/global"
 	"go.opentelemetry.io/otel/metric"
-	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
-	"go.opentelemetry.io/otel/semconv/v1.37.0/otelconv"
+	semconv "go.opentelemetry.io/otel/semconv/v1.40.0"
+	"go.opentelemetry.io/otel/semconv/v1.40.0/otelconv"
 )
 
 const (
