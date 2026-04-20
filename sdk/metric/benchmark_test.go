7d4aa8471e565c8af376f3c98d5cbbd02f509f4a
Mikhail Mazurskiy <mmazurskiy@gitlab.com>
2026-04-20 11:30:20 +1000

    fix: clear cached objects to enable GC

--- sdk/metric/benchmark_test.go
+++ sdk/metric/benchmark_test.go
@@ -635,12 +635,14 @@ func BenchmarkEndToEndCounterAdd(b *testing.B) {
 								func() {
 									attrsSlice := attrPool.Get().(*[]attribute.KeyValue)
 									defer func() {
+										clear(*attrsSlice)
 										*attrsSlice = (*attrsSlice)[:0] // Reset.
 										attrPool.Put(attrsSlice)
 									}()
 									*attrsSlice = appendAttributes(*attrsSlice, attrsLen)
 									addOpt := addOptPool.Get().(*[]metric.AddOption)
 									defer func() {
+										clear(*addOpt)
 										*addOpt = (*addOpt)[:0]
 										addOptPool.Put(addOpt)
 									}()
@@ -664,12 +666,14 @@ func BenchmarkEndToEndCounterAdd(b *testing.B) {
 								func() {
 									attrsSlice := attrPool.Get().(*[]attribute.KeyValue)
 									defer func() {
+										clear(*attrsSlice)
 										*attrsSlice = (*attrsSlice)[:0] // Reset.
 										attrPool.Put(attrsSlice)
 									}()
 									*attrsSlice = appendAttributes(*attrsSlice, attrsLen)
 									addOpt := addOptPool.Get().(*[]metric.AddOption)
 									defer func() {
+										clear(*addOpt)
 										*addOpt = (*addOpt)[:0]
 										addOptPool.Put(addOpt)
 									}()
