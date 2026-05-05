f3b2d1b28ede34cd4a26bb81b0d70132eaee12e4
muskiteer <agarwal.vansh3110@gmail.com>
2026-05-06 00:22:20 +0530

    fix: handle FixedSizeReservoir size=0 without panic

--- sdk/metric/exemplar/fixed_size_reservoir.go
+++ sdk/metric/exemplar/fixed_size_reservoir.go
@@ -146,6 +146,12 @@ func (r *FixedSizeReservoir) Offer(ctx context.Context, t time.Time, n Value, a 
 
 // reset resets r to the initial state.
 func (r *FixedSizeReservoir) reset() {
+	if cap(r.measurements) == 0 {
+		r.count = 0
+		r.next = math.MaxInt64
+		r.w = 0
+		return
+    }
 	// This resets the number of exemplars known.
 	r.count = 0
 	// Random index inserts should only happen after the storage is full.
