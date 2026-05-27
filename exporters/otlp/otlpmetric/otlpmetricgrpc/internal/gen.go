ebb15ecf52c9c8feeae927aecfdaa9e63ed3cc3f
David Ashpole <dashpole@google.com>
2026-05-27 14:29:08 +0000

    chore: generate x.go in otlpmetricgrpc

--- exporters/otlp/otlpmetric/otlpmetricgrpc/internal/gen.go
+++ exporters/otlp/otlpmetric/otlpmetricgrpc/internal/gen.go
@@ -36,3 +36,6 @@ package internal // import "go.opentelemetry.io/otel/exporters/otlp/otlpmetric/o
 
 //go:generate gotmpl --body=../../../../../internal/shared/counter/counter.go.tmpl "--data={ \"pkg\": \"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc/internal/counter\" }" --out=counter/counter.go
 //go:generate gotmpl --body=../../../../../internal/shared/counter/counter_test.go.tmpl "--data={}" --out=counter/counter_test.go
+
+//go:generate gotmpl --body=../../../../../internal/shared/x/x.go.tmpl "--data={ \"pkg\": \"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc\" }" --out=x/x.go
+//go:generate gotmpl --body=../../../../../internal/shared/x/x_test.go.tmpl "--data={}" --out=x/x_test.go
