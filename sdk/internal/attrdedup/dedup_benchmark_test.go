32cc88918331b85eb8137fe9fc18b6c3ec4774cb
Robert Pajak <pellared@hotmail.com>
2026-06-17 21:30:23 +0200

    refactor BenchmarkValue

--- sdk/internal/attrdedup/dedup_benchmark_test.go
+++ sdk/internal/attrdedup/dedup_benchmark_test.go
@@ -11,42 +11,55 @@ import (
 )
 
 func BenchmarkValue(b *testing.B) {
-	uniqueMap := attribute.MapValue(
-		attribute.String("one", "1"),
-		attribute.String("two", "2"),
-		attribute.String("three", "3"),
-	)
-	duplicateMap := attribute.MapValue(
-		attribute.String("one", "1"),
-		attribute.String("one", "2"),
-		attribute.String("two", "3"),
-	)
-	nestedMapInSlice := attribute.SliceValue(
-		attribute.MapValue(
-			attribute.String("one", "1"),
-			attribute.String("one", "2"),
-			attribute.String("two", "3"),
-		),
-	)
+	values := []struct {
+		name  string
+		value attribute.Value
+	}{
+		{
+			name: "FastPath",
+			value: attribute.MapValue(
+				attribute.String("one", "1"),
+				attribute.String("two", "2"),
+				attribute.String("three", "3"),
+			),
+		},
+		{
+			name: "DuplicateMap",
+			value: attribute.MapValue(
+				attribute.String("one", "1"),
+				attribute.String("one", "2"),
+				attribute.String("two", "3"),
+			),
+		},
+		{
+			name: "NestedMapInSlice",
+			value: attribute.SliceValue(
+				attribute.MapValue(
+					attribute.String("one", "1"),
+					attribute.String("one", "2"),
+					attribute.String("two", "3"),
+				),
+			),
+		},
+	}
 
-	b.Run("FastPath", func(b *testing.B) {
-		for b.Loop() {
-			attrdedup.Value(uniqueMap, false)
-		}
-	})
-	b.Run("DuplicateMap", func(b *testing.B) {
-		for b.Loop() {
-			attrdedup.Value(duplicateMap, false)
-		}
-	})
-	b.Run("NestedMapInSlice", func(b *testing.B) {
-		for b.Loop() {
-			attrdedup.Value(nestedMapInSlice, false)
-		}
-	})
-	b.Run("AllowKeyDuplication", func(b *testing.B) {
-		for b.Loop() {
-			attrdedup.Value(duplicateMap, true)
-		}
-	})
+	dedupModes := []struct {
+		name                string
+		allowKeyDuplication bool
+	}{
+		{name: "WithKeyDeduplication"},
+		{name: "WithoutKeyDeduplication", allowKeyDuplication: true},
+	}
+
+	for _, value := range values {
+		b.Run(value.name, func(b *testing.B) {
+			for _, mode := range dedupModes {
+				b.Run(mode.name, func(b *testing.B) {
+					for b.Loop() {
+						attrdedup.Value(value.value, mode.allowKeyDuplication)
+					}
+				})
+			}
+		})
+	}
 }
