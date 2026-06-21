5bd98cac399c65d2806f43cc476b452942ac68bc
Puneet Singh <puneet.mir@gmail.com>
2026-06-21 16:34:42 +0000

    create sdk internal attrnorm package

---
+++ sdk/internal/attrnorm/doc.go
@@ -0,0 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// Package attrnorm normalizes attribute values. It supports:
+//   - String-length truncation for attribute value types that contain strings.
+package attrnorm // import "go.opentelemetry.io/otel/sdk/internal/attrnorm"
