12712d3364444dc006bf7fd1970c59c97ed9b33d
Sam Xie <sam@samxie.me>
2026-07-24 18:02:46 -0700

    Remove trigger

--- propagation/propagation.go
+++ propagation/propagation.go
@@ -10,7 +10,6 @@ import (
 
 // TextMapCarrier is the storage medium used by a TextMapPropagator.
 // See ValuesGetter for how a TextMapCarrier can get multiple values for a key.
-// TODO: REMOVE THIS COMMENT.
 type TextMapCarrier interface {
 	// DO NOT CHANGE: any modification will not be backwards compatible and
 	// must never be done outside of a new major release.
