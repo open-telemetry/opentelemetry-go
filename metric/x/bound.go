51281bdfd6d1bf765a1d9e0375c42abb344c90e5
David Ashpole <dashpole@google.com>
2026-07-31 20:29:26 +0000

    Fix failing CI checks

--- metric/x/bound.go
+++ metric/x/bound.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package x // import "go.opentelemetry.io/otel/metric/x"
+package x
 
 import (
 	"go.opentelemetry.io/otel/attribute"
