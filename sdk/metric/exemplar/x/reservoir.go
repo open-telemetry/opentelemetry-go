e3eacf377ef286449860029726be093fe17e64ad
David Ashpole <dashpole@google.com>
2026-08-13 19:28:34 +0000

    Fix failing lint checks

--- sdk/metric/exemplar/x/reservoir.go
+++ sdk/metric/exemplar/x/reservoir.go
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // Package x contains experimental exemplar reservoirs.
-package x // import "go.opentelemetry.io/otel/sdk/metric/exemplar/x"
+package x
 
 import (
 	"context"
