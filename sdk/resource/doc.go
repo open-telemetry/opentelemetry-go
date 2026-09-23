8857622550caf2f91cb57db4346ad4443e72b682
aman99dex <kumarguptaaman896@gmail.com>
2026-09-23 15:51:36 +0530

    sdk/resource: document supported environment variables
    Add an Environment Variables section to the package documentation covering OTEL_RESOURCE_ATTRIBUTES and OTEL_SERVICE_NAME, and mention OTEL_SERVICE_NAME in the Environment and EnvironmentWithContext doc comments.
    
    Part of #2046.

--- sdk/resource/doc.go
+++ sdk/resource/doc.go
@@ -10,10 +10,23 @@
 // interface is defined. Implementations of this interface can be passed to
 // the Detect function to generate a Resource from the merged information.
 //
-// To load a user defined Resource from the environment variable
-// OTEL_RESOURCE_ATTRIBUTES the FromEnv Detector can be used. It will interpret
-// the value as a list of comma delimited key/value pairs
-// (e.g. `<key1>=<value1>,<key2>=<value2>,...`).
+// # Environment Variables
+//
+// The environment variables described below are read by [Default],
+// [Environment], and the detector added by [WithFromEnv].
+// [Default] reads them only once and caches the resulting Resource.
+//
+// OTEL_RESOURCE_ATTRIBUTES (default: none) -
+// a comma-separated list of key=value pairs added as string resource attributes.
+// Example value: "key1=value1,key2=value2".
+// Keys and values are trimmed of surrounding whitespace, and values are percent-decoded.
+// Pairs without "=" are dropped and reported as an error wrapping [ErrPartialResource].
+//
+// OTEL_SERVICE_NAME (default: none) -
+// the value of the "service.name" resource attribute.
+// OTEL_SERVICE_NAME takes precedence over a "service.name" set in OTEL_RESOURCE_ATTRIBUTES.
+// If neither variable sets "service.name", [Default] uses "unknown_service:"
+// followed by the name of the executable.
 //
 // While this package provides a stable API,
 // the attributes added by resource detectors may change.
